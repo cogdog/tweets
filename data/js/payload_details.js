96c0e3ee062d26625d3b1d5dd7555f88197a6e66
Alan Levine <cogdogblog@gmail.com>
2016-08-26 06:15:14 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73878,
-	"created_at": "Fri Aug 26 08:15:10 +0000 2016",
+	"tweets": 73880,
+	"created_at": "Fri Aug 26 13:15:13 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_08.js",
 		"year": 2016,
 		"var_name": "tweets_2016_08",
-		"tweet_count": 598,
+		"tweet_count": 600,
 		"month": 8
 	},
 	{

3e695ce61fa817af78519d2f29285be840f8eb70
Alan Levine <cogdogblog@gmail.com>
2017-02-15 22:15:15 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77762,
-	"created_at": "Thu Feb 16 03:15:14 +0000 2017",
+	"tweets": 77763,
+	"created_at": "Thu Feb 16 05:15:14 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_02.js
+++ data/js/tweets/2017_02.js
@@ -1,6 +1,352 @@
 Grailbird.data.tweets_2017_02 = 
 [
 	{
+		"created_at": "Thu Feb 16 05:06:25 +0000 2017",
+		"id": 832093757042339800,
+		"id_str": "832093757042339840",
+		"text": "RT @rabiasquared: Oh this word is a keeper https://t.co/nRTiDeuXtj",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rabiasquared",
+					"name": "rabia chaudry",
+					"id": 2209742392,
+					"id_str": "2209742392",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nRTiDeuXtj",
+					"expanded_url": "https://twitter.com/aparnapkin/status/831989908680081409",
+					"display_url": "twitter.com/aparnapkin/sta…",
+					"indices": [
+						43,
+						66
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Strawberry, Arizona",
+			"description": "Barks about and plays with web tech at https://t.co/ZxC70Ze4em Digs photography, guitars, #ds106, mocking MOOCs, biking, coding, the Who. Has shots.",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70Ze4em",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10088,
+			"friends_count": 1318,
+			"listed_count": 783,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 728,
+			"utc_offset": -25200,
+			"time_zone": "Arizona",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 77754,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Feb 16 04:32:52 +0000 2017",
+			"id": 832085314181165000,
+			"id_str": "832085314181165061",
+			"text": "Oh this word is a keeper https://t.co/nRTiDeuXtj",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/nRTiDeuXtj",
+						"expanded_url": "https://twitter.com/aparnapkin/status/831989908680081409",
+						"display_url": "twitter.com/aparnapkin/sta…",
+						"indices": [
+							25,
+							48
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2209742392,
+				"id_str": "2209742392",
+				"name": "rabia chaudry",
+				"screen_name": "rabiasquared",
+				"location": "Washington DC ",
+				"description": "@NewAmerica @Undisclosedpod lawyer/agitator/#MrBeans lover. NY Times Bestselling Author of https://t.co/DIT91yZEfa \nContact: https://t.co/tSGHTNZQB4",
+				"url": "https://t.co/pyycuGBrdJ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pyycuGBrdJ",
+								"expanded_url": "http://www.rabiachaudry.com",
+								"display_url": "rabiachaudry.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/DIT91yZEfa",
+								"expanded_url": "http://Adnanstory.com",
+								"display_url": "Adnanstory.com",
+								"indices": [
+									91,
+									114
+								]
+							},
+							{
+								"url": "https://t.co/tSGHTNZQB4",
+								"expanded_url": "http://rabiachaudry.com",
+								"display_url": "rabiachaudry.com",
+								"indices": [
+									125,
+									148
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 79330,
+				"friends_count": 1961,
+				"listed_count": 918,
+				"created_at": "Fri Dec 06 03:09:44 +0000 2013",
+				"favourites_count": 119916,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 86569,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "CEF5EE",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000170107137/bAVe_7C7.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000170107137/bAVe_7C7.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/705064915652902912/lKrVJNdE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/705064915652902912/lKrVJNdE_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2209742392/1462992271",
+				"profile_link_color": "090A09",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 831989908680081400,
+			"quoted_status_id_str": "831989908680081409",
+			"quoted_status": {
+				"created_at": "Wed Feb 15 22:13:46 +0000 2017",
+				"id": 831989908680081400,
+				"id_str": "831989908680081409",
+				"text": "New word suggestion: Hatriotism (HATE-TREE-A-TISM). -n.- Spreading vitriol and bigotry under the guise of national pride, Also see: hatriot.",
+				"truncated": false,
+				"entities": {
+					"hashtags": [],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": []
+				},
+				"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 11757522,
+					"id_str": "11757522",
+					"name": "Aparna Nancherla",
+					"screen_name": "aparnapkin",
+					"location": "NYc, a mere butt dial away",
+					"description": "I am a scrunched up napkin with recyclable dreams. Also a comedian in some cultures. Total drag in others. album available here: https://t.co/CyTTz5j9ET",
+					"url": "https://t.co/t59uLIoppM",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "https://t.co/t59uLIoppM",
+									"expanded_url": "http://www.aparnacomedy.com",
+									"display_url": "aparnacomedy.com",
+									"indices": [
+										0,
+										23
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": [
+								{
+									"url": "https://t.co/CyTTz5j9ET",
+									"expanded_url": "http://bentzenballrecords.com",
+									"display_url": "bentzenballrecords.com",
+									"indices": [
+										129,
+										152
+									]
+								}
+							]
+						}
+					},
+					"protected": false,
+					"followers_count": 156801,
+					"friends_count": 2800,
+					"listed_count": 2333,
+					"created_at": "Wed Jan 02 15:11:12 +0000 2008",
+					"favourites_count": 24183,
+					"utc_offset": -18000,
+					"time_zone": "Eastern Time (US & Canada)",
+					"geo_enabled": false,
+					"verified": true,
+					"statuses_count": 16364,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "642D8B",
+					"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/11804600/happy_monsters2.jpg",
+					"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/11804600/happy_monsters2.jpg",
+					"profile_background_tile": true,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/293909760/frowsy_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/293909760/frowsy_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/11757522/1392069730",
+					"profile_link_color": "FF0000",
+					"profile_sidebar_border_color": "65B0DA",
+					"profile_sidebar_fill_color": "7AC3EE",
+					"profile_text_color": "3D1957",
+					"profile_use_background_image": true,
+					"has_extended_profile": false,
+					"default_profile": false,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 4092,
+				"favorite_count": 8801,
+				"favorited": false,
+				"retweeted": false,
+				"lang": "en"
+			},
+			"retweet_count": 61,
+			"favorite_count": 143,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 831989908680081400,
+		"quoted_status_id_str": "831989908680081409",
+		"retweet_count": 61,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Feb 16 03:05:29 +0000 2017",
 		"id": 832063323373871100,
 		"id_str": "832063323373871105",

a8422f87bffe36ae145c8a7394529211b883f76b
Alan Levine <cogdogblog@gmail.com>
2017-04-18 14:15:16 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79131,
-	"created_at": "Tue Apr 18 20:15:13 +0000 2017",
+	"tweets": 79132,
+	"created_at": "Tue Apr 18 21:15:16 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,354 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Tue Apr 18 20:50:08 +0000 2017",
+		"id": 854436909501661200,
+		"id_str": "854436909501661184",
+		"text": "And you @ronald_2008 are absolutely 100000% #ds106 #4life Thanks for all you bring to #ds106 https://t.co/5hFFNZ69Gk",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ds106",
+					"indices": [
+						44,
+						50
+					]
+				},
+				{
+					"text": "4life",
+					"indices": [
+						51,
+						57
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						86,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ronald_2008",
+					"name": "Ronald_2008",
+					"id": 211458710,
+					"id_str": "211458710",
+					"indices": [
+						8,
+						20
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5hFFNZ69Gk",
+					"expanded_url": "https://twitter.com/ronald_2008/status/854426407384092672",
+					"display_url": "twitter.com/ronald_2008/st…",
+					"indices": [
+						93,
+						116
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Strawberry, Arizona",
+			"description": "Barks about and plays with web tech at https://t.co/ZxC70Ze4em Digs photography, guitars, #ds106, mocking MOOCs, biking, coding, the Who. Has shots.",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70Ze4em",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10165,
+			"friends_count": 1345,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 717,
+			"utc_offset": -25200,
+			"time_zone": "Arizona",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 79118,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 854426407384092700,
+		"quoted_status_id_str": "854426407384092672",
+		"quoted_status": {
+			"created_at": "Tue Apr 18 20:08:24 +0000 2017",
+			"id": 854426407384092700,
+			"id_str": "854426407384092672",
+			"text": "@ds106dc #tdc1927  #ds106  \nI love DS106\nIt's #4life https://t.co/llJjhP6tMU",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "tdc1927",
+						"indices": [
+							9,
+							17
+						]
+					},
+					{
+						"text": "ds106",
+						"indices": [
+							19,
+							25
+						]
+					},
+					{
+						"text": "4life",
+						"indices": [
+							46,
+							52
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ds106dc",
+						"name": "ds106 Daily Create",
+						"id": 995562187,
+						"id_str": "995562187",
+						"indices": [
+							0,
+							8
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 854426319966408700,
+						"id_str": "854426319966408710",
+						"indices": [
+							53,
+							76
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/C9uINghXkAY_NhA.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/C9uINghXkAY_NhA.jpg",
+						"url": "https://t.co/llJjhP6tMU",
+						"display_url": "pic.twitter.com/llJjhP6tMU",
+						"expanded_url": "https://twitter.com/ronald_2008/status/854426407384092672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 460,
+								"h": 614,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 340,
+								"h": 454,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 460,
+								"h": 614,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 854426319966408700,
+						"id_str": "854426319966408710",
+						"indices": [
+							53,
+							76
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/C9uINghXkAY_NhA.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/C9uINghXkAY_NhA.jpg",
+						"url": "https://t.co/llJjhP6tMU",
+						"display_url": "pic.twitter.com/llJjhP6tMU",
+						"expanded_url": "https://twitter.com/ronald_2008/status/854426407384092672/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"medium": {
+								"w": 460,
+								"h": 614,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 340,
+								"h": 454,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 460,
+								"h": 614,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								230,
+								307
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/C9uINghXkAY_NhA.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 854266147398537200,
+			"in_reply_to_status_id_str": "854266147398537216",
+			"in_reply_to_user_id": 995562187,
+			"in_reply_to_user_id_str": "995562187",
+			"in_reply_to_screen_name": "ds106dc",
+			"user": {
+				"id": 211458710,
+				"id_str": "211458710",
+				"name": "Ronald_2008",
+				"screen_name": "ronald_2008",
+				"location": "black water reservoir",
+				"description": "Designer of medical education. DS106 enthousiast and promoter. Rhizomatic student for life. Free time story teller & illustrator. Public library ambassador.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 298,
+				"friends_count": 149,
+				"listed_count": 46,
+				"created_at": "Wed Nov 03 09:39:33 +0000 2010",
+				"favourites_count": 19896,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 10793,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/601670226048995328/JvoMygtS_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/601670226048995328/JvoMygtS_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/211458710/1411894065",
+				"profile_link_color": "4A913C",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Apr 18 19:16:28 +0000 2017",
 		"id": 854413336313708500,
 		"id_str": "854413336313708544",

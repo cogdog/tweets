8f46f26dcfc56767a0bd4de9aef1b1b65a85a0d6
Alan Levine <cogdogblog@gmail.com>
2017-06-01 10:15:11 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80001,
-	"created_at": "Thu Jun 01 16:15:10 +0000 2017",
+	"tweets": 80003,
+	"created_at": "Thu Jun 01 17:15:10 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_06.js
+++ data/js/tweets/2017_06.js
@@ -1,6 +1,657 @@
 Grailbird.data.tweets_2017_06 = 
 [
 	{
+		"created_at": "Thu Jun 01 16:17:12 +0000 2017",
+		"id": 870313291490381800,
+		"id_str": "870313291490381824",
+		"text": "@ds106dc #tdc1970 #30daytdc I work on my creative muscles away from the computer. But there is sand to kick. https://t.co/nwcyvL38ac",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc1970",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"text": "30daytdc",
+					"indices": [
+						18,
+						27
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 870313289426673700,
+					"id_str": "870313289426673665",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/DBP5U1uUIAEkeIz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DBP5U1uUIAEkeIz.jpg",
+					"url": "https://t.co/nwcyvL38ac",
+					"display_url": "pic.twitter.com/nwcyvL38ac",
+					"expanded_url": "https://twitter.com/cogdog/status/870313291490381824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 870313289426673700,
+					"id_str": "870313289426673665",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/DBP5U1uUIAEkeIz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DBP5U1uUIAEkeIz.jpg",
+					"url": "https://t.co/nwcyvL38ac",
+					"display_url": "pic.twitter.com/nwcyvL38ac",
+					"expanded_url": "https://twitter.com/cogdog/status/870313291490381824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Strawberry, Arizona",
+			"description": "Barks about and plays with web tech at https://t.co/ZxC70Ze4em Digs photography, guitars, #ds106, mocking MOOCs, biking, coding, the Who. Has shots.",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70Ze4em",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10225,
+			"friends_count": 1356,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 717,
+			"utc_offset": -25200,
+			"time_zone": "Arizona",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 79984,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Jun 01 16:15:10 +0000 2017",
+		"id": 870312778799566800,
+		"id_str": "870312778799566848",
+		"text": "RT @johnjohnston: @ds106dc #DS106 #tdc1970  Number 1  of  #30dayTDC #4life on the beach https://t.co/lvzOFcFwoD",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DS106",
+					"indices": [
+						27,
+						33
+					]
+				},
+				{
+					"text": "tdc1970",
+					"indices": [
+						34,
+						42
+					]
+				},
+				{
+					"text": "30dayTDC",
+					"indices": [
+						58,
+						67
+					]
+				},
+				{
+					"text": "4life",
+					"indices": [
+						68,
+						74
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "johnjohnston",
+					"name": "john johnston",
+					"id": 7484192,
+					"id_str": "7484192",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						18,
+						26
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 870309175548678100,
+					"id_str": "870309175548678144",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+					"url": "https://t.co/lvzOFcFwoD",
+					"display_url": "pic.twitter.com/lvzOFcFwoD",
+					"expanded_url": "https://twitter.com/johnjohnston/status/870309395753824256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 431,
+							"h": 373,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 431,
+							"h": 373,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 431,
+							"h": 373,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 870309395753824300,
+					"source_status_id_str": "870309395753824256",
+					"source_user_id": 7484192,
+					"source_user_id_str": "7484192"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 870309175548678100,
+					"id_str": "870309175548678144",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+					"url": "https://t.co/lvzOFcFwoD",
+					"display_url": "pic.twitter.com/lvzOFcFwoD",
+					"expanded_url": "https://twitter.com/johnjohnston/status/870309395753824256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 431,
+							"h": 373,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 431,
+							"h": 373,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 431,
+							"h": 373,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 870309395753824300,
+					"source_status_id_str": "870309395753824256",
+					"source_user_id": 7484192,
+					"source_user_id_str": "7484192"
+				}
+			]
+		},
+		"source": "<a href=\"http://www.echofon.com/\" rel=\"nofollow\">Echofon</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Strawberry, Arizona",
+			"description": "Barks about and plays with web tech at https://t.co/ZxC70Ze4em Digs photography, guitars, #ds106, mocking MOOCs, biking, coding, the Who. Has shots.",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70Ze4em",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10225,
+			"friends_count": 1356,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 717,
+			"utc_offset": -25200,
+			"time_zone": "Arizona",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 79984,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/88763117/dog2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Jun 01 16:01:44 +0000 2017",
+			"id": 870309395753824300,
+			"id_str": "870309395753824256",
+			"text": "@ds106dc #DS106 #tdc1970  Number 1  of  #30dayTDC #4life on the beach https://t.co/lvzOFcFwoD",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "DS106",
+						"indices": [
+							9,
+							15
+						]
+					},
+					{
+						"text": "tdc1970",
+						"indices": [
+							16,
+							24
+						]
+					},
+					{
+						"text": "30dayTDC",
+						"indices": [
+							40,
+							49
+						]
+					},
+					{
+						"text": "4life",
+						"indices": [
+							50,
+							56
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ds106dc",
+						"name": "ds106 Daily Create",
+						"id": 995562187,
+						"id_str": "995562187",
+						"indices": [
+							0,
+							8
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 870309175548678100,
+						"id_str": "870309175548678144",
+						"indices": [
+							70,
+							93
+						],
+						"media_url": "http://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+						"url": "https://t.co/lvzOFcFwoD",
+						"display_url": "pic.twitter.com/lvzOFcFwoD",
+						"expanded_url": "https://twitter.com/johnjohnston/status/870309395753824256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 431,
+								"h": 373,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 431,
+								"h": 373,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 431,
+								"h": 373,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 870309175548678100,
+						"id_str": "870309175548678144",
+						"indices": [
+							70,
+							93
+						],
+						"media_url": "http://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DBP1lYVXkAAMCNy.jpg",
+						"url": "https://t.co/lvzOFcFwoD",
+						"display_url": "pic.twitter.com/lvzOFcFwoD",
+						"expanded_url": "https://twitter.com/johnjohnston/status/870309395753824256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 431,
+								"h": 373,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 431,
+								"h": 373,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 431,
+								"h": 373,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 870214473331523600,
+			"in_reply_to_status_id_str": "870214473331523585",
+			"in_reply_to_user_id": 995562187,
+			"in_reply_to_user_id_str": "995562187",
+			"in_reply_to_screen_name": "ds106dc",
+			"user": {
+				"id": 7484192,
+				"id_str": "7484192",
+				"name": "john johnston",
+				"screen_name": "johnjohnston",
+				"location": "Glasgow, UK",
+				"description": "Primary Teacher Scotland, EduTech, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk",
+				"url": "https://t.co/p7vNWSMAoH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/p7vNWSMAoH",
+								"expanded_url": "http://johnjohnston.info/blog/",
+								"display_url": "johnjohnston.info/blog/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3712,
+				"friends_count": 2410,
+				"listed_count": 283,
+				"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+				"favourites_count": 3369,
+				"utc_offset": 3600,
+				"time_zone": "Edinburgh",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 15546,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EE6C00",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+				"profile_link_color": "0000FF",
+				"profile_sidebar_border_color": "87BC44",
+				"profile_sidebar_fill_color": "FFFF00",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jun 01 15:45:00 +0000 2017",
 		"id": 870305186484322300,
 		"id_str": "870305186484322304",

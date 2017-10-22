a82cfe9d5748336e165732fc036d46baf143141e
Alan Levine <cogdogblog@gmail.com>
2017-10-21 17:15:13 -0700

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_10.js",
 		"year": 2017,
 		"var_name": "tweets_2017_10",
-		"tweet_count": 353,
+		"tweet_count": 354,
 		"month": 10
 	},
 	{

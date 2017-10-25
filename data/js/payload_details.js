265b63c32b52d6c13f7cbb120d9fbc93d5c98002
Alan Levine <cogdogblog@gmail.com>
2017-10-25 12:15:18 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82590,
-	"created_at": "Wed Oct 25 17:15:14 +0000 2017",
+	"tweets": 82591,
+	"created_at": "Wed Oct 25 19:15:17 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_10.js",
 		"year": 2017,
 		"var_name": "tweets_2017_10",
-		"tweet_count": 423,
+		"tweet_count": 424,
 		"month": 10
 	},
 	{

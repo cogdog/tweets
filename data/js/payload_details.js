8bc236aa2b59b87322fdc2454c3ae558dd4a643a
Alan Levine <cogdogblog@gmail.com>
2018-06-25 14:15:14 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86002,
-	"created_at": "Mon Jun 25 19:15:11 +0000 2018",
+	"tweets": 86003,
+	"created_at": "Mon Jun 25 20:15:12 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Mon Jun 25 19:16:28 +0000 2018",
+		"id": 1011327282391199700,
+		"id_str": "1011327282391199744",
+		"text": "Getting attribution for images is nice even when credit is given to some guy name Adam #itHappensAlot #WhoIsThatGuy https://t.co/GgXAS9Z8sr",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "itHappensAlot",
+					"indices": [
+						87,
+						101
+					]
+				},
+				{
+					"text": "WhoIsThatGuy",
+					"indices": [
+						102,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1011327261960572900,
+					"id_str": "1011327261960572928",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/Dgj0yVjUEAA8_uL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dgj0yVjUEAA8_uL.jpg",
+					"url": "https://t.co/GgXAS9Z8sr",
+					"display_url": "pic.twitter.com/GgXAS9Z8sr",
+					"expanded_url": "https://twitter.com/cogdog/status/1011327282391199744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 548,
+							"h": 160,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 548,
+							"h": 160,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 548,
+							"h": 160,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1011327261960572900,
+					"id_str": "1011327261960572928",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/Dgj0yVjUEAA8_uL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dgj0yVjUEAA8_uL.jpg",
+					"url": "https://t.co/GgXAS9Z8sr",
+					"display_url": "pic.twitter.com/GgXAS9Z8sr",
+					"expanded_url": "https://twitter.com/cogdog/status/1011327282391199744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 548,
+							"h": 160,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 548,
+							"h": 160,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 548,
+							"h": 160,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine ☠",
+			"screen_name": "cogdog",
+			"location": "Strawberry, Arizona",
+			"description": "Barks about and plays with web tech at https://t.co/ZxC70Ze4em Digs photography, guitars, #ds106, mocking MOOCs, biking, coding, the Who. Has shots.",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70Ze4em",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10554,
+			"friends_count": 1447,
+			"listed_count": 793,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 754,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 85970,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Jun 25 18:51:03 +0000 2018",
 		"id": 1011320884768931800,
 		"id_str": "1011320884768931841",

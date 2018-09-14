ebd59ed9702f2059fbf57c1b28a849c8a1017877
Alan Levine <cogdogblog@gmail.com>
2018-09-14 13:15:10 -0600

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86472,
-	"created_at": "Fri Sep 14 03:15:09 +0000 2018",
+	"tweets": 86474,
+	"created_at": "Fri Sep 14 19:15:09 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,555 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Fri Sep 14 19:08:24 +0000 2018",
+		"id": 1040678668178206700,
+		"id_str": "1040678668178206720",
+		"text": "Amazon preschools preps us? \"human transport device may include a platform to support a user, an enclosure coupled… https://t.co/gMlWl3u6N4",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gMlWl3u6N4",
+					"expanded_url": "https://twitter.com/i/web/status/1040678668178206720",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine ☠",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10330,
+			"friends_count": 1447,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 86435,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Fri Sep 14 18:47:48 +0000 2018",
+		"id": 1040673488087011300,
+		"id_str": "1040673488087011329",
+		"text": "RT @Brill4Congress: Thank you to @IndivisibleTeam &amp; @IndivisPrescott for the endorsement!\n#BlueWave #FlipItBlue https://t.co/OMkhFKXQ9M",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BlueWave",
+					"indices": [
+						94,
+						103
+					]
+				},
+				{
+					"text": "FlipItBlue",
+					"indices": [
+						104,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Brill4Congress",
+					"name": "Brill for Congress",
+					"id": 977271571680145400,
+					"id_str": "977271571680145408",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "IndivisibleTeam",
+					"name": "Indivisible Guide",
+					"id": 810590628727914500,
+					"id_str": "810590628727914496",
+					"indices": [
+						33,
+						49
+					]
+				},
+				{
+					"screen_name": "IndivisPrescott",
+					"name": "Prescott Indivisible",
+					"id": 840646971534655500,
+					"id_str": "840646971534655488",
+					"indices": [
+						56,
+						72
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1040633951541493800,
+					"id_str": "1040633951541493767",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+					"url": "https://t.co/OMkhFKXQ9M",
+					"display_url": "pic.twitter.com/OMkhFKXQ9M",
+					"expanded_url": "https://twitter.com/Brill4Congress/status/1040633953022083073/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1040633953022083100,
+					"source_status_id_str": "1040633953022083073",
+					"source_user_id": 977271571680145400,
+					"source_user_id_str": "977271571680145408"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1040633951541493800,
+					"id_str": "1040633951541493767",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+					"url": "https://t.co/OMkhFKXQ9M",
+					"display_url": "pic.twitter.com/OMkhFKXQ9M",
+					"expanded_url": "https://twitter.com/Brill4Congress/status/1040633953022083073/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1040633953022083100,
+					"source_status_id_str": "1040633953022083073",
+					"source_user_id": 977271571680145400,
+					"source_user_id_str": "977271571680145408"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine ☠",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10330,
+			"friends_count": 1447,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 86435,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Sep 14 16:10:43 +0000 2018",
+			"id": 1040633953022083100,
+			"id_str": "1040633953022083073",
+			"text": "Thank you to @IndivisibleTeam &amp; @IndivisPrescott for the endorsement!\n#BlueWave #FlipItBlue https://t.co/OMkhFKXQ9M",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "BlueWave",
+						"indices": [
+							74,
+							83
+						]
+					},
+					{
+						"text": "FlipItBlue",
+						"indices": [
+							84,
+							95
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "IndivisibleTeam",
+						"name": "Indivisible Guide",
+						"id": 810590628727914500,
+						"id_str": "810590628727914496",
+						"indices": [
+							13,
+							29
+						]
+					},
+					{
+						"screen_name": "IndivisPrescott",
+						"name": "Prescott Indivisible",
+						"id": 840646971534655500,
+						"id_str": "840646971534655488",
+						"indices": [
+							36,
+							52
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1040633951541493800,
+						"id_str": "1040633951541493767",
+						"indices": [
+							96,
+							119
+						],
+						"media_url": "http://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+						"url": "https://t.co/OMkhFKXQ9M",
+						"display_url": "pic.twitter.com/OMkhFKXQ9M",
+						"expanded_url": "https://twitter.com/Brill4Congress/status/1040633953022083073/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1040633951541493800,
+						"id_str": "1040633951541493767",
+						"indices": [
+							96,
+							119
+						],
+						"media_url": "http://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DnETEL7XgAcoBUa.jpg",
+						"url": "https://t.co/OMkhFKXQ9M",
+						"display_url": "pic.twitter.com/OMkhFKXQ9M",
+						"expanded_url": "https://twitter.com/Brill4Congress/status/1040633953022083073/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 977271571680145400,
+				"id_str": "977271571680145408",
+				"name": "Brill for Congress",
+				"screen_name": "Brill4Congress",
+				"location": "Prescott, AZ",
+				"description": "Official Twitter account for David Brill, M.D., Democratic candidate in Arizona's Fourth Congressional District. Paid for by Brill for Congress.",
+				"url": "https://t.co/XCT47hRv1T",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/XCT47hRv1T",
+								"expanded_url": "http://www.brillforcongress.com",
+								"display_url": "brillforcongress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 749,
+				"friends_count": 1092,
+				"listed_count": 18,
+				"created_at": "Fri Mar 23 19:51:14 +0000 2018",
+				"favourites_count": 478,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 424,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1018197291293741056/fkA3B8l9_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1018197291293741056/fkA3B8l9_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/977271571680145408/1531595135",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 29,
+			"favorite_count": 30,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 29,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Sep 14 02:27:14 +0000 2018",
 		"id": 1040426716504698900,
 		"id_str": "1040426716504698880",

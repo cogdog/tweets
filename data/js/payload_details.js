dd6eeabd956cbac1dbc9986f78ef2ef3d23e6d02
Alan Levine <cogdogblog@gmail.com>
2018-09-19 13:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86505,
-	"created_at": "Wed Sep 19 17:15:10 +0000 2018",
+	"tweets": 86506,
+	"created_at": "Wed Sep 19 19:15:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Wed Sep 19 18:28:58 +0000 2018",
+		"id": 1042480686958104600,
+		"id_str": "1042480686958104577",
+		"text": "\"It Answers in Lights!\" https://t.co/Owl0oKClzo",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Owl0oKClzo",
+					"expanded_url": "https://twitter.com/TheoKL/status/1042480325920792578",
+					"display_url": "twitter.com/TheoKL/status/…",
+					"indices": [
+						24,
+						47
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine ☠",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10336,
+			"friends_count": 1448,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 752,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 86467,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1042480325920792600,
+		"quoted_status_id_str": "1042480325920792578",
+		"quoted_status": {
+			"created_at": "Wed Sep 19 18:27:32 +0000 2018",
+			"id": 1042480325920792600,
+			"id_str": "1042480325920792578",
+			"text": "Gotta Love Edtech! https://t.co/LMjV2ToCJ1",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1042480300813692900,
+						"id_str": "1042480300813692928",
+						"indices": [
+							19,
+							42
+						],
+						"media_url": "http://pbs.twimg.com/media/DneiT29XsAAuPyW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DneiT29XsAAuPyW.jpg",
+						"url": "https://t.co/LMjV2ToCJ1",
+						"display_url": "pic.twitter.com/LMjV2ToCJ1",
+						"expanded_url": "https://twitter.com/TheoKL/status/1042480325920792578/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 613,
+								"h": 440,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 613,
+								"h": 440,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 613,
+								"h": 440,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1042480300813692900,
+						"id_str": "1042480300813692928",
+						"indices": [
+							19,
+							42
+						],
+						"media_url": "http://pbs.twimg.com/media/DneiT29XsAAuPyW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DneiT29XsAAuPyW.jpg",
+						"url": "https://t.co/LMjV2ToCJ1",
+						"display_url": "pic.twitter.com/LMjV2ToCJ1",
+						"expanded_url": "https://twitter.com/TheoKL/status/1042480325920792578/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 613,
+								"h": 440,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 613,
+								"h": 440,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 613,
+								"h": 440,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1290061,
+				"id_str": "1290061",
+				"name": "Theo Kuechel",
+				"screen_name": "TheoKL",
+				"location": "",
+				"description": "Learning Technology,  Ed Research, Video for Learning, Archives/Collections, Open Education, Digital Culture, Fellow MirandaNet",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3485,
+				"friends_count": 2692,
+				"listed_count": 235,
+				"created_at": "Fri Mar 16 14:36:02 +0000 2007",
+				"favourites_count": 5852,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 29758,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/689957937545113601/z79K8wBd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/689957937545113601/z79K8wBd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1290061/1453976284",
+				"profile_link_color": "1212BC",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Sep 19 17:01:19 +0000 2018",
 		"id": 1042458628299604000,
 		"id_str": "1042458628299603968",

72c67099cb05cb58640530c923d08af4192b7373
Alan Levine <cogdogblog@gmail.com>
2018-10-14 20:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86693,
-	"created_at": "Sun Oct 14 23:15:09 +0000 2018",
+	"tweets": 86694,
+	"created_at": "Mon Oct 15 02:15:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Mon Oct 15 01:38:21 +0000 2018",
+		"id": 1051648438998392800,
+		"id_str": "1051648438998392834",
+		"full_text": "Celebrating a 50 Year Old Demo #theDemoat50 https://t.co/V65Fn2JA30 https://t.co/WiyVE8nEf7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "theDemoat50",
+					"indices": [
+						31,
+						43
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/V65Fn2JA30",
+					"expanded_url": "http://cogdogblog.com/2018/10/50-year-old-demo/",
+					"display_url": "cogdogblog.com/2018/10/50-yea…",
+					"indices": [
+						44,
+						67
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1051648437400129500,
+					"id_str": "1051648437400129536",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/Dpg0rpPUUAAlUEa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dpg0rpPUUAAlUEa.jpg",
+					"url": "https://t.co/WiyVE8nEf7",
+					"display_url": "pic.twitter.com/WiyVE8nEf7",
+					"expanded_url": "https://twitter.com/cogdog/status/1051648438998392834/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 619,
+							"h": 316,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 619,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 619,
+							"h": 316,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1051648437400129500,
+					"id_str": "1051648437400129536",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/Dpg0rpPUUAAlUEa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dpg0rpPUUAAlUEa.jpg",
+					"url": "https://t.co/WiyVE8nEf7",
+					"display_url": "pic.twitter.com/WiyVE8nEf7",
+					"expanded_url": "https://twitter.com/cogdog/status/1051648438998392834/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 619,
+							"h": 316,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 619,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 619,
+							"h": 316,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine ☠",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10402,
+			"friends_count": 1450,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 752,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 86654,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Celebrating a 50 Year Old Demo #theDemoat50 https://t.co/V65Fn2JA30 https://t.co/WiyVE8nEf7"
+	},
+	{
 		"created_at": "Sun Oct 14 22:23:52 +0000 2018",
 		"id": 1051599497317572600,
 		"id_str": "1051599497317572609",

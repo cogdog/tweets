8a3064d6abfc1c056a156210222a204bf5d70d27
Alan Levine <cogdogblog@gmail.com>
2018-10-16 10:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86712,
-	"created_at": "Tue Oct 16 14:15:10 +0000 2018",
+	"tweets": 86713,
+	"created_at": "Tue Oct 16 16:15:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Tue Oct 16 15:54:04 +0000 2018",
+		"id": 1052226175609827300,
+		"id_str": "1052226175609827328",
+		"full_text": "@tmaynard5 @techsavvyed @ronhoutman A timer for the whole presentation or per slide? For the latter, one way is to add a video of a timer set to autoplay; see  https://t.co/78IAjjywZS",
+		"truncated": false,
+		"display_text_range": [
+			36,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tmaynard5",
+					"name": "Tara Maynard",
+					"id": 503192132,
+					"id_str": "503192132",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "techsavvyed",
+					"name": "Ben Rimes",
+					"id": 11144032,
+					"id_str": "11144032",
+					"indices": [
+						11,
+						23
+					]
+				},
+				{
+					"screen_name": "ronhoutman",
+					"name": "Ron Houtman",
+					"id": 7494332,
+					"id_str": "7494332",
+					"indices": [
+						24,
+						35
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/78IAjjywZS",
+					"expanded_url": "https://www.youtube.com/watch?v=Cs7sA7WQS4I",
+					"display_url": "youtube.com/watch?v=Cs7sA7…",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1052209455977418800,
+		"in_reply_to_status_id_str": "1052209455977418752",
+		"in_reply_to_user_id": 503192132,
+		"in_reply_to_user_id_str": "503192132",
+		"in_reply_to_screen_name": "tmaynard5",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine ☠",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10400,
+			"friends_count": 1450,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 753,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 86673,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@tmaynard5 @techsavvyed @ronhoutman A timer for the whole presentation or per slide? For the latter, one way is to add a video of a timer set to autoplay; see  https://t.co/78IAjjywZS"
+	},
+	{
 		"created_at": "Tue Oct 16 13:44:08 +0000 2018",
 		"id": 1052193476178137100,
 		"id_str": "1052193476178137089",

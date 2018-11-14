54acedc6cf7bbfff531451fb915395a9f4a835e6
Alan Levine <cogdogblog@gmail.com>
2018-11-13 18:15:13 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87030,
-	"created_at": "Tue Nov 13 23:15:11 +0000 2018",
+	"tweets": 87031,
+	"created_at": "Wed Nov 14 00:15:13 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Tue Nov 13 23:26:09 +0000 2018",
+		"id": 1062486805084360700,
+		"id_str": "1062486805084360710",
+		"full_text": "@hjarche @1Password TAs9ZdcRaXkx4C9ZC}TR7Z3H$VWP3&amp;L;2)kZ;,D2xW2Riyq@nsipUC&gt;eArzj(pn6.yfnaJ9,LZ[m&amp;Hz)zYG9WA)dxJue#dF4H3mqMDdfumRY)6yqa&amp;3PaZ28iDnv83LC@udEdc;MP&gt;n&amp;4fdv3Ae8uv6^hN6[uW9ghW+dkBathBptj4wqn.Nf8mr8*m4aBM6e;eAJNHob)98)ETYMMf46RxHxTaTC98C])CKkxEa4+c8AkFdG&gt;X6[vMDAFasV&amp;$",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			303
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hjarche",
+					"name": "Harold Jarche",
+					"id": 11698322,
+					"id_str": "11698322",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "1Password",
+					"name": "1Password",
+					"id": 793926,
+					"id_str": "793926",
+					"indices": [
+						9,
+						19
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1062482634146832400,
+		"in_reply_to_status_id_str": "1062482634146832384",
+		"in_reply_to_user_id": 11698322,
+		"in_reply_to_user_id_str": "11698322",
+		"in_reply_to_screen_name": "hjarche",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10374,
+			"friends_count": 1452,
+			"listed_count": 791,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 759,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 86951,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "@hjarche @1Password TAs9ZdcRaXkx4C9ZC}TR7Z3H$VWP3&amp;L;2)kZ;,D2xW2Riyq@nsipUC&gt;eArzj(pn6.yfnaJ9,LZ[m&amp;Hz)zYG9WA)dxJue#dF4H3mqMDdfumRY)6yqa&amp;3PaZ28iDnv83LC@udEdc;MP&gt;n&amp;4fdv3Ae8uv6^hN6[uW9ghW+dkBathBptj4wqn.Nf8mr8*m4aBM6e;eAJNHob)98)ETYMMf46RxHxTaTC98C])CKkxEa4+c8AkFdG&gt;X6[vMDAFasV&amp;$"
+	},
+	{
 		"created_at": "Tue Nov 13 22:51:59 +0000 2018",
 		"id": 1062478206861762600,
 		"id_str": "1062478206861762566",

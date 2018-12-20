a04690f84afb1be0af8f79ef029786de8c579168
Alan Levine <cogdogblog@gmail.com>
2018-12-20 10:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87515,
-	"created_at": "Wed Dec 19 16:15:12 +0000 2018",
+	"tweets": 87516,
+	"created_at": "Thu Dec 20 16:15:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Thu Dec 20 15:56:28 +0000 2018",
+		"id": 1075781992715817000,
+		"id_str": "1075781992715816960",
+		"full_text": "@NurseKillam @data_professor @jesslyndw @greeneterry @ConestogaC @ontarioextend @TLConestoga @cambrianhub You can count on having Extend Lunches (and other times) via Zoom during the @ontarioextend mOOC",
+		"truncated": false,
+		"display_text_range": [
+			106,
+			202
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NurseKillam",
+					"name": "Laura Killam",
+					"id": 268432639,
+					"id_str": "268432639",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "data_professor",
+					"name": "Professor Sid",
+					"id": 1029384755178090500,
+					"id_str": "1029384755178090496",
+					"indices": [
+						13,
+						28
+					]
+				},
+				{
+					"screen_name": "jesslyndw",
+					"name": "Jess Wilkinson",
+					"id": 1519806174,
+					"id_str": "1519806174",
+					"indices": [
+						29,
+						39
+					]
+				},
+				{
+					"screen_name": "greeneterry",
+					"name": "It's Beginning to Look a Lot Like Terry Greene",
+					"id": 212019344,
+					"id_str": "212019344",
+					"indices": [
+						40,
+						52
+					]
+				},
+				{
+					"screen_name": "ConestogaC",
+					"name": "Conestoga College",
+					"id": 289459240,
+					"id_str": "289459240",
+					"indices": [
+						53,
+						64
+					]
+				},
+				{
+					"screen_name": "ontarioextend",
+					"name": "OntarioExtend",
+					"id": 885339669457776600,
+					"id_str": "885339669457776641",
+					"indices": [
+						65,
+						79
+					]
+				},
+				{
+					"screen_name": "TLConestoga",
+					"name": "Teaching and Learning Conestoga",
+					"id": 951156227224096800,
+					"id_str": "951156227224096768",
+					"indices": [
+						80,
+						92
+					]
+				},
+				{
+					"screen_name": "cambrianhub",
+					"name": "Cambrian Teaching & Learning Hub",
+					"id": 892395460354355200,
+					"id_str": "892395460354355200",
+					"indices": [
+						93,
+						105
+					]
+				},
+				{
+					"screen_name": "ontarioextend",
+					"name": "OntarioExtend",
+					"id": 885339669457776600,
+					"id_str": "885339669457776641",
+					"indices": [
+						183,
+						197
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1075605047768166400,
+		"in_reply_to_status_id_str": "1075605047768166406",
+		"in_reply_to_user_id": 268432639,
+		"in_reply_to_user_id_str": "268432639",
+		"in_reply_to_screen_name": "NurseKillam",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10397,
+			"friends_count": 1451,
+			"listed_count": 791,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 762,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87201,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@NurseKillam @data_professor @jesslyndw @greeneterry @ConestogaC @ontarioextend @TLConestoga @cambrianhub You can count on having Extend Lunches (and other times) via Zoom during the @ontarioextend mOOC"
+	},
+	{
 		"created_at": "Wed Dec 19 15:42:41 +0000 2018",
 		"id": 1075416136127340500,
 		"id_str": "1075416136127340545",

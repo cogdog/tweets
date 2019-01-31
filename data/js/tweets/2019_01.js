d3f8ef186c0359e78b287bbde02c5d0ea08df88e
Alan Levine <cogdogblog@gmail.com>
2019-01-31 17:15:13 -0600

    Added 1 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 31 22:25:10 +0000 2019",
+		"id": 1091100102993809400,
+		"id_str": "1091100102993809410",
+		"full_text": "@netnarr #dda247 For our #netnarr students studying the darkness of the 2019 net, broaden your twitter stream, consider following @hypervisible @h0d3r @mcsweeneys @manjusrii @podehaye @zeynep @worrydream @paolobacigalupi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "dda247",
+					"indices": [
+						9,
+						16
+					]
+				},
+				{
+					"text": "netnarr",
+					"indices": [
+						25,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "netnarr",
+					"name": "Networked Narratives",
+					"id": 801306049663893500,
+					"id_str": "801306049663893504",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "hypervisible",
+					"name": "Should old surveillance be forgot",
+					"id": 175292337,
+					"id_str": "175292337",
+					"indices": [
+						130,
+						143
+					]
+				},
+				{
+					"screen_name": "h0d3r",
+					"name": "Hossein Derakhshan",
+					"id": 16779204,
+					"id_str": "16779204",
+					"indices": [
+						144,
+						150
+					]
+				},
+				{
+					"screen_name": "mcsweeneys",
+					"name": "Timothy McSweeney",
+					"id": 30109507,
+					"id_str": "30109507",
+					"indices": [
+						151,
+						162
+					]
+				},
+				{
+					"screen_name": "manjusrii",
+					"name": "Belinda Barnet",
+					"id": 47316108,
+					"id_str": "47316108",
+					"indices": [
+						163,
+						173
+					]
+				},
+				{
+					"screen_name": "podehaye",
+					"name": "Paul-Olivier Dehaye",
+					"id": 2219841242,
+					"id_str": "2219841242",
+					"indices": [
+						174,
+						183
+					]
+				},
+				{
+					"screen_name": "zeynep",
+					"name": "zeynep tufekci",
+					"id": 65375759,
+					"id_str": "65375759",
+					"indices": [
+						184,
+						191
+					]
+				},
+				{
+					"screen_name": "worrydream",
+					"name": "Bret Victor",
+					"id": 255617445,
+					"id_str": "255617445",
+					"indices": [
+						192,
+						203
+					]
+				},
+				{
+					"screen_name": "paolobacigalupi",
+					"name": "Paolo Bacigalupi",
+					"id": 19002473,
+					"id_str": "19002473",
+					"indices": [
+						204,
+						220
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 801306049663893500,
+		"in_reply_to_user_id_str": "801306049663893504",
+		"in_reply_to_screen_name": "netnarr",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10419,
+			"friends_count": 1451,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 764,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87533,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@netnarr #dda247 For our #netnarr students studying the darkness of the 2019 net, broaden your twitter stream, consider following @hypervisible @h0d3r @mcsweeneys @manjusrii @podehaye @zeynep @worrydream @paolobacigalupi"
+	},
+	{
 		"created_at": "Thu Jan 31 19:09:11 +0000 2019",
 		"id": 1091050779560038400,
 		"id_str": "1091050779560038401",

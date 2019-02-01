4104bd6f790909133b28ff99088200f1a69ba4a7
Alan Levine <cogdogblog@gmail.com>
2019-01-31 22:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87890,
-	"created_at": "Fri Feb 01 02:15:09 +0000 2019",
+	"tweets": 87891,
+	"created_at": "Fri Feb 01 04:15:08 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 01 04:02:11 +0000 2019",
+		"id": 1091184915461361700,
+		"id_str": "1091184915461361670",
+		"full_text": "@joefromkenyon @netnarr @NicCagePlotBot @nounjective @edtechepigrams @ro_bot_dylan We love twitter bots for #netnarr - stick around when later we make our own - I live @MagicRealismBot @DeathMedieval  @TwoHeadlines and also am a fan of @ro_bot_dylan",
+		"truncated": false,
+		"display_text_range": [
+			83,
+			249
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						108,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "joefromkenyon",
+					"name": "Joe Murphy",
+					"id": 14466272,
+					"id_str": "14466272",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "netnarr",
+					"name": "Networked Narratives",
+					"id": 801306049663893500,
+					"id_str": "801306049663893504",
+					"indices": [
+						15,
+						23
+					]
+				},
+				{
+					"screen_name": "NicCagePlotBot",
+					"name": "Nic Cage Bot",
+					"id": 981660520477548500,
+					"id_str": "981660520477548544",
+					"indices": [
+						24,
+						39
+					]
+				},
+				{
+					"screen_name": "nounjective",
+					"name": "NounjectiveBot",
+					"id": 991453437467512800,
+					"id_str": "991453437467512834",
+					"indices": [
+						40,
+						52
+					]
+				},
+				{
+					"screen_name": "edtechepigrams",
+					"name": "Edtech Epigrams",
+					"id": 1058912370263818200,
+					"id_str": "1058912370263818240",
+					"indices": [
+						53,
+						68
+					]
+				},
+				{
+					"screen_name": "ro_bot_dylan",
+					"name": "Ro Bot Dylan",
+					"id": 200272047,
+					"id_str": "200272047",
+					"indices": [
+						69,
+						82
+					]
+				},
+				{
+					"screen_name": "MagicRealismBot",
+					"name": "Magic Realism Bot",
+					"id": 3701125272,
+					"id_str": "3701125272",
+					"indices": [
+						168,
+						184
+					]
+				},
+				{
+					"screen_name": "DeathMedieval",
+					"name": "Medieval Death Bot",
+					"id": 1970799990,
+					"id_str": "1970799990",
+					"indices": [
+						185,
+						199
+					]
+				},
+				{
+					"screen_name": "TwoHeadlines",
+					"name": "Two Headlines",
+					"id": 1705052335,
+					"id_str": "1705052335",
+					"indices": [
+						201,
+						214
+					]
+				},
+				{
+					"screen_name": "ro_bot_dylan",
+					"name": "Ro Bot Dylan",
+					"id": 200272047,
+					"id_str": "200272047",
+					"indices": [
+						236,
+						249
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1091168711044726800,
+		"in_reply_to_status_id_str": "1091168711044726784",
+		"in_reply_to_user_id": 14466272,
+		"in_reply_to_user_id_str": "14466272",
+		"in_reply_to_screen_name": "joefromkenyon",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10419,
+			"friends_count": 1454,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 764,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87542,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@joefromkenyon @netnarr @NicCagePlotBot @nounjective @edtechepigrams @ro_bot_dylan We love twitter bots for #netnarr - stick around when later we make our own - I live @MagicRealismBot @DeathMedieval  @TwoHeadlines and also am a fan of @ro_bot_dylan"
+	},
+	{
 		"created_at": "Fri Feb 01 01:25:27 +0000 2019",
 		"id": 1091145470976520200,
 		"id_str": "1091145470976520192",

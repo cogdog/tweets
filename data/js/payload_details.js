591b940762b8210492e71dc85e728063a3c52892
Alan Levine <cogdogblog@gmail.com>
2019-02-03 12:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87914,
-	"created_at": "Sun Feb 03 06:15:08 +0000 2019",
+	"tweets": 87915,
+	"created_at": "Sun Feb 03 18:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 03 17:58:46 +0000 2019",
+		"id": 1092120221647806500,
+		"id_str": "1092120221647806464",
+		"full_text": "@tanbob Gulp. I'm dead.  https://t.co/XmjlM2Q8Zi https://t.co/yXtXuKcS4q",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tanbob",
+					"name": "Dr. Tannis Morgan",
+					"id": 10817782,
+					"id_str": "10817782",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XmjlM2Q8Zi",
+					"expanded_url": "http://bit.ly/ExtendMOOC",
+					"display_url": "bit.ly/ExtendMOOC",
+					"indices": [
+						25,
+						48
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1092119639058829300,
+					"id_str": "1092119639058829312",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyf9BoJUUAAh6Pn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyf9BoJUUAAh6Pn.jpg",
+					"url": "https://t.co/yXtXuKcS4q",
+					"display_url": "pic.twitter.com/yXtXuKcS4q",
+					"expanded_url": "https://twitter.com/cogdog/status/1092120221647806464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 630,
+							"h": 420,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 630,
+							"h": 420,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 630,
+							"h": 420,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092119639058829300,
+					"id_str": "1092119639058829312",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyf9BoJUUAAh6Pn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyf9BoJUUAAh6Pn.jpg",
+					"url": "https://t.co/yXtXuKcS4q",
+					"display_url": "pic.twitter.com/yXtXuKcS4q",
+					"expanded_url": "https://twitter.com/cogdog/status/1092120221647806464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 630,
+							"h": 420,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 630,
+							"h": 420,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 630,
+							"h": 420,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1092114568917053400,
+		"in_reply_to_status_id_str": "1092114568917053442",
+		"in_reply_to_user_id": 10817782,
+		"in_reply_to_user_id_str": "10817782",
+		"in_reply_to_screen_name": "tanbob",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10417,
+			"friends_count": 1453,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 765,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87564,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@tanbob Gulp. I'm dead.  https://t.co/XmjlM2Q8Zi https://t.co/yXtXuKcS4q"
+	},
+	{
 		"created_at": "Sun Feb 03 05:15:43 +0000 2019",
 		"id": 1091928195568414700,
 		"id_str": "1091928195568414720",

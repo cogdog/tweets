f5a30abf8bdf2e480b5b39eab421b47a7e024b51
Alan Levine <cogdogblog@gmail.com>
2019-02-19 11:15:27 -0600

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -205,6 +205,249 @@ Grailbird.data.tweets_2019_02 =
 			"time_zone": null,
 			"geo_enabled": false,
 			"verified": false,
+			"statuses_count": 2046,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/885340451284238336/Zxiv4NQL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/885340451284238336/Zxiv4NQL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/885339669457776641/1501704100",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Coming up at noon on https://t.co/GrDnlQce33 is the Technologist Module Episode of #ExtendmOOC Radio!\n\nWith special guest @xinli_w!\n@cogdog @greeneterry @eCampusOntario @voicEdcanada https://t.co/7ewGTryvpf"
+	},
+	{
+		"created_at": "Tue Feb 19 14:27:36 +0000 2019",
+		"id": 1097865288589213700,
+		"id_str": "1097865288589213700",
+		"full_text": "Coming up at noon on https://t.co/GrDnlQce33 is the Technologist Module Episode of #ExtendmOOC Radio!\n\nWith special guest @xinli_w!\n@cogdog @greeneterry @eCampusOntario @voicEdcanada https://t.co/7ewGTryvpf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ExtendmOOC",
+					"indices": [
+						83,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "xinli_w",
+					"name": "Xinli Wang",
+					"id": 134294732,
+					"id_str": "134294732",
+					"indices": [
+						122,
+						130
+					]
+				},
+				{
+					"screen_name": "cogdog",
+					"name": "Alan Levine",
+					"id": 740343,
+					"id_str": "740343",
+					"indices": [
+						132,
+						139
+					]
+				},
+				{
+					"screen_name": "greeneterry",
+					"name": "Terry Ready For Spring Greene",
+					"id": 212019344,
+					"id_str": "212019344",
+					"indices": [
+						140,
+						152
+					]
+				},
+				{
+					"screen_name": "eCampusOntario",
+					"name": "eCampusOntario",
+					"id": 3429881050,
+					"id_str": "3429881050",
+					"indices": [
+						153,
+						168
+					]
+				},
+				{
+					"screen_name": "voicEdcanada",
+					"name": "voicEd.ca",
+					"id": 514144436,
+					"id_str": "514144436",
+					"indices": [
+						169,
+						182
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GrDnlQce33",
+					"expanded_url": "http://voiced.ca",
+					"display_url": "voiced.ca",
+					"indices": [
+						21,
+						44
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1097865209891471400,
+					"id_str": "1097865209891471360",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/Dzxml3bXQAAUoZQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dzxml3bXQAAUoZQ.jpg",
+					"url": "https://t.co/7ewGTryvpf",
+					"display_url": "pic.twitter.com/7ewGTryvpf",
+					"expanded_url": "https://twitter.com/ontarioextend/status/1097865288589213700/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2047,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1097865209891471400,
+					"id_str": "1097865209891471360",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/Dzxml3bXQAAUoZQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dzxml3bXQAAUoZQ.jpg",
+					"url": "https://t.co/7ewGTryvpf",
+					"display_url": "pic.twitter.com/7ewGTryvpf",
+					"expanded_url": "https://twitter.com/ontarioextend/status/1097865288589213700/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2047,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 885339669457776600,
+			"id_str": "885339669457776641",
+			"name": "OntarioExtend",
+			"screen_name": "ontarioextend",
+			"location": "Toronto, Ontario",
+			"description": "Join us as we extend our skills and knowledge by curating, creating, collaborating and experimenting to enrich technology-enabled learning experiences.",
+			"url": "https://t.co/VrJt2KX1mc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VrJt2KX1mc",
+							"expanded_url": "https://extend.ecampusontario.ca",
+							"display_url": "extend.ecampusontario.ca",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 713,
+			"friends_count": 439,
+			"listed_count": 15,
+			"created_at": "Thu Jul 13 03:26:40 +0000 2017",
+			"favourites_count": 2518,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
 			"statuses_count": 2044,
 			"lang": "en",
 			"contributors_enabled": false,

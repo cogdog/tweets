a0447250f43d2804edf2a976a6819a75eae80292
Alan Levine <cogdogblog@gmail.com>
2019-03-06 07:15:13 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88212,
-	"created_at": "Wed Mar 06 12:15:10 +0000 2019",
+	"tweets": 88213,
+	"created_at": "Wed Mar 06 13:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 13:00:49 +0000 2019",
+		"id": 1103279265729179600,
+		"id_str": "1103279265729179648",
+		"full_text": "Curate lunch with us! #extendmooc open office hours, drop in talk about the Curator Module, today at noon ET https://t.co/5KTiogJhp2\n\n--\n\nCC-BY image https://t.co/FfOFbVeBSO https://t.co/FpcPjE5eEQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "extendmooc",
+					"indices": [
+						22,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5KTiogJhp2",
+					"expanded_url": "http://ecampusontario.zoom.us/my/cogdog",
+					"display_url": "ecampusontario.zoom.us/my/cogdog",
+					"indices": [
+						109,
+						132
+					]
+				},
+				{
+					"url": "https://t.co/FfOFbVeBSO",
+					"expanded_url": "https://flickr.com/photos/buzzymelibee/8539742484",
+					"display_url": "flickr.com/photos/buzzyme…",
+					"indices": [
+						150,
+						173
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1103278933385101300,
+					"id_str": "1103278933385101312",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/D0-iWDCXQAAfpLI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0-iWDCXQAAfpLI.jpg",
+					"url": "https://t.co/FpcPjE5eEQ",
+					"display_url": "pic.twitter.com/FpcPjE5eEQ",
+					"expanded_url": "https://twitter.com/cogdog/status/1103279265729179648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 514,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 514,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 514,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103278933385101300,
+					"id_str": "1103278933385101312",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/D0-iWDCXQAAfpLI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0-iWDCXQAAfpLI.jpg",
+					"url": "https://t.co/FpcPjE5eEQ",
+					"display_url": "pic.twitter.com/FpcPjE5eEQ",
+					"expanded_url": "https://twitter.com/cogdog/status/1103279265729179648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 514,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 514,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 514,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10419,
+			"friends_count": 1462,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 771,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87860,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Curate lunch with us! #extendmooc open office hours, drop in talk about the Curator Module, today at noon ET https://t.co/5KTiogJhp2\n\n--\n\nCC-BY image https://t.co/FfOFbVeBSO https://t.co/FpcPjE5eEQ"
+	},
+	{
 		"created_at": "Wed Mar 06 12:12:44 +0000 2019",
 		"id": 1103267164197404700,
 		"id_str": "1103267164197404673",

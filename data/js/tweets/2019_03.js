994d2502d4436528d164e54926a32160c19bbb68
Alan Levine <cogdogblog@gmail.com>
2019-03-22 17:15:11 -0600

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 22 22:21:30 +0000 2019",
+		"id": 1109218572046385200,
+		"id_str": "1109218572046385152",
+		"full_text": "I don't know how they did it, but each cartoon gets more scathingly funny (and terrifying) as you scroll down.\n\nWhat human beings actually can support this #fakepresident https://t.co/OpjvqsxOHv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "fakepresident",
+					"indices": [
+						156,
+						170
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OpjvqsxOHv",
+					"expanded_url": "https://twitter.com/GuyKawasaki/status/1109216238411104256",
+					"display_url": "twitter.com/GuyKawasaki/st…",
+					"indices": [
+						171,
+						194
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10434,
+			"friends_count": 1470,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 775,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88023,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1109216238411104300,
+		"quoted_status_id_str": "1109216238411104256",
+		"quoted_status_permalink": {
+			"url": "https://t.co/OpjvqsxOHv",
+			"expanded": "https://twitter.com/GuyKawasaki/status/1109216238411104256",
+			"display": "twitter.com/GuyKawasaki/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Mar 22 22:12:14 +0000 2019",
+			"id": 1109216238411104300,
+			"id_str": "1109216238411104256",
+			"full_text": "5 scathing cartoons about Trump’s never-ending feud with John McCain https://t.co/uptABIQODW",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				92
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/uptABIQODW",
+						"expanded_url": "https://theweek.com/articles/830463/5-scathing-cartoons-about-trumps-neverending-feud-john-mccain",
+						"display_url": "theweek.com/articles/83046…",
+						"indices": [
+							69,
+							92
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 8453452,
+				"id_str": "8453452",
+				"name": "Guy Kawasaki",
+				"screen_name": "GuyKawasaki",
+				"location": "Silicon Valley, California",
+				"description": "Chief evangelist @Canva; board of directors @Cheeze; and brand ambassador @MercedesBenz",
+				"url": "https://t.co/2K4jQp9hVt",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/2K4jQp9hVt",
+								"expanded_url": "http://guykawasaki.com",
+								"display_url": "guykawasaki.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1439478,
+				"friends_count": 294,
+				"listed_count": 39783,
+				"created_at": "Mon Aug 27 03:36:53 +0000 2007",
+				"favourites_count": 883,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 190375,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/791041568006348801/_4Bbilhv_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/791041568006348801/_4Bbilhv_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/8453452/1537815888",
+				"profile_link_color": "1EB9CB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FCFCFC",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I don't know how they did it, but each cartoon gets more scathingly funny (and terrifying) as you scroll down.\n\nWhat human beings actually can support this #fakepresident https://t.co/OpjvqsxOHv"
+	},
+	{
 		"created_at": "Fri Mar 22 22:15:06 +0000 2019",
 		"id": 1109216959995043800,
 		"id_str": "1109216959995043840",

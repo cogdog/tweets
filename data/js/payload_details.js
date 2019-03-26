014a00a63d0b0ce99c5f2c617b46b41be41bbbb4
Alan Levine <cogdogblog@gmail.com>
2019-03-26 09:15:15 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88356,
-	"created_at": "Tue Mar 26 14:15:14 +0000 2019",
+	"tweets": 88357,
+	"created_at": "Tue Mar 26 15:15:13 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 26 14:21:14 +0000 2019",
+		"id": 1110547260247560200,
+		"id_str": "1110547260247560194",
+		"full_text": "The GOP pulled out a key ACA jenga block by removing the individual mandate, now a judge topples the whole thing. Cleverly diabolical. \n\nLaughing ensues as they can then give more tax cuts to the rich and play with walls. Everything falls for 20 million ACA insured [voters] https://t.co/35Lme6SHn0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1110546716661563400,
+					"id_str": "1110546716661563392",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D2l0WtkWwAA4f59.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D2l0WtkWwAA4f59.jpg",
+					"url": "https://t.co/35Lme6SHn0",
+					"display_url": "pic.twitter.com/35Lme6SHn0",
+					"expanded_url": "https://twitter.com/cogdog/status/1110547260247560194/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 250,
+							"h": 250,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 250,
+							"h": 250,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 250,
+							"h": 250,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1110546716661563400,
+					"id_str": "1110546716661563392",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D2l0WtkWwAA4f59.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D2l0WtkWwAA4f59.jpg",
+					"url": "https://t.co/35Lme6SHn0",
+					"display_url": "pic.twitter.com/35Lme6SHn0",
+					"expanded_url": "https://twitter.com/cogdog/status/1110547260247560194/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 250,
+							"h": 250,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 250,
+							"h": 250,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 250,
+							"h": 250,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D2l0WtkWwAA4f59.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10439,
+			"friends_count": 1470,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 776,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88054,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The GOP pulled out a key ACA jenga block by removing the individual mandate, now a judge topples the whole thing. Cleverly diabolical. \n\nLaughing ensues as they can then give more tax cuts to the rich and play with walls. Everything falls for 20 million ACA insured [voters] https://t.co/35Lme6SHn0"
+	},
+	{
 		"created_at": "Tue Mar 26 14:06:22 +0000 2019",
 		"id": 1110543518823518200,
 		"id_str": "1110543518823518208",

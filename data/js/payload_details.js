4f7d2008c98f47e07721d881b4e13421f9416bc5
Alan Levine <cogdogblog@gmail.com>
2019-04-04 10:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88456,
-	"created_at": "Thu Apr 04 14:15:09 +0000 2019",
+	"tweets": 88457,
+	"created_at": "Thu Apr 04 16:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,257 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 04 15:31:09 +0000 2019",
+		"id": 1113826345702887400,
+		"id_str": "1113826345702887427",
+		"full_text": "Very timely for #netnarr topic this week https://t.co/cqFBprY8DA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						16,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cqFBprY8DA",
+					"expanded_url": "https://twitter.com/CommonSense/status/1113794648508649472",
+					"display_url": "twitter.com/CommonSense/st…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10440,
+			"friends_count": 1471,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 776,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88186,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1113794648508649500,
+		"quoted_status_id_str": "1113794648508649472",
+		"quoted_status_permalink": {
+			"url": "https://t.co/cqFBprY8DA",
+			"expanded": "https://twitter.com/CommonSense/status/1113794648508649472",
+			"display": "twitter.com/CommonSense/st…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 04 13:25:12 +0000 2019",
+			"id": 1113794648508649500,
+			"id_str": "1113794648508649472",
+			"full_text": "Today, we'll be live-tweeting our conference Truth About Tech: Solutions for Digital Well-Being. Stay tuned for all the updates!  You can also watch the Livestream here: https://t.co/WpNxqKVaC8 #digitalwellbeing #DWB19",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				218
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "digitalwellbeing",
+						"indices": [
+							194,
+							211
+						]
+					},
+					{
+						"text": "DWB19",
+						"indices": [
+							212,
+							218
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/WpNxqKVaC8",
+						"expanded_url": "https://comsen.se/2Flbx7Q",
+						"display_url": "comsen.se/2Flbx7Q",
+						"indices": [
+							170,
+							193
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 29549839,
+				"id_str": "29549839",
+				"name": "Common Sense Media",
+				"screen_name": "CommonSense",
+				"location": "San Francisco, CA",
+				"description": "Nat’l nonprofit helping families thrive in our 24/7 world of media and tech. Media ratings/reviews, parenting advice, privacy + tech policy. #digitalwellbeing",
+				"url": "https://t.co/J9ejvbcyGd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/J9ejvbcyGd",
+								"expanded_url": "http://www.commonsense.org",
+								"display_url": "commonsense.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 103542,
+				"friends_count": 7076,
+				"listed_count": 2264,
+				"created_at": "Tue Apr 07 21:26:26 +0000 2009",
+				"favourites_count": 15370,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 25975,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/936310245315375104/Ye32osut_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/936310245315375104/Ye32osut_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/29549839/1550803049",
+				"profile_link_color": "14A153",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E3E2DE",
+				"profile_text_color": "634047",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Very timely for #netnarr topic this week https://t.co/cqFBprY8DA"
+	},
+	{
 		"created_at": "Thu Apr 04 14:15:05 +0000 2019",
 		"id": 1113807203322998800,
 		"id_str": "1113807203322998785",

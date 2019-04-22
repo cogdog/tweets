0ac2cf5f7bbc1505c49b0ead63063409adcac786
Alan Levine <cogdogblog@gmail.com>
2019-04-22 13:15:14 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88604,
-	"created_at": "Mon Apr 22 06:15:10 +0000 2019",
+	"tweets": 88605,
+	"created_at": "Mon Apr 22 19:15:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 22 18:40:38 +0000 2019",
+		"id": 1120396859997151200,
+		"id_str": "1120397010962649088",
+		"full_text": "Soylent: I can't trust you with CSS layouts, let alone my body's nutrition https://t.co/hkzRjpNMJf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1120396858050871300,
+					"id_str": "1120396858050871299",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/D4xzAZuWAAMs0XY.png",
+					"media_url_https": "https://pbs.twimg.com/media/D4xzAZuWAAMs0XY.png",
+					"url": "https://t.co/hkzRjpNMJf",
+					"display_url": "pic.twitter.com/hkzRjpNMJf",
+					"expanded_url": "https://twitter.com/iamdevloper/status/1120396859997151233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 212,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 374,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 638,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1120396858050871300,
+					"id_str": "1120396858050871299",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/D4xzAZuWAAMs0XY.png",
+					"media_url_https": "https://pbs.twimg.com/media/D4xzAZuWAAMs0XY.png",
+					"url": "https://t.co/hkzRjpNMJf",
+					"display_url": "pic.twitter.com/hkzRjpNMJf",
+					"expanded_url": "https://twitter.com/iamdevloper/status/1120396859997151233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 212,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 374,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 638,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 564919357,
+			"id_str": "564919357",
+			"name": "I Am Devloper",
+			"screen_name": "iamdevloper",
+			"location": "Localhost",
+			"description": "The Vista of Twitter accounts  adeveloper1@gmail.com | DM | Merch: https://t.co/Fv5n2yAXPS | 💰 in tweet = ad",
+			"url": "https://t.co/Fv5n2yAXPS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Fv5n2yAXPS",
+							"expanded_url": "https://teespring.com/en-GB/stores/iamdevloper",
+							"display_url": "teespring.com/en-GB/stores/i…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Fv5n2yAXPS",
+							"expanded_url": "https://teespring.com/en-GB/stores/iamdevloper",
+							"display_url": "teespring.com/en-GB/stores/i…",
+							"indices": [
+								67,
+								90
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 353749,
+			"friends_count": 0,
+			"listed_count": 4274,
+			"created_at": "Fri Apr 27 21:06:51 +0000 2012",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5297,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1071055431215276033/U9-RIlDs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1071055431215276033/U9-RIlDs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/564919357/1543573660",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 76,
+		"favorite_count": 376,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Soylent: I can't trust you with CSS layouts, let alone my body's nutrition https://t.co/hkzRjpNMJf"
+	},
+	{
 		"created_at": "Mon Apr 22 06:02:04 +0000 2019",
 		"id": 1120206112698064900,
 		"id_str": "1120206112698064896",

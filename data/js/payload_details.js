0c6d87d3afa67935282b55930ff8eb8cd7d48bf9
Alan Levine <cogdogblog@gmail.com>
2019-05-01 10:15:13 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88652,
-	"created_at": "Wed May 01 15:15:11 +0000 2019",
+	"tweets": 88653,
+	"created_at": "Wed May 01 16:15:11 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 01 15:52:34 +0000 2019",
+		"id": 1123616207016464400,
+		"id_str": "1123616207016464385",
+		"full_text": "First hearing about @ontarioextend  Woah! #oext344 #ExtendmOOC @TLConestoga #TechforTeaching https://t.co/8d5RlYldWO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "oext344",
+					"indices": [
+						42,
+						50
+					]
+				},
+				{
+					"text": "ExtendmOOC",
+					"indices": [
+						51,
+						62
+					]
+				},
+				{
+					"text": "TechforTeaching",
+					"indices": [
+						76,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ontarioextend",
+					"name": "OntarioExtend",
+					"id": 885339669457776600,
+					"id_str": "885339669457776641",
+					"indices": [
+						20,
+						34
+					]
+				},
+				{
+					"screen_name": "TLConestoga",
+					"name": "Teaching and Learning Conestoga",
+					"id": 951156227224096800,
+					"id_str": "951156227224096768",
+					"indices": [
+						63,
+						75
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123616175974436900,
+					"id_str": "1123616175974436864",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5fi9XjW0AAySGa.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5fi9XjW0AAySGa.jpg",
+					"url": "https://t.co/8d5RlYldWO",
+					"display_url": "pic.twitter.com/8d5RlYldWO",
+					"expanded_url": "https://twitter.com/cogdog/status/1123616207016464385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 500,
+							"h": 214,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 500,
+							"h": 214,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 214,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123616175974436900,
+					"id_str": "1123616175974436864",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5fi9XjW0AAySGa.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5fi9XjW0AAySGa.jpg",
+					"url": "https://t.co/8d5RlYldWO",
+					"display_url": "pic.twitter.com/8d5RlYldWO",
+					"expanded_url": "https://twitter.com/cogdog/status/1123616207016464385/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 500,
+							"h": 214,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 500,
+							"h": 214,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 214,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							250,
+							107
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D5fi9XjW0AAySGa.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10459,
+			"friends_count": 1475,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 779,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88442,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "First hearing about @ontarioextend  Woah! #oext344 #ExtendmOOC @TLConestoga #TechforTeaching https://t.co/8d5RlYldWO"
+	},
+	{
 		"created_at": "Wed May 01 14:27:51 +0000 2019",
 		"id": 1123594886723330000,
 		"id_str": "1123594886723330052",

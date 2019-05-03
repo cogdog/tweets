d35ae7399b719500be23ecba21d854b31a0b2d6a
Alan Levine <cogdogblog@gmail.com>
2019-05-03 08:15:15 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88676,
-	"created_at": "Fri May 03 04:15:09 +0000 2019",
+	"tweets": 88677,
+	"created_at": "Fri May 03 14:15:13 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 14:13:09 +0000 2019",
+		"id": 1124315965607567400,
+		"id_str": "1124315965607567361",
+		"full_text": "@vladaslaughter I feel great! You? #netnarr https://t.co/8D6XcBcsz8",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			43
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						35,
+						43
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "vladaslaughter",
+					"name": "vlada slaughter ❣️",
+					"id": 1112524799568478200,
+					"id_str": "1112524799568478208",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1124315924637671400,
+					"id_str": "1124315924637671424",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5pfYGdX4AA8yB-.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5pfYGdX4AA8yB-.jpg",
+					"url": "https://t.co/8D6XcBcsz8",
+					"display_url": "pic.twitter.com/8D6XcBcsz8",
+					"expanded_url": "https://twitter.com/cogdog/status/1124315965607567361/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 198,
+							"h": 100,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 198,
+							"h": 100,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 100,
+							"h": 100,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 198,
+							"h": 100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124315924637671400,
+					"id_str": "1124315924637671424",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5pfYGdX4AA8yB-.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5pfYGdX4AA8yB-.jpg",
+					"url": "https://t.co/8D6XcBcsz8",
+					"display_url": "pic.twitter.com/8D6XcBcsz8",
+					"expanded_url": "https://twitter.com/cogdog/status/1124315965607567361/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 198,
+							"h": 100,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 198,
+							"h": 100,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 100,
+							"h": 100,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 198,
+							"h": 100,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							99,
+							50
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D5pfYGdX4AA8yB-.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1124287900210946000,
+		"in_reply_to_status_id_str": "1124287900210946050",
+		"in_reply_to_user_id": 1112524799568478200,
+		"in_reply_to_user_id_str": "1112524799568478208",
+		"in_reply_to_screen_name": "vladaslaughter",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10460,
+			"friends_count": 1474,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 780,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88470,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@vladaslaughter I feel great! You? #netnarr https://t.co/8D6XcBcsz8"
+	},
+	{
 		"created_at": "Fri May 03 04:01:03 +0000 2019",
 		"id": 1124161922935472100,
 		"id_str": "1124161922935472128",

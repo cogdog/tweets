6103e6b34cbdade7e420e636413999d7951b15d8
Alan Levine <cogdogblog@gmail.com>
2019-05-05 10:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88681,
-	"created_at": "Sat May 04 01:15:08 +0000 2019",
+	"tweets": 88682,
+	"created_at": "Sun May 05 16:15:08 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 05 15:46:46 +0000 2019",
+		"id": 1125064300320755700,
+		"id_str": "1125064300320755714",
+		"full_text": "@gotanda @lesleyito @michaelegriffin That’s my site; though https://t.co/8mMTcJPbMI not a random *conference theme* generator, it makes up names for massive online open courses. And the Anita Bryant image is not part of it, but from a side link to a more general collection of sarcastic blog posts.",
+		"truncated": false,
+		"display_text_range": [
+			37,
+			298
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gotanda",
+					"name": "Ted O'Neill 令和",
+					"id": 9875912,
+					"id_str": "9875912",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "lesleyito",
+					"name": "Lesley Ito",
+					"id": 45095562,
+					"id_str": "45095562",
+					"indices": [
+						9,
+						19
+					]
+				},
+				{
+					"screen_name": "michaelegriffin",
+					"name": "Michael Griffin",
+					"id": 394053348,
+					"id_str": "394053348",
+					"indices": [
+						20,
+						36
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8mMTcJPbMI",
+					"expanded_url": "http://mooc.cogdogblog.com",
+					"display_url": "mooc.cogdogblog.com",
+					"indices": [
+						60,
+						83
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.echofon.com/\" rel=\"nofollow\">Echofon</a>",
+		"in_reply_to_status_id": 1124929011526713300,
+		"in_reply_to_status_id_str": "1124929011526713345",
+		"in_reply_to_user_id": 9875912,
+		"in_reply_to_user_id_str": "9875912",
+		"in_reply_to_screen_name": "gotanda",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10460,
+			"friends_count": 1475,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 780,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88476,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@gotanda @lesleyito @michaelegriffin That’s my site; though https://t.co/8mMTcJPbMI not a random *conference theme* generator, it makes up names for massive online open courses. And the Anita Bryant image is not part of it, but from a side link to a more general collection of sarcastic blog posts."
+	},
+	{
 		"created_at": "Sat May 04 00:35:37 +0000 2019",
 		"id": 1124472615018606600,
 		"id_str": "1124472615018606592",

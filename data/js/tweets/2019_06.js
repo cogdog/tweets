f288942c2a17e095c6f87033e3288167dff11dc2
Alan Levine <cogdogblog@gmail.com>
2019-06-25 11:15:11 -0700

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 25 18:01:54 +0000 2019",
+		"id": 1143580090153996300,
+		"id_str": "1143580090153996288",
+		"full_text": "\"To Google Man\"\n\n\"Please, Mr Chambers, search. We wouldn't want you to lose knowledge\" https://t.co/DtmNWxMe4G",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DtmNWxMe4G",
+					"expanded_url": "https://twitter.com/TheNightGallery/status/1143579612972118016",
+					"display_url": "twitter.com/TheNightGaller…",
+					"indices": [
+						87,
+						110
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10482,
+			"friends_count": 1480,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 789,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88675,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1143579612972118000,
+		"quoted_status_id_str": "1143579612972118016",
+		"quoted_status_permalink": {
+			"url": "https://t.co/DtmNWxMe4G",
+			"expanded": "https://twitter.com/TheNightGallery/status/1143579612972118016",
+			"display": "twitter.com/TheNightGaller…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jun 25 18:00:01 +0000 2019",
+			"id": 1143579612972118000,
+			"id_str": "1143579612972118016",
+			"full_text": "\"Please, Mr. Chambers, eat. We wouldn't want you to lose weight.\" \n\n#ZoneQuotes #S3E24 \n\n\"To Serve Man\" by Rod Serling stars Lloyd Bochner and Richard Kiel. https://t.co/5DpfcQTZNc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				156
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "ZoneQuotes",
+						"indices": [
+							68,
+							79
+						]
+					},
+					{
+						"text": "S3E24",
+						"indices": [
+							80,
+							86
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1143574675701866500,
+						"id_str": "1143574675701866496",
+						"indices": [
+							157,
+							180
+						],
+						"media_url": "http://pbs.twimg.com/media/D97LG89XoAA-xvv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D97LG89XoAA-xvv.jpg",
+						"url": "https://t.co/5DpfcQTZNc",
+						"display_url": "pic.twitter.com/5DpfcQTZNc",
+						"expanded_url": "https://twitter.com/TheNightGallery/status/1143579612972118016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1008,
+								"h": 768,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1008,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 518,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1143574675701866500,
+						"id_str": "1143574675701866496",
+						"indices": [
+							157,
+							180
+						],
+						"media_url": "http://pbs.twimg.com/media/D97LG89XoAA-xvv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D97LG89XoAA-xvv.jpg",
+						"url": "https://t.co/5DpfcQTZNc",
+						"display_url": "pic.twitter.com/5DpfcQTZNc",
+						"expanded_url": "https://twitter.com/TheNightGallery/status/1143579612972118016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1008,
+								"h": 768,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1008,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 518,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 190403384,
+				"id_str": "190403384",
+				"name": "The Twilight Zone",
+				"screen_name": "TheNightGallery",
+				"location": "A corner of the fifth dimension near Baltimore, MD",
+				"description": "Fan of the Twilight Zone? No need to wait for a marathon. Join me (Paul) for daily quotes and facts from TZ, Night Gallery, and Rod Serling's many other works.",
+				"url": "https://t.co/eJdaA1wl4G",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/eJdaA1wl4G",
+								"expanded_url": "http://thenightgallery.wordpress.com/",
+								"display_url": "thenightgallery.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 28879,
+				"friends_count": 1954,
+				"listed_count": 365,
+				"created_at": "Mon Sep 13 21:34:06 +0000 2010",
+				"favourites_count": 20972,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 64946,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "352726",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/471378910114889728/ZoYJ17X-_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/471378910114889728/ZoYJ17X-_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/190403384/1466395060",
+				"profile_link_color": "DE0B0B",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "47C1D1",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 50,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"To Google Man\"\n\n\"Please, Mr Chambers, search. We wouldn't want you to lose knowledge\" https://t.co/DtmNWxMe4G"
+	},
+	{
 		"created_at": "Tue Jun 25 16:32:37 +0000 2019",
 		"id": 1143557621250777100,
 		"id_str": "1143557621250777088",

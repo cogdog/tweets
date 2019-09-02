910a5b5c9f7e9e8878bd4bf5c5d3b8e3344f9823
Alan Levine <cogdogblog@gmail.com>
2019-09-02 02:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89055,
-	"created_at": "Mon Sep 02 04:15:09 +0000 2019",
+	"tweets": 89056,
+	"created_at": "Mon Sep 02 08:15:11 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 02 07:17:15 +0000 2019",
+		"id": 1168422619382714400,
+		"id_str": "1168422619382714368",
+		"full_text": "@tara @znmeb @anildash @hrheingold Give this a try https://t.co/5tT9pBlWc7",
+		"truncated": false,
+		"display_text_range": [
+			35,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tara",
+					"name": "Tara Tiger Brown Ⓥ 🌲",
+					"id": 10959642,
+					"id_str": "10959642",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "znmeb",
+					"name": "Zorro Notorious M. E. B. he / him",
+					"id": 741803,
+					"id_str": "741803",
+					"indices": [
+						6,
+						12
+					]
+				},
+				{
+					"screen_name": "anildash",
+					"name": "Anil Dash 🥭",
+					"id": 36823,
+					"id_str": "36823",
+					"indices": [
+						13,
+						22
+					]
+				},
+				{
+					"screen_name": "hrheingold",
+					"name": "(((Howard Rheingold)))",
+					"id": 5388852,
+					"id_str": "5388852",
+					"indices": [
+						23,
+						34
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5tT9pBlWc7",
+					"expanded_url": "https://www.pipes.digital",
+					"display_url": "pipes.digital",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.echofon.com/\" rel=\"nofollow\">Echofon</a>",
+		"in_reply_to_status_id": 1168332315988127700,
+		"in_reply_to_status_id_str": "1168332315988127744",
+		"in_reply_to_user_id": 10959642,
+		"in_reply_to_user_id_str": "10959642",
+		"in_reply_to_screen_name": "tara",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10471,
+			"friends_count": 1479,
+			"listed_count": 785,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 794,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 88954,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@tara @znmeb @anildash @hrheingold Give this a try https://t.co/5tT9pBlWc7"
+	},
+	{
 		"created_at": "Mon Sep 02 03:44:52 +0000 2019",
 		"id": 1168369173061341200,
 		"id_str": "1168369173061341184",

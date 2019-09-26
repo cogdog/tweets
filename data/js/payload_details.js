d04d863173b0d578d3d1e2faa62607285e41c303
Alan Levine <cogdogblog@gmail.com>
2019-09-26 13:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89208,
-	"created_at": "Thu Sep 26 17:15:10 +0000 2019",
+	"tweets": 89209,
+	"created_at": "Thu Sep 26 19:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 26 18:33:38 +0000 2019",
+		"id": 1177279669411954700,
+		"id_str": "1177290144858202112",
+		"full_text": "BuzzFeed News: This Website Will Turn Wikipedia Articles Into \"Real\" Academic Papers - https://t.co/yFOWGan9gD (This article is why RBF now has an \"I don't know whether to laugh or throw things\" tag.)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			200
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yFOWGan9gD",
+					"expanded_url": "https://buff.ly/2lF7Nbg",
+					"display_url": "buff.ly/2lF7Nbg",
+					"indices": [
+						87,
+						110
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6979102,
+			"id_str": "6979102",
+			"name": "Tara Calishain",
+			"screen_name": "ResearchBuzz",
+			"location": "",
+			"description": "Obsessed with search engines, info collections, and databases since forever. News and resources covering same since 1998. Wrote a stack of books. Say hi.",
+			"url": "http://t.co/N8gqhQKHJ5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/N8gqhQKHJ5",
+							"expanded_url": "http://www.researchbuzz.com",
+							"display_url": "researchbuzz.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5468,
+			"friends_count": 3355,
+			"listed_count": 599,
+			"created_at": "Wed Jun 20 22:25:01 +0000 2007",
+			"favourites_count": 4425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 52516,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/572069625517453312/0zTcuSnw_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/572069625517453312/0zTcuSnw_normal.png",
+			"profile_link_color": "243A8F",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BuzzFeed News: This Website Will Turn Wikipedia Articles Into \"Real\" Academic Papers - https://t.co/yFOWGan9gD (This article is why RBF now has an \"I don't know whether to laugh or throw things\" tag.)"
+	},
+	{
 		"created_at": "Thu Sep 26 17:03:43 +0000 2019",
 		"id": 1177267518148423700,
 		"id_str": "1177267518148423687",

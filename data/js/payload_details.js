cf980d33f6ef45c9eddf1b1dd43adb62a2718868
Alan Levine <cogdogblog@gmail.com>
2019-10-21 16:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89369,
-	"created_at": "Mon Oct 21 21:15:10 +0000 2019",
+	"tweets": 89370,
+	"created_at": "Mon Oct 21 22:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Mon Oct 21 21:23:27 +0000 2019",
+		"id": 1186392577043509200,
+		"id_str": "1186392577043509249",
+		"full_text": "Best littlest online conference— 25th year, that says a lot #tccsilver https://t.co/7pYa7xWyXR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tccsilver",
+					"indices": [
+						60,
+						70
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7pYa7xWyXR",
+					"expanded_url": "https://twitter.com/kimubert/status/1186287931465486336",
+					"display_url": "twitter.com/kimubert/statu…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10486,
+			"friends_count": 1484,
+			"listed_count": 785,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 802,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 89375,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1186287931465486300,
+		"quoted_status_id_str": "1186287931465486336",
+		"quoted_status_permalink": {
+			"url": "https://t.co/7pYa7xWyXR",
+			"expanded": "https://twitter.com/kimubert/status/1186287931465486336",
+			"display": "twitter.com/kimubert/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Oct 21 14:27:37 +0000 2019",
+			"id": 1186287931465486300,
+			"id_str": "1186287931465486336",
+			"full_text": "Join us! TCC 2020@25: Vision 2020. April 14-16. Call for Proposals https://t.co/mQr12U3QwS #tccsilver #tcccfp https://t.co/NX5YHA10GH",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				109
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "tccsilver",
+						"indices": [
+							91,
+							101
+						]
+					},
+					{
+						"text": "tcccfp",
+						"indices": [
+							102,
+							109
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mQr12U3QwS",
+						"expanded_url": "http://tcchawaii.org/call-for-proposals-2020",
+						"display_url": "tcchawaii.org/call-for-propo…",
+						"indices": [
+							67,
+							90
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1186287913610272800,
+						"id_str": "1186287913610272768",
+						"indices": [
+							110,
+							133
+						],
+						"media_url": "http://pbs.twimg.com/media/EHaKknwUYAA1PyM.png",
+						"media_url_https": "https://pbs.twimg.com/media/EHaKknwUYAA1PyM.png",
+						"url": "https://t.co/NX5YHA10GH",
+						"display_url": "pic.twitter.com/NX5YHA10GH",
+						"expanded_url": "https://twitter.com/kimubert/status/1186287931465486336/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 560,
+								"h": 315,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 560,
+								"h": 315,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 560,
+								"h": 315,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1186287913610272800,
+						"id_str": "1186287913610272768",
+						"indices": [
+							110,
+							133
+						],
+						"media_url": "http://pbs.twimg.com/media/EHaKknwUYAA1PyM.png",
+						"media_url_https": "https://pbs.twimg.com/media/EHaKknwUYAA1PyM.png",
+						"url": "https://t.co/NX5YHA10GH",
+						"display_url": "pic.twitter.com/NX5YHA10GH",
+						"expanded_url": "https://twitter.com/kimubert/status/1186287931465486336/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 560,
+								"h": 315,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 560,
+								"h": 315,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 560,
+								"h": 315,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 5800082,
+				"id_str": "5800082",
+				"name": "Bert Kimura",
+				"screen_name": "kimubert",
+				"location": "Japan & Hawaii",
+				"description": "I tweet, I learn. Curious about nature. TCC Online Conference coordinator. University of Hawaii, Kansai University faculty. Ukulele cat by Cane Haul Road.",
+				"url": "http://t.co/ZU81dj0sDN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/ZU81dj0sDN",
+								"expanded_url": "http://about.me/kimubert",
+								"display_url": "about.me/kimubert",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 683,
+				"friends_count": 1091,
+				"listed_count": 65,
+				"created_at": "Sun May 06 03:37:30 +0000 2007",
+				"favourites_count": 859,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6933,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/27006502/ukelele_cat_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/27006502/ukelele_cat_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/5800082/1411376449",
+				"profile_link_color": "19CF86",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Best littlest online conference— 25th year, that says a lot #tccsilver https://t.co/7pYa7xWyXR"
+	},
+	{
 		"created_at": "Mon Oct 21 20:34:21 +0000 2019",
 		"id": 1186380220233932800,
 		"id_str": "1186380220233932800",

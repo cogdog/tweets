1e11d3b9ef82415e70c0c25a3cac14e4c597d887
Alan Levine <cogdogblog@gmail.com>
2019-11-22 10:15:14 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89578,
-	"created_at": "Fri Nov 22 02:15:10 +0000 2019",
+	"tweets": 89579,
+	"created_at": "Fri Nov 22 16:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 15:47:04 +0000 2019",
+		"id": 1197904336069611500,
+		"id_str": "1197904336069611525",
+		"full_text": "I'm not even going to bother trying WordPress plugins that use the words \"upgrade\" / \"pro features\" - they list crippled useless versions to get in the plugin directory. It's WP-Upsell at its worst.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10505,
+			"friends_count": 1497,
+			"listed_count": 784,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 811,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 89640,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "I'm not even going to bother trying WordPress plugins that use the words \"upgrade\" / \"pro features\" - they list crippled useless versions to get in the plugin directory. It's WP-Upsell at its worst."
+	},
+	{
 		"created_at": "Fri Nov 22 02:12:03 +0000 2019",
 		"id": 1197699233060728800,
 		"id_str": "1197699233060728832",
@@ -155,7 +262,7 @@ Grailbird.data.tweets_2019_11 =
 				"name": "Jeffrey Luscombe",
 				"screen_name": "JeffreyLuscombe",
 				"location": "Toronto",
-				"description": "Author of SHIRTS AND SKINS. Centrist. Atheist. Existentialist. Married. Gay. Ojibwe ancestry. New novel, TO REFRAIN FROM EMBRACING, coming 2020 🇨🇦🏳️‍🌈",
+				"description": "Author of SHIRTS AND SKINS. Centrist. Atheist. Existentialist. Married. Gay. Ojibwe ancestry. New novel, TO REFRAIN FROM EMBRACING, coming 2020 🇨🇦🏳️🌈",
 				"url": "https://t.co/7mswi9EEn2",
 				"entities": {
 					"url": {

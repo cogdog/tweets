0ae4f3586a3255dfde6d833784f66d32090871a4
Alan Levine <cogdogblog@gmail.com>
2019-11-22 20:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89584,
-	"created_at": "Sat Nov 23 01:15:11 +0000 2019",
+	"tweets": 89585,
+	"created_at": "Sat Nov 23 02:15:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,165 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 23 01:17:08 +0000 2019",
+		"id": 1198047799318741000,
+		"id_str": "1198047799318740992",
+		"full_text": "@infology @brlamb @supernova_k @scottmcmillan Wonderful news and well deserved recognition for Will's openness work over the long haul. Had a chance to chat with him about Wiki Gardening in 2014 https://t.co/cGpO0pTaWp",
+		"truncated": false,
+		"display_text_range": [
+			46,
+			218
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "infology",
+					"name": "Wɪʟʟ Eɴɢʟᴇ",
+					"id": 93031146,
+					"id_str": "93031146",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "brlamb",
+					"name": "Brian Lamb",
+					"id": 745903,
+					"id_str": "745903",
+					"indices": [
+						10,
+						17
+					]
+				},
+				{
+					"screen_name": "supernova_k",
+					"name": "novak rogic",
+					"id": 3446221,
+					"id_str": "3446221",
+					"indices": [
+						18,
+						30
+					]
+				},
+				{
+					"screen_name": "scottmcmillan",
+					"name": "Scott McMillan",
+					"id": 18662724,
+					"id_str": "18662724",
+					"indices": [
+						31,
+						45
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/cGpO0pTaWp",
+					"expanded_url": "https://cogdogblog.com/2014/01/an-interview-with-the-wiki-gardener/",
+					"display_url": "cogdogblog.com/2014/01/an-int…",
+					"indices": [
+						195,
+						218
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1198036801115418600,
+		"in_reply_to_status_id_str": "1198036801115418625",
+		"in_reply_to_user_id": 93031146,
+		"in_reply_to_user_id_str": "93031146",
+		"in_reply_to_screen_name": "infology",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10506,
+			"friends_count": 1497,
+			"listed_count": 784,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 811,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 89648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@infology @brlamb @supernova_k @scottmcmillan Wonderful news and well deserved recognition for Will's openness work over the long haul. Had a chance to chat with him about Wiki Gardening in 2014 https://t.co/cGpO0pTaWp"
+	},
+	{
 		"created_at": "Sat Nov 23 01:03:19 +0000 2019",
 		"id": 1198044322785980400,
 		"id_str": "1198044322785980416",

7b3bbc3e40ebc785e3abe31ad2944f23540fa74f
Alan Levine <cogdogblog@gmail.com>
2019-11-29 09:15:12 -0600

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,345 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 29 15:10:30 +0000 2019",
+		"id": 1200431848158982100,
+		"id_str": "1200431848158982144",
+		"full_text": "@mhawksey @BryanAlexander Hello two great friends meeting! Martin deserves an honorary beard. https://t.co/WplzqgnFmi",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mhawksey",
+					"name": "Martin Hawksey #altc",
+					"id": 13046992,
+					"id_str": "13046992",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "BryanAlexander",
+					"name": "Bryan Alexander",
+					"id": 755991,
+					"id_str": "755991",
+					"indices": [
+						10,
+						25
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200431711672127500,
+					"id_str": "1200431711672127488",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EKjKSBdU0AADWcb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKjKSBdU0AADWcb.jpg",
+					"url": "https://t.co/WplzqgnFmi",
+					"display_url": "pic.twitter.com/WplzqgnFmi",
+					"expanded_url": "https://twitter.com/cogdog/status/1200431848158982144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 502,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 885,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1511,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 417,
+									"y": 229,
+									"h": 67,
+									"w": 67
+								},
+								{
+									"x": 47,
+									"y": 118,
+									"h": 264,
+									"w": 264
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1258,
+									"y": 692,
+									"h": 204,
+									"w": 204
+								},
+								{
+									"x": 142,
+									"y": 356,
+									"h": 796,
+									"w": 796
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 737,
+									"y": 405,
+									"h": 119,
+									"w": 119
+								},
+								{
+									"x": 83,
+									"y": 208,
+									"h": 466,
+									"w": 466
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1258,
+									"y": 692,
+									"h": 204,
+									"w": 204
+								},
+								{
+									"x": 142,
+									"y": 356,
+									"h": 796,
+									"w": 796
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200431711672127500,
+					"id_str": "1200431711672127488",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EKjKSBdU0AADWcb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKjKSBdU0AADWcb.jpg",
+					"url": "https://t.co/WplzqgnFmi",
+					"display_url": "pic.twitter.com/WplzqgnFmi",
+					"expanded_url": "https://twitter.com/cogdog/status/1200431848158982144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 502,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 885,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1511,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 417,
+									"y": 229,
+									"h": 67,
+									"w": 67
+								},
+								{
+									"x": 47,
+									"y": 118,
+									"h": 264,
+									"w": 264
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1258,
+									"y": 692,
+									"h": 204,
+									"w": 204
+								},
+								{
+									"x": 142,
+									"y": 356,
+									"h": 796,
+									"w": 796
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 737,
+									"y": 405,
+									"h": 119,
+									"w": 119
+								},
+								{
+									"x": 83,
+									"y": 208,
+									"h": 466,
+									"w": 466
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1258,
+									"y": 692,
+									"h": 204,
+									"w": 204
+								},
+								{
+									"x": 142,
+									"y": 356,
+									"h": 796,
+									"w": 796
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1200428708856782800,
+		"in_reply_to_status_id_str": "1200428708856782851",
+		"in_reply_to_user_id": 13046992,
+		"in_reply_to_user_id_str": "13046992",
+		"in_reply_to_screen_name": "mhawksey",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10502,
+			"friends_count": 1496,
+			"listed_count": 785,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 813,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 89707,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@mhawksey @BryanAlexander Hello two great friends meeting! Martin deserves an honorary beard. https://t.co/WplzqgnFmi"
+	},
+	{
 		"created_at": "Fri Nov 29 13:25:42 +0000 2019",
 		"id": 1200405477068136400,
 		"id_str": "1200405477068136450",

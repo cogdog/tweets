4e48ba013cbe2a99dc368bf2466c65327a64c080
Alan Levine <cogdogblog@gmail.com>
2020-01-14 15:15:13 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89869,
-	"created_at": "Tue Jan 14 20:15:09 +0000 2020",
+	"tweets": 89870,
+	"created_at": "Tue Jan 14 21:15:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 14 21:14:41 +0000 2020",
+		"id": 1217193341692989400,
+		"id_str": "1217193341692989442",
+		"full_text": "@LaurenHeywood @noahrmitchell @villaronrubia Until you fork and push and pull, there's no other place to go.",
+		"truncated": false,
+		"display_text_range": [
+			45,
+			108
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LaurenHeywood",
+					"name": "Ｌａｕｒｅｎ",
+					"id": 34058341,
+					"id_str": "34058341",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "noahrmitchell",
+					"name": "Noah Mitchell",
+					"id": 404908919,
+					"id_str": "404908919",
+					"indices": [
+						15,
+						29
+					]
+				},
+				{
+					"screen_name": "villaronrubia",
+					"name": "Daniel Villar-Onrubia",
+					"id": 41424326,
+					"id_str": "41424326",
+					"indices": [
+						30,
+						44
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1217174858318127000,
+		"in_reply_to_status_id_str": "1217174858318127110",
+		"in_reply_to_user_id": 34058341,
+		"in_reply_to_user_id_str": "34058341",
+		"in_reply_to_screen_name": "LaurenHeywood",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10504,
+			"friends_count": 1499,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 800,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90003,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@LaurenHeywood @noahrmitchell @villaronrubia Until you fork and push and pull, there's no other place to go."
+	},
+	{
 		"created_at": "Tue Jan 14 19:40:29 +0000 2020",
 		"id": 1217169635964932000,
 		"id_str": "1217169635964932096",

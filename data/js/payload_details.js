c88947a954ff08ac9206098c6730fc82759db098
Alan Levine <cogdogblog@gmail.com>
2020-02-02 15:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89983,
-	"created_at": "Fri Jan 31 22:15:12 +0000 2020",
+	"tweets": 89984,
+	"created_at": "Sun Feb 02 21:15:09 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,420 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sun Feb 02 20:28:00 +0000 2020",
+		"id": 1224066961392447500,
+		"id_str": "1224066961392447489",
+		"full_text": "Performance and reality of rerouting traffic with 99 second hand phones pulled in a little read wagon, virtual traffic jam. Consider this as counteracting the #netmirror #netnarr https://t.co/2V9FSVZW7Q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netmirror",
+					"indices": [
+						159,
+						169
+					]
+				},
+				{
+					"text": "netnarr",
+					"indices": [
+						170,
+						178
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2V9FSVZW7Q",
+					"expanded_url": "https://twitter.com/simon_deliver/status/1223569659645112320",
+					"display_url": "twitter.com/simon_deliver/…",
+					"indices": [
+						179,
+						202
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10501,
+			"friends_count": 1505,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 804,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90142,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1223569659645112300,
+		"quoted_status_id_str": "1223569659645112320",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2V9FSVZW7Q",
+			"expanded": "https://twitter.com/simon_deliver/status/1223569659645112320",
+			"display": "twitter.com/simon_deliver/…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 01 11:31:54 +0000 2020",
+			"id": 1223569659645112300,
+			"id_str": "1223569659645112320",
+			"full_text": "99 smartphones are transported in a handcart to generate virtual traffic jam in Google Maps. Through this activity, it is possible to turn a green street red which has an impact in the physical world by navigating cars on another route! #googlemapshacks https://t.co/3gixMxopE6 https://t.co/6KcMm1XgAF",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				277
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "googlemapshacks",
+						"indices": [
+							237,
+							253
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/3gixMxopE6",
+						"expanded_url": "http://www.simonweckert.com/googlemapshacks.html",
+						"display_url": "simonweckert.com/googlemapshack…",
+						"indices": [
+							254,
+							277
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1223569651759751200,
+						"id_str": "1223569651759751169",
+						"indices": [
+							278,
+							301
+						],
+						"media_url": "http://pbs.twimg.com/media/EPr-HZMWoAEf2l7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EPr-HZMWoAEf2l7.jpg",
+						"url": "https://t.co/6KcMm1XgAF",
+						"display_url": "pic.twitter.com/6KcMm1XgAF",
+						"expanded_url": "https://twitter.com/simon_deliver/status/1223569659645112320/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 576,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 576,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"small": {
+								"faces": [
+									{
+										"x": 101,
+										"y": 57,
+										"h": 47,
+										"w": 47
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 153,
+										"y": 86,
+										"h": 72,
+										"w": 72
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 153,
+										"y": 86,
+										"h": 72,
+										"w": 72
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 153,
+										"y": 86,
+										"h": 72,
+										"w": 72
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1223569651759751200,
+						"id_str": "1223569651759751169",
+						"indices": [
+							278,
+							301
+						],
+						"media_url": "http://pbs.twimg.com/media/EPr-HZMWoAEf2l7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EPr-HZMWoAEf2l7.jpg",
+						"url": "https://t.co/6KcMm1XgAF",
+						"display_url": "pic.twitter.com/6KcMm1XgAF",
+						"expanded_url": "https://twitter.com/simon_deliver/status/1223569659645112320/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 576,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 576,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"small": {
+								"faces": [
+									{
+										"x": 101,
+										"y": 57,
+										"h": 47,
+										"w": 47
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 153,
+										"y": 86,
+										"h": 72,
+										"w": 72
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 153,
+										"y": 86,
+										"h": 72,
+										"w": 72
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 153,
+										"y": 86,
+										"h": 72,
+										"w": 72
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 839518822310772700,
+				"id_str": "839518822310772736",
+				"name": "Simon Weckert",
+				"screen_name": "simon_deliver",
+				"location": "Berlin, Germany",
+				"description": "artist from Berlin with the focus in digital world – including everything related to code and electronics under the reflection on current social aspects.",
+				"url": "https://t.co/5Nveop927C",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5Nveop927C",
+								"expanded_url": "http://www.simonweckert.com",
+								"display_url": "simonweckert.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 934,
+				"friends_count": 396,
+				"listed_count": 6,
+				"created_at": "Wed Mar 08 16:50:59 +0000 2017",
+				"favourites_count": 491,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 67,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1107649335418855425/slRD2sQ9_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1107649335418855425/slRD2sQ9_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/839518822310772736/1580557303",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8702,
+			"favorite_count": 19330,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Performance and reality of rerouting traffic with 99 second hand phones pulled in a little read wagon, virtual traffic jam. Consider this as counteracting the #netmirror #netnarr https://t.co/2V9FSVZW7Q"
+	}
+]

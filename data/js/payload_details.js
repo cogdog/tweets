c28d75fdc4d3e45ed5fcb0ba215a4a78a5d18491
Alan Levine <cogdogblog@gmail.com>
2020-02-10 08:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90015,
-	"created_at": "Mon Feb 10 02:15:10 +0000 2020",
+	"tweets": 90016,
+	"created_at": "Mon Feb 10 14:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 10 14:11:19 +0000 2020",
+		"id": 1226871268462485500,
+		"id_str": "1226871268462485505",
+		"full_text": "That works quite well! We asked #netnarr to play with https://t.co/SFWdj8LtCo\n\nBut also consider the implications of clicking \"Locate me\". https://t.co/hkwqVvcHNK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						32,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SFWdj8LtCo",
+					"expanded_url": "https://satellitestud.io/osm-haiku/app/",
+					"display_url": "satellitestud.io/osm-haiku/app/",
+					"indices": [
+						54,
+						77
+					]
+				},
+				{
+					"url": "https://t.co/hkwqVvcHNK",
+					"expanded_url": "https://twitter.com/medeathewriter/status/1226531466475114496",
+					"display_url": "twitter.com/medeathewriter…",
+					"indices": [
+						139,
+						162
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10508,
+			"friends_count": 1513,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 803,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90188,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1226531466475114500,
+		"quoted_status_id_str": "1226531466475114496",
+		"quoted_status_permalink": {
+			"url": "https://t.co/hkwqVvcHNK",
+			"expanded": "https://twitter.com/medeathewriter/status/1226531466475114496",
+			"display": "twitter.com/medeathewriter…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Feb 09 15:41:04 +0000 2020",
+			"id": 1226531466475114500,
+			"id_str": "1226531466475114496",
+			"full_text": "❤️ the  Haiku  map.  I  clicked  Fort  Tryon  Park,  near  Cloisters  Museum  (love  it  there)  and  i  got:  Traffic  light  goes  red/  A  day  of  learning  ahead/  Walking  in  Laurel  Hill  Terrace  #netnarr #digciz",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				221
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "netnarr",
+						"indices": [
+							205,
+							213
+						]
+					},
+					{
+						"text": "digciz",
+						"indices": [
+							214,
+							221
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1171559262260269000,
+				"id_str": "1171559262260269056",
+				"name": "Medea_Chillemi",
+				"screen_name": "medeathewriter",
+				"location": "",
+				"description": "Creative writing is my passion. I am a student of art & culture. The photo above is from the Illustr'd vrsn of Jane Eyre. An Attorney; spks Italian & Espan~ol.",
+				"url": "https://t.co/bExiqJEcTF",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bExiqJEcTF",
+								"expanded_url": "http://www.writingisevolutionmedea.wordpress.com",
+								"display_url": "writingisevolutionmedea.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 647,
+				"friends_count": 905,
+				"listed_count": 4,
+				"created_at": "Tue Sep 10 23:02:07 +0000 2019",
+				"favourites_count": 1560,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1002,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1178019264575483905/yejSoK9k_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1178019264575483905/yejSoK9k_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1171559262260269056/1568213255",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "That works quite well! We asked #netnarr to play with https://t.co/SFWdj8LtCo\n\nBut also consider the implications of clicking \"Locate me\". https://t.co/hkwqVvcHNK"
+	},
+	{
 		"created_at": "Mon Feb 10 01:32:10 +0000 2020",
 		"id": 1226680224408100900,
 		"id_str": "1226680224408100864",

deb28b27a98db90c5500695fdb1688d2b0505fae
Alan Levine <cogdogblog@gmail.com>
2020-03-28 20:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90319,
-	"created_at": "Sat Mar 28 19:15:12 +0000 2020",
+	"tweets": 90320,
+	"created_at": "Sun Mar 29 02:15:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 29 02:12:37 +0000 2020",
+		"id": 1244085021620572200,
+		"id_str": "1244085021620572165",
+		"full_text": "I've found the best loading... gif to use in web projects. \n\nIt was on the great Michigan Tech Free Inactive Patent search  (hence public domain) https://t.co/tht25OYldK https://t.co/ipINu7btkP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tht25OYldK",
+					"expanded_url": "http://freeip.mtu.edu/",
+					"display_url": "freeip.mtu.edu",
+					"indices": [
+						146,
+						169
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1244084990842777600,
+					"id_str": "1244084990842777600",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUPgtS2UMAAfoPq.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUPgtS2UMAAfoPq.jpg",
+					"url": "https://t.co/ipINu7btkP",
+					"display_url": "pic.twitter.com/ipINu7btkP",
+					"expanded_url": "https://twitter.com/cogdog/status/1244085021620572165/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 362,
+							"h": 74,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 74,
+							"h": 74,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 362,
+							"h": 74,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 362,
+							"h": 74,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1244084990842777600,
+					"id_str": "1244084990842777600",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUPgtS2UMAAfoPq.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUPgtS2UMAAfoPq.jpg",
+					"url": "https://t.co/ipINu7btkP",
+					"display_url": "pic.twitter.com/ipINu7btkP",
+					"expanded_url": "https://twitter.com/cogdog/status/1244085021620572165/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 362,
+							"h": 74,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 74,
+							"h": 74,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 362,
+							"h": 74,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 362,
+							"h": 74,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							181,
+							37
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EUPgtS2UMAAfoPq.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10545,
+			"friends_count": 1519,
+			"listed_count": 792,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 813,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90579,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I've found the best loading... gif to use in web projects. \n\nIt was on the great Michigan Tech Free Inactive Patent search  (hence public domain) https://t.co/tht25OYldK https://t.co/ipINu7btkP"
+	},
+	{
 		"created_at": "Sat Mar 28 18:39:08 +0000 2020",
 		"id": 1243970896777203700,
 		"id_str": "1243970896777203713",

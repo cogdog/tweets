d1870df145f13adf0a000ec533193456630626af
Alan Levine <cogdogblog@gmail.com>
2020-04-14 00:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90417,
-	"created_at": "Tue Apr 14 05:15:09 +0000 2020",
+	"tweets": 90418,
+	"created_at": "Tue Apr 14 06:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,315 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Tue Apr 14 05:17:05 +0000 2020",
+		"id": 1249929648974127000,
+		"id_str": "1249929648974127105",
+		"full_text": "Retweet-Sub-subtweet https://t.co/x7VSmO9igq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/x7VSmO9igq",
+					"expanded_url": "https://twitter.com/realdlnorman/status/1249929453959962624",
+					"display_url": "twitter.com/realdlnorman/s…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10547,
+			"friends_count": 1523,
+			"listed_count": 795,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 821,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90705,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1249929453959962600,
+		"quoted_status_id_str": "1249929453959962624",
+		"quoted_status_permalink": {
+			"url": "https://t.co/x7VSmO9igq",
+			"expanded": "https://twitter.com/realdlnorman/status/1249929453959962624",
+			"display": "twitter.com/realdlnorman/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Apr 14 05:16:19 +0000 2020",
+			"id": 1249929453959962600,
+			"id_str": "1249929453959962624",
+			"full_text": "Sub-subtweet https://t.co/rDlWzUmN2j",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				12
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1249929449287516200,
+						"id_str": "1249929449287516160",
+						"indices": [
+							13,
+							36
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/EVikNjMVAAAhbxD.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EVikNjMVAAAhbxD.jpg",
+						"url": "https://t.co/rDlWzUmN2j",
+						"display_url": "pic.twitter.com/rDlWzUmN2j",
+						"expanded_url": "https://twitter.com/realdlnorman/status/1249929453959962624/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 500,
+								"h": 370,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 500,
+								"h": 370,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 500,
+								"h": 370,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1249929449287516200,
+						"id_str": "1249929449287516160",
+						"indices": [
+							13,
+							36
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/EVikNjMVAAAhbxD.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EVikNjMVAAAhbxD.jpg",
+						"url": "https://t.co/rDlWzUmN2j",
+						"display_url": "pic.twitter.com/rDlWzUmN2j",
+						"expanded_url": "https://twitter.com/realdlnorman/status/1249929453959962624/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"large": {
+								"w": 500,
+								"h": 370,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 500,
+								"h": 370,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 500,
+								"h": 370,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								50,
+								37
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/EVikNjMVAAAhbxD.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 817493053527113700,
+				"id_str": "817493053527113729",
+				"name": "D'Arcy Norman",
+				"screen_name": "realdlnorman",
+				"location": "Calgary, Alberta",
+				"description": "Futurist. Thought leader. Vapid platitude generator. The leading global expert on the Dunning–Kruger effect. My TED Talk has nearly 3 views.",
+				"url": "https://t.co/YfF3SjsidZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/YfF3SjsidZ",
+								"expanded_url": "https://darcynorman.net",
+								"display_url": "darcynorman.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 480,
+				"friends_count": 348,
+				"listed_count": 13,
+				"created_at": "Fri Jan 06 22:08:27 +0000 2017",
+				"favourites_count": 4838,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 286,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1205654620955004928/8NQv6CXu_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1205654620955004928/8NQv6CXu_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/817493053527113729/1563929703",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Retweet-Sub-subtweet https://t.co/x7VSmO9igq"
+	},
+	{
 		"created_at": "Tue Apr 14 05:04:00 +0000 2020",
 		"id": 1249926354444800000,
 		"id_str": "1249926354444800000",

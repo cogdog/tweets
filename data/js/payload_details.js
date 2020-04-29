7cc9cf2e117ba500dcb197f805832036331ba3d5
Alan Levine <cogdogblog@gmail.com>
2020-04-29 12:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90484,
-	"created_at": "Wed Apr 29 17:15:11 +0000 2020",
+	"tweets": 90485,
+	"created_at": "Wed Apr 29 18:15:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Wed Apr 29 17:34:24 +0000 2020",
+		"id": 1255551019871690800,
+		"id_str": "1255551019871690752",
+		"full_text": "@bennettscience If willing try a new admin option to disable the URl entry field; replace these two theme files, then change the new theme option. \n\nhttps://t.co/BktVJWVQ7R\n\nhttps://t.co/HDjq7F9c22 https://t.co/nKe9PZcgHf",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			197
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bennettscience",
+					"name": "Brian E. Bennett",
+					"id": 270307546,
+					"id_str": "270307546",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BktVJWVQ7R",
+					"expanded_url": "https://github.com/cogdog/splotbox/blob/master/class.splotbox-theme-options.php",
+					"display_url": "github.com/cogdog/splotbo…",
+					"indices": [
+						149,
+						172
+					]
+				},
+				{
+					"url": "https://t.co/HDjq7F9c22",
+					"expanded_url": "https://github.com/cogdog/splotbox/blob/master/page-share.php",
+					"display_url": "github.com/cogdog/splotbo…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1255550981409919000,
+					"id_str": "1255550981409918981",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/EWyc9yRU0AU5aBK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWyc9yRU0AU5aBK.jpg",
+					"url": "https://t.co/nKe9PZcgHf",
+					"display_url": "pic.twitter.com/nKe9PZcgHf",
+					"expanded_url": "https://twitter.com/cogdog/status/1255551019871690752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1578,
+							"h": 448,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 341,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 193,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1255550981409919000,
+					"id_str": "1255550981409918981",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/EWyc9yRU0AU5aBK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWyc9yRU0AU5aBK.jpg",
+					"url": "https://t.co/nKe9PZcgHf",
+					"display_url": "pic.twitter.com/nKe9PZcgHf",
+					"expanded_url": "https://twitter.com/cogdog/status/1255551019871690752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1578,
+							"h": 448,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 341,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 193,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1255529567906861000,
+		"in_reply_to_status_id_str": "1255529567906861059",
+		"in_reply_to_user_id": 270307546,
+		"in_reply_to_user_id_str": "270307546",
+		"in_reply_to_screen_name": "bennettscience",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10565,
+			"friends_count": 1523,
+			"listed_count": 795,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 825,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90789,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@bennettscience If willing try a new admin option to disable the URl entry field; replace these two theme files, then change the new theme option. \n\nhttps://t.co/BktVJWVQ7R\n\nhttps://t.co/HDjq7F9c22 https://t.co/nKe9PZcgHf"
+	},
+	{
 		"created_at": "Wed Apr 29 16:52:01 +0000 2020",
 		"id": 1255540352259219500,
 		"id_str": "1255540352259219456",

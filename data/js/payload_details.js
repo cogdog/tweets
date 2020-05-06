c0806b95f564d0d44eb619c74e2b8a2d6f3eaeb4
Alan Levine <cogdogblog@gmail.com>
2020-05-06 12:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90510,
-	"created_at": "Wed May 06 15:15:10 +0000 2020",
+	"tweets": 90511,
+	"created_at": "Wed May 06 18:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 06 17:19:59 +0000 2020",
+		"id": 1258056120280678400,
+		"id_str": "1258084105545060353",
+		"full_text": "#OEGLOBAL20 Update!  OPEN EDUCATION GLOBAL 2020 CONFERENCE IS TRANSITIONING TO A VIRTUAL FORMAT! Join us in November.  @cccoer \nhttps://t.co/LMHT08Ypyq https://t.co/8rwTv2IIlT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OEGLOBAL20",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cccoer",
+					"name": "CCCOER",
+					"id": 3576910273,
+					"id_str": "3576910273",
+					"indices": [
+						119,
+						126
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/LMHT08Ypyq",
+					"expanded_url": "https://bit.ly/35EiYU9",
+					"display_url": "bit.ly/35EiYU9",
+					"indices": [
+						128,
+						151
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1258053269995139000,
+					"id_str": "1258053269995139072",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/EXWAyJqUMAAugia.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXWAyJqUMAAugia.jpg",
+					"url": "https://t.co/8rwTv2IIlT",
+					"display_url": "pic.twitter.com/8rwTv2IIlT",
+					"expanded_url": "https://twitter.com/OpenEdGlobal/status/1258056120280678402/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1258053269995139000,
+					"id_str": "1258053269995139072",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/EXWAyJqUMAAugia.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXWAyJqUMAAugia.jpg",
+					"url": "https://t.co/8rwTv2IIlT",
+					"display_url": "pic.twitter.com/8rwTv2IIlT",
+					"expanded_url": "https://twitter.com/OpenEdGlobal/status/1258056120280678402/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16548567,
+			"id_str": "16548567",
+			"name": "Open Education Global",
+			"screen_name": "OpenEdGlobal",
+			"location": "Global",
+			"description": "The Open Education Global is a worldwide community of hundreds of universities and associated organizations committed to advancing Open Education globally.",
+			"url": "https://t.co/0Ax8ss2IzW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0Ax8ss2IzW",
+							"expanded_url": "https://www.oeglobal.org/",
+							"display_url": "oeglobal.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10227,
+			"friends_count": 463,
+			"listed_count": 549,
+			"created_at": "Wed Oct 01 19:17:02 +0000 2008",
+			"favourites_count": 2256,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9915,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E6E3E6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1218899679561310208/TTweH6n8_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1218899679561310208/TTweH6n8_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16548567/1579445669",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#OEGLOBAL20 Update!  OPEN EDUCATION GLOBAL 2020 CONFERENCE IS TRANSITIONING TO A VIRTUAL FORMAT! Join us in November.  @cccoer \nhttps://t.co/LMHT08Ypyq https://t.co/8rwTv2IIlT"
+	},
+	{
 		"created_at": "Wed May 06 15:14:25 +0000 2020",
 		"id": 1258052506661187600,
 		"id_str": "1258052506661187586",

b9a1b40e74b3b02516aa9767fbe6a8e93f8eecb1
Alan Levine <cogdogblog@gmail.com>
2020-05-26 19:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90579,
-	"created_at": "Tue May 26 19:15:09 +0000 2020",
+	"tweets": 90580,
+	"created_at": "Wed May 27 01:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 27 01:10:07 +0000 2020",
+		"id": 1265450174723154000,
+		"id_str": "1265450174723153920",
+		"full_text": "@Devon_OnEarth @savasavasava What about the ones that had to wait 100 years for Happy Cat? https://t.co/xJ5ZcjUJzz https://t.co/m6O0utdlbQ",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Devon_OnEarth",
+					"name": "The Mx the Myth the Legend",
+					"id": 1000110254078640100,
+					"id_str": "1000110254078640128",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "savasavasava",
+					"name": "dr. savasavasava",
+					"id": 16699990,
+					"id_str": "16699990",
+					"indices": [
+						15,
+						28
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/xJ5ZcjUJzz",
+					"expanded_url": "https://theconversation.com/explainer-what-are-memes-20789",
+					"display_url": "theconversation.com/explainer-what…",
+					"indices": [
+						91,
+						114
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1265450167529926700,
+					"id_str": "1265450167529926663",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/EY_IOQSUcAc1bIT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EY_IOQSUcAc1bIT.jpg",
+					"url": "https://t.co/m6O0utdlbQ",
+					"display_url": "pic.twitter.com/m6O0utdlbQ",
+					"expanded_url": "https://twitter.com/cogdog/status/1265450174723153920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 605,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1265450167529926700,
+					"id_str": "1265450167529926663",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/EY_IOQSUcAc1bIT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EY_IOQSUcAc1bIT.jpg",
+					"url": "https://t.co/m6O0utdlbQ",
+					"display_url": "pic.twitter.com/m6O0utdlbQ",
+					"expanded_url": "https://twitter.com/cogdog/status/1265450174723153920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 605,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1264897969268166700,
+		"in_reply_to_status_id_str": "1264897969268166656",
+		"in_reply_to_user_id": 1000110254078640100,
+		"in_reply_to_user_id_str": "1000110254078640128",
+		"in_reply_to_screen_name": "Devon_OnEarth",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10556,
+			"friends_count": 1524,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 822,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90896,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Devon_OnEarth @savasavasava What about the ones that had to wait 100 years for Happy Cat? https://t.co/xJ5ZcjUJzz https://t.co/m6O0utdlbQ"
+	},
+	{
 		"created_at": "Tue May 26 18:23:33 +0000 2020",
 		"id": 1265347861950185500,
 		"id_str": "1265347861950185473",

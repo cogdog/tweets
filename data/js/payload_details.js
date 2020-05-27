f2b55f57004b2d606ffddcda3939d23a76041d2b
Alan Levine <cogdogblog@gmail.com>
2020-05-27 12:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90582,
-	"created_at": "Wed May 27 16:15:09 +0000 2020",
+	"tweets": 90583,
+	"created_at": "Wed May 27 18:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 27 18:07:54 +0000 2020",
+		"id": 1265706307379581000,
+		"id_str": "1265706307379580929",
+		"full_text": "Fun to see an AI's efforts describing Wikimedia Commons images. If you love AI you cheer at how well it does (on easy stuff). If you are on the side of John Henry, there's so much it misses outright, or in context that a human senses. Opinion mileage varies. https://t.co/rwQr44Xc0C",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rwQr44Xc0C",
+					"expanded_url": "https://twitter.com/picdescbot/status/1264889019537776641",
+					"display_url": "twitter.com/picdescbot/sta…",
+					"indices": [
+						259,
+						282
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10556,
+			"friends_count": 1524,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 822,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90900,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1264889019537776600,
+		"quoted_status_id_str": "1264889019537776641",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rwQr44Xc0C",
+			"expanded": "https://twitter.com/picdescbot/status/1264889019537776641",
+			"display": "twitter.com/picdescbot/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon May 25 12:00:17 +0000 2020",
+			"id": 1264889019537776600,
+			"id_str": "1264889019537776641",
+			"full_text": "a close up of a window\n\nhttps://t.co/Jko2348Yz5 https://t.co/FvdMwUfr7s",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				47
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Jko2348Yz5",
+						"expanded_url": "https://commons.wikimedia.org/w/index.php?curid=55257010",
+						"display_url": "commons.wikimedia.org/w/index.php?cu…",
+						"indices": [
+							24,
+							47
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1264889017855889400,
+						"id_str": "1264889017855889410",
+						"indices": [
+							48,
+							71
+						],
+						"media_url": "http://pbs.twimg.com/media/EY3J3C2X0AINN0m.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EY3J3C2X0AINN0m.jpg",
+						"url": "https://t.co/FvdMwUfr7s",
+						"display_url": "pic.twitter.com/FvdMwUfr7s",
+						"expanded_url": "https://twitter.com/picdescbot/status/1264889019537776641/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1280,
+								"h": 960,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1264889017855889400,
+						"id_str": "1264889017855889410",
+						"indices": [
+							48,
+							71
+						],
+						"media_url": "http://pbs.twimg.com/media/EY3J3C2X0AINN0m.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EY3J3C2X0AINN0m.jpg",
+						"url": "https://t.co/FvdMwUfr7s",
+						"display_url": "pic.twitter.com/FvdMwUfr7s",
+						"expanded_url": "https://twitter.com/picdescbot/status/1264889019537776641/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1280,
+								"h": 960,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://github.com/elad661/picdescbot\" rel=\"nofollow\">picdescbot</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 715584275123449900,
+				"id_str": "715584275123449857",
+				"name": "picdescbot",
+				"screen_name": "picdescbot",
+				"location": "Not affiliated with Microsoft",
+				"description": "I'm a bot that describes random pictures from wikimedia commons using Microsoft Cognitive Services. Made by @elad3,\nModerated by @elad3 and @0xabad1dea",
+				"url": "https://t.co/RES1DigPgM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/RES1DigPgM",
+								"expanded_url": "https://picdescbot.tumblr.com/about",
+								"display_url": "picdescbot.tumblr.com/about",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8536,
+				"friends_count": 0,
+				"listed_count": 186,
+				"created_at": "Thu Mar 31 16:59:20 +0000 2016",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 12022,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/715641764158316544/kotPMx2h_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/715641764158316544/kotPMx2h_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fun to see an AI's efforts describing Wikimedia Commons images. If you love AI you cheer at how well it does (on easy stuff). If you are on the side of John Henry, there's so much it misses outright, or in context that a human senses. Opinion mileage varies. https://t.co/rwQr44Xc0C"
+	},
+	{
 		"created_at": "Wed May 27 15:51:28 +0000 2020",
 		"id": 1265671974023356400,
 		"id_str": "1265671974023356421",

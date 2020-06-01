67b8ff1b6c3f1733245ed4781b3c237c9b91b719
Alan Levine <cogdogblog@gmail.com>
2020-06-01 15:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90599,
-	"created_at": "Mon Jun 01 16:15:09 +0000 2020",
+	"tweets": 90600,
+	"created_at": "Mon Jun 01 21:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 01 20:29:31 +0000 2020",
+		"id": 1267553885633171500,
+		"id_str": "1267553885633171457",
+		"full_text": "@blahbarian @Influxpress Were there more as brave as El Negro Matapacos... thanks @RespectableLaw for this story and the link to art at https://t.co/nEkAMmtlet",
+		"truncated": false,
+		"display_text_range": [
+			25,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "blahbarian",
+					"name": "Jamie Allen",
+					"id": 199263161,
+					"id_str": "199263161",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "Influxpress",
+					"name": "Influx Press",
+					"id": 275007500,
+					"id_str": "275007500",
+					"indices": [
+						12,
+						24
+					]
+				},
+				{
+					"screen_name": "RespectableLaw",
+					"name": "Respectable Lawyer",
+					"id": 863320472641044500,
+					"id_str": "863320472641044481",
+					"indices": [
+						82,
+						97
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nEkAMmtlet",
+					"expanded_url": "https://matapacos.cl/",
+					"display_url": "matapacos.cl",
+					"indices": [
+						136,
+						159
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1267552490628812800,
+		"in_reply_to_status_id_str": "1267552490628812806",
+		"in_reply_to_user_id": 199263161,
+		"in_reply_to_user_id_str": "199263161",
+		"in_reply_to_screen_name": "blahbarian",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10554,
+			"friends_count": 1524,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 824,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90924,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@blahbarian @Influxpress Were there more as brave as El Negro Matapacos... thanks @RespectableLaw for this story and the link to art at https://t.co/nEkAMmtlet"
+	},
+	{
 		"created_at": "Mon Jun 01 15:57:15 +0000 2020",
 		"id": 1267485370955886600,
 		"id_str": "1267485370955886594",

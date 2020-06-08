3467d15d993174e9e9a201ab11062d023e655ae0
Alan Levine <cogdogblog@gmail.com>
2020-06-08 11:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90618,
-	"created_at": "Mon Jun 08 14:15:09 +0000 2020",
+	"tweets": 90619,
+	"created_at": "Mon Jun 08 17:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 08 16:38:15 +0000 2020",
+		"id": 1270032400773312500,
+		"id_str": "1270032400773312513",
+		"full_text": "Taking Back the Narrative of Ed Tech by @SheaSwauger is just damn good, strong, researched writing with a direct message. \n https://t.co/1bnhVGb6jc https://t.co/Ov65yhMFpP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SheaSwauger",
+					"name": "Shea Swauger",
+					"id": 880813452356440000,
+					"id_str": "880813452356440066",
+					"indices": [
+						40,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1bnhVGb6jc",
+					"expanded_url": "https://www.sheaswauger.com/post/taking-back-the-narrative-of-ed-tech",
+					"display_url": "sheaswauger.com/post/taking-ba…",
+					"indices": [
+						124,
+						147
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1270032247064719400,
+					"id_str": "1270032247064719361",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EaAPmYuVcAEqr3Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaAPmYuVcAEqr3Z.jpg",
+					"url": "https://t.co/Ov65yhMFpP",
+					"display_url": "pic.twitter.com/Ov65yhMFpP",
+					"expanded_url": "https://twitter.com/cogdog/status/1270032400773312513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 225,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 397,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1646,
+							"h": 544,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1270032247064719400,
+					"id_str": "1270032247064719361",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EaAPmYuVcAEqr3Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaAPmYuVcAEqr3Z.jpg",
+					"url": "https://t.co/Ov65yhMFpP",
+					"display_url": "pic.twitter.com/Ov65yhMFpP",
+					"expanded_url": "https://twitter.com/cogdog/status/1270032400773312513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 225,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 397,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1646,
+							"h": 544,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10543,
+			"friends_count": 1522,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 828,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 90947,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Taking Back the Narrative of Ed Tech by @SheaSwauger is just damn good, strong, researched writing with a direct message. \n https://t.co/1bnhVGb6jc https://t.co/Ov65yhMFpP"
+	},
+	{
 		"created_at": "Mon Jun 08 13:56:31 +0000 2020",
 		"id": 1269991703030595600,
 		"id_str": "1269991703030595585",

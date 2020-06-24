63f6a07e94c439fdcecff202f2c0010aafd4aff4
Alan Levine <cogdogblog@gmail.com>
2020-06-24 01:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90676,
-	"created_at": "Wed Jun 24 05:15:10 +0000 2020",
+	"tweets": 90677,
+	"created_at": "Wed Jun 24 07:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 24 06:53:59 +0000 2020",
+		"id": 1275683572129636400,
+		"id_str": "1275683572129636352",
+		"full_text": "If you are ready for travel, I heard the airport in ag Etukokuk is now open, https://t.co/lO50B0ONsy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lO50B0ONsy",
+					"expanded_url": "https://twitter.com/unchartedatlas/status/1275681435551227907",
+					"display_url": "twitter.com/unchartedatlas…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10524,
+			"friends_count": 1523,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 832,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91018,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1275681435551228000,
+		"quoted_status_id_str": "1275681435551227907",
+		"quoted_status_permalink": {
+			"url": "https://t.co/lO50B0ONsy",
+			"expanded": "https://twitter.com/unchartedatlas/status/1275681435551227907",
+			"display": "twitter.com/unchartedatlas…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jun 24 06:45:29 +0000 2020",
+			"id": 1275681435551228000,
+			"id_str": "1275681435551227907",
+			"full_text": "Southern Heights of Alultlálqrar https://t.co/9lLrgGRPYB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				32
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1275681433051414500,
+						"id_str": "1275681433051414529",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/EbQhgSeXQAEzCiC.png",
+						"media_url_https": "https://pbs.twimg.com/media/EbQhgSeXQAEzCiC.png",
+						"url": "https://t.co/9lLrgGRPYB",
+						"display_url": "pic.twitter.com/9lLrgGRPYB",
+						"expanded_url": "https://twitter.com/unchartedatlas/status/1275681435551227907/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 670,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 808,
+								"h": 820,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 808,
+								"h": 820,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1275681433051414500,
+						"id_str": "1275681433051414529",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/EbQhgSeXQAEzCiC.png",
+						"media_url_https": "https://pbs.twimg.com/media/EbQhgSeXQAEzCiC.png",
+						"url": "https://t.co/9lLrgGRPYB",
+						"display_url": "pic.twitter.com/9lLrgGRPYB",
+						"expanded_url": "https://twitter.com/unchartedatlas/status/1275681435551227907/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 670,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 808,
+								"h": 820,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 808,
+								"h": 820,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.martinoleary.com/\" rel=\"nofollow\">Martin's Bot App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 702210829966516200,
+				"id_str": "702210829966516225",
+				"name": "Uncharted Atlas",
+				"screen_name": "unchartedatlas",
+				"location": "",
+				"description": "An atlas of unknown lands | bot by @mewo2",
+				"url": "https://t.co/faIImHVXQ2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/faIImHVXQ2",
+								"expanded_url": "https://shop.mewo2.com/",
+								"display_url": "shop.mewo2.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 15938,
+				"friends_count": 4,
+				"listed_count": 478,
+				"created_at": "Tue Feb 23 19:18:03 +0000 2016",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 37562,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/702213693673762816/D0Wokuj8_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/702213693673762816/D0Wokuj8_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/702210829966516225/1456255669",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If you are ready for travel, I heard the airport in ag Etukokuk is now open, https://t.co/lO50B0ONsy"
+	},
+	{
 		"created_at": "Wed Jun 24 05:02:17 +0000 2020",
 		"id": 1275655464680898600,
 		"id_str": "1275655464680898564",

1532e7910089c9ac4f565ef214f127cf1f4c8527
Alan Levine <cogdogblog@gmail.com>
2020-07-05 14:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90724,
-	"created_at": "Sat Jul 04 07:15:09 +0000 2020",
+	"tweets": 90725,
+	"created_at": "Sun Jul 05 20:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sun Jul 05 19:49:11 +0000 2020",
+		"id": 1279864925200846800,
+		"id_str": "1279864925200846848",
+		"full_text": "The GOP incumbent for Arizona District 6 State Senator is whacky enough, her primary rival manages to make her look sane (see below). I strongly support and already voted for @French4AZ a tireless community supporter and real hero. Go Cia Go! https://t.co/JEZkP8eWuO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			242
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "French4AZ",
+					"name": "Felicia French",
+					"id": 960572331218251800,
+					"id_str": "960572331218251776",
+					"indices": [
+						175,
+						185
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1279864479916802000,
+					"id_str": "1279864479916802050",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/media/EcL99qYUwAIMxpx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcL99qYUwAIMxpx.jpg",
+					"url": "https://t.co/JEZkP8eWuO",
+					"display_url": "pic.twitter.com/JEZkP8eWuO",
+					"expanded_url": "https://twitter.com/cogdog/status/1279864925200846848/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1234,
+							"h": 180,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 175,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 99,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1279864479916802000,
+					"id_str": "1279864479916802050",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/media/EcL99qYUwAIMxpx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcL99qYUwAIMxpx.jpg",
+					"url": "https://t.co/JEZkP8eWuO",
+					"display_url": "pic.twitter.com/JEZkP8eWuO",
+					"expanded_url": "https://twitter.com/cogdog/status/1279864925200846848/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1234,
+							"h": 180,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 175,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 99,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10525,
+			"friends_count": 1529,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 838,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91077,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The GOP incumbent for Arizona District 6 State Senator is whacky enough, her primary rival manages to make her look sane (see below). I strongly support and already voted for @French4AZ a tireless community supporter and real hero. Go Cia Go! https://t.co/JEZkP8eWuO"
+	},
+	{
 		"created_at": "Sat Jul 04 07:08:53 +0000 2020",
 		"id": 1279311200434585600,
 		"id_str": "1279311200434585602",

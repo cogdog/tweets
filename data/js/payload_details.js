b2b6daaf0f8411a597d8c4e0b4effb9f8f85a15c
Alan Levine <cogdogblog@gmail.com>
2020-07-29 13:15:14 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90786,
-	"created_at": "Wed Jul 29 17:17:05 +0000 2020",
+	"tweets": 90787,
+	"created_at": "Wed Jul 29 19:15:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Wed Jul 29 18:41:12 +0000 2020",
+		"id": 1288545124226498600,
+		"id_str": "1288545124226498560",
+		"full_text": "\"Rediscovering the Small Web\" is a refreshing breath of air https://t.co/zBW0mDk68V \"A web that is unpolished, often quirky but often also fun, creative and interesting\" Not everything needs to be corporatized/datatized h/t @jay_hoffmann https://t.co/9SnSbgmcBe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jay_hoffmann",
+					"name": "Jay Hoffmann",
+					"id": 590143431,
+					"id_str": "590143431",
+					"indices": [
+						224,
+						237
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zBW0mDk68V",
+					"expanded_url": "https://neustadt.fr/essays/the-small-web/",
+					"display_url": "neustadt.fr/essays/the-sma…",
+					"indices": [
+						60,
+						83
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1288544955187650600,
+					"id_str": "1288544955187650560",
+					"indices": [
+						238,
+						261
+					],
+					"media_url": "http://pbs.twimg.com/media/EeHUz0sU4AADxS7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeHUz0sU4AADxS7.jpg",
+					"url": "https://t.co/9SnSbgmcBe",
+					"display_url": "pic.twitter.com/9SnSbgmcBe",
+					"expanded_url": "https://twitter.com/cogdog/status/1288545124226498560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1370,
+							"h": 616,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 306,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1288544955187650600,
+					"id_str": "1288544955187650560",
+					"indices": [
+						238,
+						261
+					],
+					"media_url": "http://pbs.twimg.com/media/EeHUz0sU4AADxS7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeHUz0sU4AADxS7.jpg",
+					"url": "https://t.co/9SnSbgmcBe",
+					"display_url": "pic.twitter.com/9SnSbgmcBe",
+					"expanded_url": "https://twitter.com/cogdog/status/1288545124226498560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1370,
+							"h": 616,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 306,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10513,
+			"friends_count": 1530,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 841,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91145,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Rediscovering the Small Web\" is a refreshing breath of air https://t.co/zBW0mDk68V \"A web that is unpolished, often quirky but often also fun, creative and interesting\" Not everything needs to be corporatized/datatized h/t @jay_hoffmann https://t.co/9SnSbgmcBe"
+	},
+	{
 		"created_at": "Wed Jul 29 17:11:40 +0000 2020",
 		"id": 1288522591540359200,
 		"id_str": "1288522591540359168",

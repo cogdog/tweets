5211457c349ff1c5a1923578404ec2fe996f689a
Alan Levine <cogdogblog@gmail.com>
2020-08-08 10:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90826,
-	"created_at": "Sat Aug 08 06:15:09 +0000 2020",
+	"tweets": 90827,
+	"created_at": "Sat Aug 08 16:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 08 16:12:11 +0000 2020",
+		"id": 1292131503254601700,
+		"id_str": "1292131503254601734",
+		"full_text": "@chefdgreen @edtechfactotum It should if opengraph meta tags in view source of single video page reports mp4 url for og:video - look for a comment \"&lt;!-- direct asset ref --&gt;\" More on the behind the curtain https://t.co/3kd0C5LM4k\n\nI dont know if this requires specific options set on Kaltura admin side.",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			309
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "chefdgreen",
+					"name": "Dennis Green",
+					"id": 482461267,
+					"id_str": "482461267",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "edtechfactotum",
+					"name": "Clint Lalonde (he/him)",
+					"id": 822204322796609500,
+					"id_str": "822204322796609536",
+					"indices": [
+						12,
+						27
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3kd0C5LM4k",
+					"expanded_url": "https://cogdogblog.com/2020/07/google-sheet-karate-moves/",
+					"display_url": "cogdogblog.com/2020/07/google…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1292095570274705400,
+		"in_reply_to_status_id_str": "1292095570274705409",
+		"in_reply_to_user_id": 482461267,
+		"in_reply_to_user_id_str": "482461267",
+		"in_reply_to_screen_name": "chefdgreen",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10515,
+			"friends_count": 1529,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 843,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91197,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@chefdgreen @edtechfactotum It should if opengraph meta tags in view source of single video page reports mp4 url for og:video - look for a comment \"&lt;!-- direct asset ref --&gt;\" More on the behind the curtain https://t.co/3kd0C5LM4k\n\nI dont know if this requires specific options set on Kaltura admin side."
+	},
+	{
 		"created_at": "Sat Aug 08 05:48:08 +0000 2020",
 		"id": 1291974454587793400,
 		"id_str": "1291974454587793408",

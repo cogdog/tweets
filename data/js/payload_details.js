d8170042a5e055111732173491193476711bb05a
Alan Levine <cogdogblog@gmail.com>
2020-08-18 16:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90839,
-	"created_at": "Tue Aug 18 20:15:09 +0000 2020",
+	"tweets": 90840,
+	"created_at": "Tue Aug 18 22:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Tue Aug 18 21:22:41 +0000 2020",
+		"id": 1295833519235580000,
+		"id_str": "1295833519235579904",
+		"full_text": "So cognitively there \"Person. Woman. Man Camera. TV\" https://t.co/zhkGtU5MLy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			52
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zhkGtU5MLy",
+					"expanded_url": "https://twitter.com/RadioFreeTom/status/1295772248071118848",
+					"display_url": "twitter.com/RadioFreeTom/s…",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10522,
+			"friends_count": 1530,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 846,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91213,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1295772248071118800,
+		"quoted_status_id_str": "1295772248071118848",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zhkGtU5MLy",
+			"expanded": "https://twitter.com/RadioFreeTom/status/1295772248071118848",
+			"display": "twitter.com/RadioFreeTom/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Aug 18 17:19:12 +0000 2020",
+			"id": 1295772248071118800,
+			"id_str": "1295772248071118848",
+			"full_text": "There's always a pic of Trump with the guy he claims he's never heard of before. https://t.co/YjeNWuQi41",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				80
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/YjeNWuQi41",
+						"expanded_url": "https://twitter.com/MilesTaylorUSA/status/1295720245508878336",
+						"display_url": "twitter.com/MilesTaylorUSA…",
+						"indices": [
+							81,
+							104
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 459872442,
+				"id_str": "459872442",
+				"name": "Tom Nichols",
+				"screen_name": "RadioFreeTom",
+				"location": "Newport, RI",
+				"description": "Professor, author of \"The Death of Expertise.\" Curmudgeon. Cat guy. Ex-GOP, Never Trumper \"human scum.\" Senior adviser, @ProjectLincoln. Views solely my own.",
+				"url": "https://t.co/TDY5lnwnqH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/TDY5lnwnqH",
+								"expanded_url": "https://www.facebook.com/DeathOfExpertise/",
+								"display_url": "facebook.com/DeathOfExperti…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 440810,
+				"friends_count": 1499,
+				"listed_count": 4680,
+				"created_at": "Tue Jan 10 04:23:15 +0000 2012",
+				"favourites_count": 78342,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 331710,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0099B9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1295102326814969856/7-9W712Z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1295102326814969856/7-9W712Z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/459872442/1546472365",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E6F6F9",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1295720245508878300,
+			"quoted_status_id_str": "1295720245508878336",
+			"quoted_status_permalink": {
+				"url": "https://t.co/YjeNWuQi41",
+				"expanded": "https://twitter.com/MilesTaylorUSA/status/1295720245508878336",
+				"display": "twitter.com/MilesTaylorUSA…"
+			},
+			"retweet_count": 912,
+			"favorite_count": 4246,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So cognitively there \"Person. Woman. Man Camera. TV\" https://t.co/zhkGtU5MLy"
+	},
+	{
 		"created_at": "Tue Aug 18 19:53:29 +0000 2020",
 		"id": 1295811072977334300,
 		"id_str": "1295811072977334272",

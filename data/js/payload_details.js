4f28595ab5a93b9ac51511c8b22eaa33b5f3f79e
Alan Levine <cogdogblog@gmail.com>
2020-08-22 08:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90850,
-	"created_at": "Sat Aug 22 04:15:16 +0000 2020",
+	"tweets": 90851,
+	"created_at": "Sat Aug 22 14:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 22 13:18:31 +0000 2020",
+		"id": 1297161226770735000,
+		"id_str": "1297161226770735104",
+		"full_text": "@ActivateLearn @tmccormick @hrheingold @nayafia @housing_wiki @courosa #etmooc and many more were set up with WordPress syndication as Connected Courses https://t.co/mIcmlFNjYM",
+		"truncated": false,
+		"display_text_range": [
+			71,
+			176
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "etmooc",
+					"indices": [
+						71,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ActivateLearn",
+					"name": "Helen Blunden #AlwaysBeLearning 🤔🧠🦉📚🥇",
+					"id": 450340061,
+					"id_str": "450340061",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "tmccormick",
+					"name": "Tim McCormick",
+					"id": 21759817,
+					"id_str": "21759817",
+					"indices": [
+						15,
+						26
+					]
+				},
+				{
+					"screen_name": "hrheingold",
+					"name": "(((Howard Rheingold)))",
+					"id": 5388852,
+					"id_str": "5388852",
+					"indices": [
+						27,
+						38
+					]
+				},
+				{
+					"screen_name": "nayafia",
+					"name": "Nadia",
+					"id": 326511843,
+					"id_str": "326511843",
+					"indices": [
+						39,
+						47
+					]
+				},
+				{
+					"screen_name": "housing_wiki",
+					"name": "HousingWiki",
+					"id": 786798234689687600,
+					"id_str": "786798234689687557",
+					"indices": [
+						48,
+						61
+					]
+				},
+				{
+					"screen_name": "courosa",
+					"name": "Dr. Alec Couros",
+					"id": 739293,
+					"id_str": "739293",
+					"indices": [
+						62,
+						70
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mIcmlFNjYM",
+					"expanded_url": "https://cogdogblog.com/2014/07/feed-wordpress-101/",
+					"display_url": "cogdogblog.com/2014/07/feed-w…",
+					"indices": [
+						153,
+						176
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1297143556679725000,
+		"in_reply_to_status_id_str": "1297143556679725058",
+		"in_reply_to_user_id": 450340061,
+		"in_reply_to_user_id_str": "450340061",
+		"in_reply_to_screen_name": "ActivateLearn",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10529,
+			"friends_count": 1530,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 846,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91227,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ActivateLearn @tmccormick @hrheingold @nayafia @housing_wiki @courosa #etmooc and many more were set up with WordPress syndication as Connected Courses https://t.co/mIcmlFNjYM"
+	},
+	{
 		"created_at": "Sat Aug 22 03:54:58 +0000 2020",
 		"id": 1297019406682210300,
 		"id_str": "1297019406682210304",

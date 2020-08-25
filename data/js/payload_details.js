eca1e2ffb89904c8509259019310d11207eb7a5f
Alan Levine <cogdogblog@gmail.com>
2020-08-25 07:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90855,
-	"created_at": "Mon Aug 24 21:15:09 +0000 2020",
+	"tweets": 90856,
+	"created_at": "Tue Aug 25 13:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Tue Aug 25 13:13:51 +0000 2020",
+		"id": 1298247218055454700,
+		"id_str": "1298247218055454721",
+		"full_text": "Thanks @resistbot for making it easy to write @SenMcSallyAZ about saving the post office. Her response citing its importance was hardly convincing and showed zero indication of action. We need better leaders, like @CaptMarkKelly https://t.co/d9mR3XF5kz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "resistbot",
+					"name": "Resistbot",
+					"id": 835740314006511600,
+					"id_str": "835740314006511618",
+					"indices": [
+						7,
+						17
+					]
+				},
+				{
+					"screen_name": "SenMcSallyAZ",
+					"name": "Martha McSally",
+					"id": 2964949642,
+					"id_str": "2964949642",
+					"indices": [
+						46,
+						59
+					]
+				},
+				{
+					"screen_name": "CaptMarkKelly",
+					"name": "Captain Mark Kelly",
+					"id": 65707359,
+					"id_str": "65707359",
+					"indices": [
+						214,
+						228
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1298247099297853400,
+					"id_str": "1298247099297853440",
+					"indices": [
+						229,
+						252
+					],
+					"media_url": "http://pbs.twimg.com/media/EgRM28BUcAAhOj0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgRM28BUcAAhOj0.jpg",
+					"url": "https://t.co/d9mR3XF5kz",
+					"display_url": "pic.twitter.com/d9mR3XF5kz",
+					"expanded_url": "https://twitter.com/cogdog/status/1298247218055454721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 422,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 728,
+							"h": 1174,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 728,
+							"h": 1174,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1298247099297853400,
+					"id_str": "1298247099297853440",
+					"indices": [
+						229,
+						252
+					],
+					"media_url": "http://pbs.twimg.com/media/EgRM28BUcAAhOj0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgRM28BUcAAhOj0.jpg",
+					"url": "https://t.co/d9mR3XF5kz",
+					"display_url": "pic.twitter.com/d9mR3XF5kz",
+					"expanded_url": "https://twitter.com/cogdog/status/1298247218055454721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 422,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 728,
+							"h": 1174,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 728,
+							"h": 1174,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10527,
+			"friends_count": 1530,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 846,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91232,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thanks @resistbot for making it easy to write @SenMcSallyAZ about saving the post office. Her response citing its importance was hardly convincing and showed zero indication of action. We need better leaders, like @CaptMarkKelly https://t.co/d9mR3XF5kz"
+	},
+	{
 		"created_at": "Mon Aug 24 20:43:33 +0000 2020",
 		"id": 1297997997951787000,
 		"id_str": "1297997997951787008",

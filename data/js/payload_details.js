f4e60ca8ec65b169bbacfd585c57026d30f7ba10
Alan Levine <cogdogblog@gmail.com>
2020-08-27 12:15:13 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90870,
-	"created_at": "Thu Aug 27 17:15:11 +0000 2020",
+	"tweets": 90871,
+	"created_at": "Thu Aug 27 18:15:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Thu Aug 27 17:18:46 +0000 2020",
+		"id": 1299033627414524000,
+		"id_str": "1299033627414523904",
+		"full_text": "Wow,head spinning from amazing @BCcampus webinar on Designing Practice Problems in H5P with guests @juliejeremiah @JRDingwall Check out the bursting bag of links shared https://t.co/DhFazHDhNI\n\nResources available and soon the archive will be at https://t.co/triCuDLMV1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BCcampus",
+					"name": "BCcampus",
+					"id": 93710949,
+					"id_str": "93710949",
+					"indices": [
+						31,
+						40
+					]
+				},
+				{
+					"screen_name": "juliejeremiah",
+					"name": "Julie Maier",
+					"id": 228482961,
+					"id_str": "228482961",
+					"indices": [
+						99,
+						113
+					]
+				},
+				{
+					"screen_name": "JRDingwall",
+					"name": "JR Dingwall",
+					"id": 141806761,
+					"id_str": "141806761",
+					"indices": [
+						114,
+						125
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DhFazHDhNI",
+					"expanded_url": "https://docs.google.com/document/d/1zDp9I3KEsWzwixJCue_JNz66d464EAZbIF77LZhY0ck/edit#",
+					"display_url": "docs.google.com/document/d/1zD…",
+					"indices": [
+						169,
+						192
+					]
+				},
+				{
+					"url": "https://t.co/triCuDLMV1",
+					"expanded_url": "https://kitchen.opened.ca/2020/08/07/august-webinar/",
+					"display_url": "kitchen.opened.ca/2020/08/07/aug…",
+					"indices": [
+						246,
+						269
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10525,
+			"friends_count": 1530,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 846,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91251,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wow,head spinning from amazing @BCcampus webinar on Designing Practice Problems in H5P with guests @juliejeremiah @JRDingwall Check out the bursting bag of links shared https://t.co/DhFazHDhNI\n\nResources available and soon the archive will be at https://t.co/triCuDLMV1"
+	},
+	{
 		"created_at": "Thu Aug 27 17:09:49 +0000 2020",
 		"id": 1299031377950605300,
 		"id_str": "1299031377950605312",

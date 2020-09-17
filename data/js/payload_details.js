a91611cdff4e3573487081cb917a2de05041a563
Alan Levine <cogdogblog@gmail.com>
2020-09-17 12:15:13 -0600

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90983,
-	"created_at": "Thu Sep 17 17:15:11 +0000 2020",
+	"tweets": 90985,
+	"created_at": "Thu Sep 17 18:15:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,552 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 17 17:48:45 +0000 2020",
+		"id": 1306651318338027500,
+		"id_str": "1306651318338027523",
+		"full_text": "@J_Everhart383 @bennettscience @clssrmtechtools I want to see that post! I need to learn to turn my old school bookmarklet tools like https://t.co/MQrViBnmt4 into proper extensions.\n\nOne day... one day...",
+		"truncated": false,
+		"display_text_range": [
+			48,
+			204
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "J_Everhart383",
+					"name": "Jeff Everhart",
+					"id": 223246172,
+					"id_str": "223246172",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "bennettscience",
+					"name": "Brian E. Bennett",
+					"id": 270307546,
+					"id_str": "270307546",
+					"indices": [
+						15,
+						30
+					]
+				},
+				{
+					"screen_name": "clssrmtechtools",
+					"name": "Classroom Tech Tools",
+					"id": 859694936140075000,
+					"id_str": "859694936140075008",
+					"indices": [
+						31,
+						47
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MQrViBnmt4",
+					"expanded_url": "https://cogdog.github.io/flickr-cc-helper/",
+					"display_url": "cogdog.github.io/flickr-cc-help…",
+					"indices": [
+						134,
+						157
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1306650477216624600,
+		"in_reply_to_status_id_str": "1306650477216624640",
+		"in_reply_to_user_id": 223246172,
+		"in_reply_to_user_id_str": "223246172",
+		"in_reply_to_screen_name": "J_Everhart383",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10526,
+			"friends_count": 1534,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 852,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91402,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@J_Everhart383 @bennettscience @clssrmtechtools I want to see that post! I need to learn to turn my old school bookmarklet tools like https://t.co/MQrViBnmt4 into proper extensions.\n\nOne day... one day..."
+	},
+	{
+		"created_at": "Thu Sep 17 17:47:19 +0000 2020",
+		"id": 1306650957095166000,
+		"id_str": "1306650957095165952",
+		"full_text": "Fantastic news for all fans of @SongExploder \n\nI always found it a metaphor educators might use to explain how they designed an activity... https://t.co/nFKyn68sDd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SongExploder",
+					"name": "Song Exploder",
+					"id": 2249590315,
+					"id_str": "2249590315",
+					"indices": [
+						31,
+						44
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nFKyn68sDd",
+					"expanded_url": "https://twitter.com/HrishiHirway/status/1306598172567904267",
+					"display_url": "twitter.com/HrishiHirway/s…",
+					"indices": [
+						140,
+						163
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10526,
+			"friends_count": 1534,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 852,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91402,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1306598172567904300,
+		"quoted_status_id_str": "1306598172567904267",
+		"quoted_status_permalink": {
+			"url": "https://t.co/nFKyn68sDd",
+			"expanded": "https://twitter.com/HrishiHirway/status/1306598172567904267",
+			"display": "twitter.com/HrishiHirway/s…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 17 14:17:34 +0000 2020",
+			"id": 1306598172567904300,
+			"id_str": "1306598172567904267",
+			"full_text": "I still don’t fully believe it but @SongExploder, a podcast I started in my bedroom!, is going to be a @netflix series.  Featuring @aliciakeys, @Lin_Manuel, @remhq, and @tydollasign. Coming October 2nd! https://t.co/8FdYSS60dV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				202
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SongExploder",
+						"name": "Song Exploder",
+						"id": 2249590315,
+						"id_str": "2249590315",
+						"indices": [
+							35,
+							48
+						]
+					},
+					{
+						"screen_name": "netflix",
+						"name": "Netflix",
+						"id": 16573941,
+						"id_str": "16573941",
+						"indices": [
+							103,
+							111
+						]
+					},
+					{
+						"screen_name": "aliciakeys",
+						"name": "Alicia Keys",
+						"id": 35094637,
+						"id_str": "35094637",
+						"indices": [
+							131,
+							142
+						]
+					},
+					{
+						"screen_name": "Lin_Manuel",
+						"name": "Lin-Manuel Miranda",
+						"id": 79923701,
+						"id_str": "79923701",
+						"indices": [
+							144,
+							155
+						]
+					},
+					{
+						"screen_name": "remhq",
+						"name": "R.E.M. HQ",
+						"id": 14100619,
+						"id_str": "14100619",
+						"indices": [
+							157,
+							163
+						]
+					},
+					{
+						"screen_name": "tydollasign",
+						"name": "Ty Dolla $ign",
+						"id": 25932445,
+						"id_str": "25932445",
+						"indices": [
+							169,
+							181
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1306528851078471700,
+						"id_str": "1306528851078471680",
+						"indices": [
+							203,
+							226
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"url": "https://t.co/8FdYSS60dV",
+						"display_url": "pic.twitter.com/8FdYSS60dV",
+						"expanded_url": "https://twitter.com/HrishiHirway/status/1306598172567904267/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1306528851078471700,
+						"id_str": "1306528851078471680",
+						"indices": [
+							203,
+							226
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"url": "https://t.co/8FdYSS60dV",
+						"display_url": "pic.twitter.com/8FdYSS60dV",
+						"expanded_url": "https://twitter.com/HrishiHirway/status/1306598172567904267/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 102978,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/pl/ukst7ZYoSCkMAvpK.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/vid/640x360/NKj4OJKqfZbf5i5j.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/vid/1280x720/O651TleIpPVCan2L.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/vid/480x270/vZYePQZLHh_KAxpD.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18695490,
+				"id_str": "18695490",
+				"name": "Hrishikesh Hirway",
+				"screen_name": "HrishiHirway",
+				"location": "Los Angeles, CA",
+				"description": "I make music & podcasts. host/creator of @songexploder · co-host/producer of Home Cooking",
+				"url": "https://t.co/Cemmd7hlN4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Cemmd7hlN4",
+								"expanded_url": "http://hrishikesh.co",
+								"display_url": "hrishikesh.co",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 64866,
+				"friends_count": 1164,
+				"listed_count": 546,
+				"created_at": "Tue Jan 06 21:08:58 +0000 2009",
+				"favourites_count": 36010,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 9841,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1267838089931546631/P-R7ZEbc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267838089931546631/P-R7ZEbc_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18695490/1591140860",
+				"profile_link_color": "333344",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 534,
+			"favorite_count": 5672,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fantastic news for all fans of @SongExploder \n\nI always found it a metaphor educators might use to explain how they designed an activity... https://t.co/nFKyn68sDd"
+	},
+	{
 		"created_at": "Thu Sep 17 16:26:57 +0000 2020",
 		"id": 1306630731318268000,
 		"id_str": "1306630731318267904",

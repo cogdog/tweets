80eb115d837bf8c4eee1c64986a7dbf870ca8bd8
Alan Levine <cogdogblog@gmail.com>
2020-09-23 22:15:13 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91022,
-	"created_at": "Thu Sep 24 03:15:11 +0000 2020",
+	"tweets": 91023,
+	"created_at": "Thu Sep 24 04:15:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 24 03:37:19 +0000 2020",
+		"id": 1308973762403074000,
+		"id_str": "1308973762403074052",
+		"full_text": "15 tweets lined up for tomorrow's #HeyPresstoConf20 blabbering on \"Doing Small Things With the WordPress API\"  \n\nhttps://t.co/vOuxfacMKb https://t.co/Iu8IUtTkVF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HeyPresstoConf20",
+					"indices": [
+						34,
+						51
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vOuxfacMKb",
+					"expanded_url": "https://2020.heypresstoconf.org/?easyconfpr=doing-small-things-with-the-wordpress-api",
+					"display_url": "2020.heypresstoconf.org/?easyconfpr=do…",
+					"indices": [
+						113,
+						136
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1308973713761722400,
+					"id_str": "1308973713761722371",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/EipoqE0XYAMJeXe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EipoqE0XYAMJeXe.jpg",
+					"url": "https://t.co/Iu8IUtTkVF",
+					"display_url": "pic.twitter.com/Iu8IUtTkVF",
+					"expanded_url": "https://twitter.com/cogdog/status/1308973762403074052/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 497,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 616,
+							"h": 842,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 616,
+							"h": 842,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1308973713761722400,
+					"id_str": "1308973713761722371",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/EipoqE0XYAMJeXe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EipoqE0XYAMJeXe.jpg",
+					"url": "https://t.co/Iu8IUtTkVF",
+					"display_url": "pic.twitter.com/Iu8IUtTkVF",
+					"expanded_url": "https://twitter.com/cogdog/status/1308973762403074052/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 497,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 616,
+							"h": 842,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 616,
+							"h": 842,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10518,
+			"friends_count": 1535,
+			"listed_count": 793,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 852,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91447,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "15 tweets lined up for tomorrow's #HeyPresstoConf20 blabbering on \"Doing Small Things With the WordPress API\"  \n\nhttps://t.co/vOuxfacMKb https://t.co/Iu8IUtTkVF"
+	},
+	{
 		"created_at": "Thu Sep 24 02:23:12 +0000 2020",
 		"id": 1308955111272583200,
 		"id_str": "1308955111272583168",

c7356bf19ff5e0f3db2b50300be96d6b301f2c81
Alan Levine <cogdogblog@gmail.com>
2020-10-12 17:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91157,
-	"created_at": "Mon Oct 12 22:15:10 +0000 2020",
+	"tweets": 91158,
+	"created_at": "Mon Oct 12 23:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 12 22:26:00 +0000 2020",
+		"id": 1315780787438907400,
+		"id_str": "1315780787438907392",
+		"full_text": "The new license labels in Google Images is craptastic. They give results from https://t.co/vEYgbEq1u0 linked to a public domain license; when you go to the site, make an account, up comes the paywall. #GoogleIsDumb https://t.co/uPKNIWJD9v",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoogleIsDumb",
+					"indices": [
+						201,
+						214
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vEYgbEq1u0",
+					"expanded_url": "http://depositphotos.com",
+					"display_url": "depositphotos.com",
+					"indices": [
+						78,
+						101
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1315780719126306800,
+					"id_str": "1315780719126306816",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/EkKXl73U4AAPFOa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkKXl73U4AAPFOa.jpg",
+					"url": "https://t.co/uPKNIWJD9v",
+					"display_url": "pic.twitter.com/uPKNIWJD9v",
+					"expanded_url": "https://twitter.com/cogdog/status/1315780787438907392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 369,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1112,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315780719126306800,
+					"id_str": "1315780719126306816",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/EkKXl73U4AAPFOa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkKXl73U4AAPFOa.jpg",
+					"url": "https://t.co/uPKNIWJD9v",
+					"display_url": "pic.twitter.com/uPKNIWJD9v",
+					"expanded_url": "https://twitter.com/cogdog/status/1315780787438907392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 369,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1112,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1315780719126356000,
+					"id_str": "1315780719126355968",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/EkKXl73VoAAn-ax.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkKXl73VoAAn-ax.jpg",
+					"url": "https://t.co/uPKNIWJD9v",
+					"display_url": "pic.twitter.com/uPKNIWJD9v",
+					"expanded_url": "https://twitter.com/cogdog/status/1315780787438907392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 404,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1215,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 712,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10523,
+			"friends_count": 1536,
+			"listed_count": 792,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 858,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91606,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The new license labels in Google Images is craptastic. They give results from https://t.co/vEYgbEq1u0 linked to a public domain license; when you go to the site, make an account, up comes the paywall. #GoogleIsDumb https://t.co/uPKNIWJD9v"
+	},
+	{
 		"created_at": "Mon Oct 12 21:44:30 +0000 2020",
 		"id": 1315770345979170800,
 		"id_str": "1315770345979170816",

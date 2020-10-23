fa3aba8808a921d149d66188ab09a1b0d7b19719
Alan Levine <cogdogblog@gmail.com>
2020-10-22 18:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91209,
-	"created_at": "Thu Oct 22 17:15:10 +0000 2020",
+	"tweets": 91210,
+	"created_at": "Fri Oct 23 00:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 22 23:20:27 +0000 2020",
+		"id": 1319418368781287400,
+		"id_str": "1319418368781287424",
+		"full_text": "@tofer8 @WickedDecent @cheryloakes50 @KerryHawk02 With a self-hosted WordPress site, the SPLOTbox theme can do this for uploaded/recorded audio (not video) plus a heap of other media sources https://t.co/b9vEIHMYbT",
+		"truncated": false,
+		"display_text_range": [
+			50,
+			214
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tofer8",
+					"name": "Terri Dawson",
+					"id": 15144649,
+					"id_str": "15144649",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "WickedDecent",
+					"name": "Dan Ryder",
+					"id": 15194262,
+					"id_str": "15194262",
+					"indices": [
+						8,
+						21
+					]
+				},
+				{
+					"screen_name": "cheryloakes50",
+					"name": "cheryloakes Rocking ACTEM 20/20 Vision",
+					"id": 5146011,
+					"id_str": "5146011",
+					"indices": [
+						22,
+						36
+					]
+				},
+				{
+					"screen_name": "KerryHawk02",
+					"name": "Kerry Gallagher JD",
+					"id": 21903605,
+					"id_str": "21903605",
+					"indices": [
+						37,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/b9vEIHMYbT",
+					"expanded_url": "https://github.com/cogdog/splotbox",
+					"display_url": "github.com/cogdog/splotbox",
+					"indices": [
+						191,
+						214
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1319392696138608600,
+		"in_reply_to_status_id_str": "1319392696138608640",
+		"in_reply_to_user_id": 15144649,
+		"in_reply_to_user_id_str": "15144649",
+		"in_reply_to_screen_name": "tofer8",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10522,
+			"friends_count": 1536,
+			"listed_count": 792,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 860,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91670,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@tofer8 @WickedDecent @cheryloakes50 @KerryHawk02 With a self-hosted WordPress site, the SPLOTbox theme can do this for uploaded/recorded audio (not video) plus a heap of other media sources https://t.co/b9vEIHMYbT"
+	},
+	{
 		"created_at": "Thu Oct 22 17:05:05 +0000 2020",
 		"id": 1319323906046447600,
 		"id_str": "1319323906046447616",

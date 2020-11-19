54ec5098176431124f0200bfc569cb137c0c87ba
Alan Levine <cogdogblog@gmail.com>
2020-11-19 15:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91429,
-	"created_at": "Thu Nov 19 20:15:11 +0000 2020",
+	"tweets": 91430,
+	"created_at": "Thu Nov 19 21:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 19 21:06:31 +0000 2020",
+		"id": 1329531522659934200,
+		"id_str": "1329531522659934209",
+		"full_text": "In a follow-up story, Martians have landed.... yes, at the Four Seasons Landscaping HQ. https://t.co/JdFpw2Lyrk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JdFpw2Lyrk",
+					"expanded_url": "https://twitter.com/remixmanifesto/status/1329512342766292993",
+					"display_url": "twitter.com/remixmanifesto…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10533,
+			"friends_count": 1539,
+			"listed_count": 793,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 866,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91964,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1329512342766293000,
+		"quoted_status_id_str": "1329512342766292993",
+		"quoted_status_permalink": {
+			"url": "https://t.co/JdFpw2Lyrk",
+			"expanded": "https://twitter.com/remixmanifesto/status/1329512342766292993",
+			"display": "twitter.com/remixmanifesto…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 19 19:50:18 +0000 2020",
+			"id": 1329512342766293000,
+			"id_str": "1329512342766292993",
+			"full_text": "wow https://t.co/MBiMfxsC5g",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				3
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/MBiMfxsC5g",
+						"expanded_url": "https://twitter.com/joshtpm/status/1329486814558752769",
+						"display_url": "twitter.com/joshtpm/status…",
+						"indices": [
+							4,
+							27
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17851599,
+				"id_str": "17851599",
+				"name": "Brett Gaylor",
+				"screen_name": "remixmanifesto",
+				"location": "Victoria, BC",
+				"description": "Making documentaries about the Internet: The Internet of Everything @donottrackdoc and Rip! A Remix Manifesto. Board @OpenMediaOrg + @fernwoodNRG, alum @mozilla",
+				"url": "https://t.co/DH73OGjmAW",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DH73OGjmAW",
+								"expanded_url": "http://www.brettgaylor.com",
+								"display_url": "brettgaylor.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5431,
+				"friends_count": 1140,
+				"listed_count": 448,
+				"created_at": "Wed Dec 03 22:36:45 +0000 2008",
+				"favourites_count": 2045,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 15297,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "0A0A0A",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/950529444740980736/ICqhiV-I_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/950529444740980736/ICqhiV-I_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17851599/1585003328",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1329486814558752800,
+			"quoted_status_id_str": "1329486814558752769",
+			"quoted_status_permalink": {
+				"url": "https://t.co/MBiMfxsC5g",
+				"expanded": "https://twitter.com/joshtpm/status/1329486814558752769",
+				"display": "twitter.com/joshtpm/status…"
+			},
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In a follow-up story, Martians have landed.... yes, at the Four Seasons Landscaping HQ. https://t.co/JdFpw2Lyrk"
+	},
+	{
 		"created_at": "Thu Nov 19 19:53:13 +0000 2020",
 		"id": 1329513075834200000,
 		"id_str": "1329513075834200071",

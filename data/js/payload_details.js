17f00f75672757394bb2819ba3d9f33aece1081c
Alan Levine <cogdogblog@gmail.com>
2020-11-23 11:15:12 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91463,
-	"created_at": "Mon Nov 23 16:15:09 +0000 2020",
+	"tweets": 91464,
+	"created_at": "Mon Nov 23 17:15:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 23 16:54:44 +0000 2020",
+		"id": 1330917713305190400,
+		"id_str": "1330917713305190406",
+		"full_text": "You are invited (all y'all) to the @BCcampus H5P/Pressbooks Kitchen webinar this Thursday (9am PT) to see examples of the H5P activities cooked up for five funded open textbook projects. Details and signup link at  https://t.co/En0zhPhE2z https://t.co/TMqDhIRzk9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			238
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BCcampus",
+					"name": "BCcampus",
+					"id": 93710949,
+					"id_str": "93710949",
+					"indices": [
+						35,
+						44
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/En0zhPhE2z",
+					"expanded_url": "https://kitchen.opened.ca/2020/11/09/november-webinar/",
+					"display_url": "kitchen.opened.ca/2020/11/09/nov…",
+					"indices": [
+						215,
+						238
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1330917511982813200,
+					"id_str": "1330917511982813184",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EnhebPcVEAA7ivN.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EnhebPcVEAA7ivN.jpg",
+					"url": "https://t.co/TMqDhIRzk9",
+					"display_url": "pic.twitter.com/TMqDhIRzk9",
+					"expanded_url": "https://twitter.com/cogdog/status/1330917713305190406/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 222,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 222,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 222,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1330917511982813200,
+					"id_str": "1330917511982813184",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EnhebPcVEAA7ivN.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EnhebPcVEAA7ivN.jpg",
+					"url": "https://t.co/TMqDhIRzk9",
+					"display_url": "pic.twitter.com/TMqDhIRzk9",
+					"expanded_url": "https://twitter.com/cogdog/status/1330917713305190406/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 222,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 222,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 222,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							80,
+							37
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EnhebPcVEAA7ivN.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10546,
+			"friends_count": 1540,
+			"listed_count": 793,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 867,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92007,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "You are invited (all y'all) to the @BCcampus H5P/Pressbooks Kitchen webinar this Thursday (9am PT) to see examples of the H5P activities cooked up for five funded open textbook projects. Details and signup link at  https://t.co/En0zhPhE2z https://t.co/TMqDhIRzk9"
+	},
+	{
 		"created_at": "Mon Nov 23 16:09:33 +0000 2020",
 		"id": 1330906340311539700,
 		"id_str": "1330906340311539712",

5f59ee466eb3560a3afd179752a50da9e893bb80
Alan Levine <cogdogblog@gmail.com>
2020-11-25 07:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91479,
-	"created_at": "Wed Nov 25 04:15:10 +0000 2020",
+	"tweets": 91480,
+	"created_at": "Wed Nov 25 13:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 25 12:56:48 +0000 2020",
+		"id": 1331582611693600800,
+		"id_str": "1331582611693600771",
+		"full_text": "ITS NOT A TOWN WITH A FOUR SEASONS BUT RUDY HAS MAPPED OUT GREAT OPTIONS FOR MY ADDRESS. SAY, HOW MANY FAKE VOTES ARE THERE IN “FOUR SCORE AND SEVEN”?? https://t.co/3Jmu9KAMis",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1331582597793603600,
+					"id_str": "1331582597793603584",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/Enq7UVKUYAAPCOW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Enq7UVKUYAAPCOW.jpg",
+					"url": "https://t.co/3Jmu9KAMis",
+					"display_url": "pic.twitter.com/3Jmu9KAMis",
+					"expanded_url": "https://twitter.com/cogdog/status/1331582611693600771/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1331582597793603600,
+					"id_str": "1331582597793603584",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/Enq7UVKUYAAPCOW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Enq7UVKUYAAPCOW.jpg",
+					"url": "https://t.co/3Jmu9KAMis",
+					"display_url": "pic.twitter.com/3Jmu9KAMis",
+					"expanded_url": "https://twitter.com/cogdog/status/1331582611693600771/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10546,
+			"friends_count": 1540,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 866,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92027,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "ITS NOT A TOWN WITH A FOUR SEASONS BUT RUDY HAS MAPPED OUT GREAT OPTIONS FOR MY ADDRESS. SAY, HOW MANY FAKE VOTES ARE THERE IN “FOUR SCORE AND SEVEN”?? https://t.co/3Jmu9KAMis"
+	},
+	{
 		"created_at": "Wed Nov 25 03:51:37 +0000 2020",
 		"id": 1331445412092289000,
 		"id_str": "1331445412092289030",

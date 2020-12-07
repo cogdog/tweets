d4d7b9694bf7a80e31d9af398611b1901b3331ed
Alan Levine <cogdogblog@gmail.com>
2020-12-07 10:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91591,
-	"created_at": "Mon Dec 07 15:15:10 +0000 2020",
+	"tweets": 91592,
+	"created_at": "Mon Dec 07 16:15:09 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 07 15:43:00 +0000 2020",
+		"id": 1335973091428552700,
+		"id_str": "1335973091428552707",
+		"full_text": "Here was zooming in 1998 using \"iVisit\" - an online meeting with 25 educators at Mesa Community College to 5 students and faculty at Seton Hall https://t.co/8yv67Bhwnh\n\nOf course no one seems to include community colleges in ed-tech histories... https://t.co/76DAgdnCRK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8yv67Bhwnh",
+					"expanded_url": "http://mcli.cogdogblog.com/eye/ivisit.html",
+					"display_url": "mcli.cogdogblog.com/eye/ivisit.html",
+					"indices": [
+						144,
+						167
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1335972945894678500,
+					"id_str": "1335972945894678528",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/EopUUOWVoAAqrxL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EopUUOWVoAAqrxL.jpg",
+					"url": "https://t.co/76DAgdnCRK",
+					"display_url": "pic.twitter.com/76DAgdnCRK",
+					"expanded_url": "https://twitter.com/cogdog/status/1335973091428552707/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 550,
+							"h": 415,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 550,
+							"h": 415,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 415,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1335972945894678500,
+					"id_str": "1335972945894678528",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/EopUUOWVoAAqrxL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EopUUOWVoAAqrxL.jpg",
+					"url": "https://t.co/76DAgdnCRK",
+					"display_url": "pic.twitter.com/76DAgdnCRK",
+					"expanded_url": "https://twitter.com/cogdog/status/1335973091428552707/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 550,
+							"h": 415,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 550,
+							"h": 415,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 415,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10541,
+			"friends_count": 1541,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 869,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92172,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Here was zooming in 1998 using \"iVisit\" - an online meeting with 25 educators at Mesa Community College to 5 students and faculty at Seton Hall https://t.co/8yv67Bhwnh\n\nOf course no one seems to include community colleges in ed-tech histories... https://t.co/76DAgdnCRK"
+	},
+	{
 		"created_at": "Mon Dec 07 15:09:27 +0000 2020",
 		"id": 1335964645354422300,
 		"id_str": "1335964645354422275",

1ef19fea5abaf2a0c1dee1929a6672c4c8a35113
Alan Levine <cogdogblog@gmail.com>
2020-12-07 20:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91603,
-	"created_at": "Tue Dec 08 01:15:10 +0000 2020",
+	"tweets": 91604,
+	"created_at": "Tue Dec 08 02:15:10 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Tue Dec 08 01:23:10 +0000 2020",
+		"id": 1336119092894261200,
+		"id_str": "1336119092894261248",
+		"full_text": "@edtechfactotum @brlamb @ken_bauer @mweller And painting wikis as a relict of the past web ignores its pervasive modern use in fan/howto sites e.g. Role of wikis in modern fan sites - https://t.co/vS6NXNZ7e4 https://t.co/TyznclUlXs  https://t.co/JBjO7Sosxj https://t.co/0pZ18aCfRg",
+		"truncated": false,
+		"display_text_range": [
+			44,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "edtechfactotum",
+					"name": "Clint Lalonde (he/him)",
+					"id": 822204322796609500,
+					"id_str": "822204322796609536",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "brlamb",
+					"name": "Brian Lamb",
+					"id": 745903,
+					"id_str": "745903",
+					"indices": [
+						16,
+						23
+					]
+				},
+				{
+					"screen_name": "ken_bauer",
+					"name": "Ken Bauer Favel",
+					"id": 16033586,
+					"id_str": "16033586",
+					"indices": [
+						24,
+						34
+					]
+				},
+				{
+					"screen_name": "mweller",
+					"name": "Martin Weller",
+					"id": 7127162,
+					"id_str": "7127162",
+					"indices": [
+						35,
+						43
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/vS6NXNZ7e4",
+					"expanded_url": "https://fandom.com/",
+					"display_url": "fandom.com",
+					"indices": [
+						184,
+						207
+					]
+				},
+				{
+					"url": "https://t.co/TyznclUlXs",
+					"expanded_url": "https://tvtropes.org/",
+					"display_url": "tvtropes.org",
+					"indices": [
+						208,
+						231
+					]
+				},
+				{
+					"url": "https://t.co/JBjO7Sosxj",
+					"expanded_url": "https://knowyourmeme.com/",
+					"display_url": "knowyourmeme.com",
+					"indices": [
+						233,
+						256
+					]
+				},
+				{
+					"url": "https://t.co/0pZ18aCfRg",
+					"expanded_url": "https://www.wikihow.com/",
+					"display_url": "wikihow.com",
+					"indices": [
+						257,
+						280
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1336118680619417600,
+		"in_reply_to_status_id_str": "1336118680619417602",
+		"in_reply_to_user_id": 740343,
+		"in_reply_to_user_id_str": "740343",
+		"in_reply_to_screen_name": "cogdog",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10544,
+			"friends_count": 1541,
+			"listed_count": 794,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 869,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92186,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@edtechfactotum @brlamb @ken_bauer @mweller And painting wikis as a relict of the past web ignores its pervasive modern use in fan/howto sites e.g. Role of wikis in modern fan sites - https://t.co/vS6NXNZ7e4 https://t.co/TyznclUlXs  https://t.co/JBjO7Sosxj https://t.co/0pZ18aCfRg"
+	},
+	{
 		"created_at": "Tue Dec 08 01:14:52 +0000 2020",
 		"id": 1336117006232260600,
 		"id_str": "1336117006232260608",

60fe1bdd0f4d58730fa6812433ca711759a89103
Alan Levine <cogdogblog@gmail.com>
2021-01-12 11:15:15 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91824,
-	"created_at": "Tue Jan 12 14:15:11 +0000 2021",
+	"tweets": 91825,
+	"created_at": "Tue Jan 12 17:15:14 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Tue Jan 12 16:40:52 +0000 2021",
+		"id": 1349032561075499000,
+		"id_str": "1349033615552770049",
+		"full_text": "this is always fun: when I snag the information from the official roster in order to set up my class spreadsheet (because the Gradebook is useless for my purposes), I snag the column with majors/minors of all the students and make a word cloud. here's our cloud for this semester: https://t.co/gjfLsMLWTh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1349032432767533000,
+					"id_str": "1349032432767533063",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/Eri52aGXAAcELOs.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eri52aGXAAcELOs.png",
+					"url": "https://t.co/gjfLsMLWTh",
+					"display_url": "pic.twitter.com/gjfLsMLWTh",
+					"expanded_url": "https://twitter.com/OnlineCrsLady/status/1349032561075499008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 879,
+							"h": 560,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 879,
+							"h": 560,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 433,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1349032432767533000,
+					"id_str": "1349032432767533063",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/Eri52aGXAAcELOs.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eri52aGXAAcELOs.png",
+					"url": "https://t.co/gjfLsMLWTh",
+					"display_url": "pic.twitter.com/gjfLsMLWTh",
+					"expanded_url": "https://twitter.com/OnlineCrsLady/status/1349032561075499008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 879,
+							"h": 560,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 879,
+							"h": 560,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 433,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7044082,
+			"id_str": "7044082",
+			"name": "Dr. Laura Gibbs",
+			"screen_name": "OnlineCrsLady",
+			"location": "the Interwebz",
+			"description": "Still teaching online at Univ. of Oklahoma, looking to OER future :-)\n#TotalCoLearner #Ungrading #UngradingSlowChat #TrustStudents #StayHomeSaveLives #BeyondLMS",
+			"url": "http://t.co/HAXevMhfOE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/HAXevMhfOE",
+							"expanded_url": "http://mythfolklore.net",
+							"display_url": "mythfolklore.net",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6021,
+			"friends_count": 2622,
+			"listed_count": 401,
+			"created_at": "Sun Jun 24 04:17:20 +0000 2007",
+			"favourites_count": 109649,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 100025,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "112E18",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2190857671/photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2190857671/photo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7044082/1607270725",
+			"profile_link_color": "196912",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "this is always fun: when I snag the information from the official roster in order to set up my class spreadsheet (because the Gradebook is useless for my purposes), I snag the column with majors/minors of all the students and make a word cloud. here's our cloud for this semester: https://t.co/gjfLsMLWTh"
+	},
+	{
 		"created_at": "Tue Jan 12 14:11:31 +0000 2021",
 		"id": 1348996030361075700,
 		"id_str": "1348996030361075714",

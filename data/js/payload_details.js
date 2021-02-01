816465c19e2612483095633489efe032f4dffc1c
Alan Levine <cogdogblog@gmail.com>
2021-01-31 22:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91958,
-	"created_at": "Sun Jan 31 21:15:10 +0000 2021",
+	"tweets": 91959,
+	"created_at": "Mon Feb 01 04:15:10 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,325 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Mon Feb 01 03:22:34 +0000 2021",
+		"id": 1356080472653144000,
+		"id_str": "1356080472653144064",
+		"full_text": "I approve of this GIF. I can't stop watching. This is #ds106 https://t.co/reYTvxli6x",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ds106",
+					"indices": [
+						54,
+						60
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/reYTvxli6x",
+					"expanded_url": "https://twitter.com/MBumblebean/status/1355946814348791808",
+					"display_url": "twitter.com/MBumblebean/st…",
+					"indices": [
+						61,
+						84
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10470,
+			"friends_count": 1531,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 876,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92641,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1355946814348791800,
+		"quoted_status_id_str": "1355946814348791808",
+		"quoted_status_permalink": {
+			"url": "https://t.co/reYTvxli6x",
+			"expanded": "https://twitter.com/MBumblebean/status/1355946814348791808",
+			"display": "twitter.com/MBumblebean/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jan 31 18:31:27 +0000 2021",
+			"id": 1355946814348791800,
+			"id_str": "1355946814348791808",
+			"full_text": "@ds106dc Bernie facing off against my cat :) #ds106 https://t.co/OzAj0BtAGM",
+			"truncated": false,
+			"display_text_range": [
+				9,
+				51
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "ds106",
+						"indices": [
+							45,
+							51
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ds106dc",
+						"name": "ds106 Daily Create",
+						"id": 995562187,
+						"id_str": "995562187",
+						"indices": [
+							0,
+							8
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1355946807595987000,
+						"id_str": "1355946807595986944",
+						"indices": [
+							52,
+							75
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/EtFKb_kVcAANmXe.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EtFKb_kVcAANmXe.jpg",
+						"url": "https://t.co/OzAj0BtAGM",
+						"display_url": "pic.twitter.com/OzAj0BtAGM",
+						"expanded_url": "https://twitter.com/MBumblebean/status/1355946814348791808/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 480,
+								"h": 398,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 480,
+								"h": 398,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 480,
+								"h": 398,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1355946807595987000,
+						"id_str": "1355946807595986944",
+						"indices": [
+							52,
+							75
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/EtFKb_kVcAANmXe.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EtFKb_kVcAANmXe.jpg",
+						"url": "https://t.co/OzAj0BtAGM",
+						"display_url": "pic.twitter.com/OzAj0BtAGM",
+						"expanded_url": "https://twitter.com/MBumblebean/status/1355946814348791808/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 480,
+								"h": 398,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 480,
+								"h": 398,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 480,
+								"h": 398,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								240,
+								199
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/EtFKb_kVcAANmXe.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": 1354391214091452400,
+			"in_reply_to_status_id_str": "1354391214091452418",
+			"in_reply_to_user_id": 995562187,
+			"in_reply_to_user_id_str": "995562187",
+			"in_reply_to_screen_name": "ds106dc",
+			"user": {
+				"id": 1350857113170370600,
+				"id_str": "1350857113170370560",
+				"name": "Mrs.Bumblebean",
+				"screen_name": "MBumblebean",
+				"location": "",
+				"description": "Graduate ELA student in the Masters Program",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 0,
+				"friends_count": 2,
+				"listed_count": 0,
+				"created_at": "Sun Jan 17 17:27:08 +0000 2021",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+				"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": true,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I approve of this GIF. I can't stop watching. This is #ds106 https://t.co/reYTvxli6x"
+	}
+]

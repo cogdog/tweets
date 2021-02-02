8d6c7670b34e215b5d5a4762a0d8c65f7f75e2f5
Alan Levine <cogdogblog@gmail.com>
2021-02-02 11:15:10 -0600

    Added 1 tweets

--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,161 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Tue Feb 02 17:14:03 +0000 2021",
+		"id": 1356652110624952300,
+		"id_str": "1356652110624952320",
+		"full_text": "@JohnStewartPhD I use https://t.co/C7vVPcc4yr for podcast transcriptions, it's good (and apparently now built into Google Meet). At @oeglobal we used https://t.co/iLCuUag8F7 for our Nov conference ($20/month for like 80 hours) was as ok as AI gets (I believe it uses https://t.co/C7vVPcc4yr)",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			291
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JohnStewartPhD",
+					"name": "John Stewart",
+					"id": 73977188,
+					"id_str": "73977188",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "oeglobal",
+					"name": "Richard Brown",
+					"id": 257137502,
+					"id_str": "257137502",
+					"indices": [
+						132,
+						141
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/C7vVPcc4yr",
+					"expanded_url": "http://otter.ai",
+					"display_url": "otter.ai",
+					"indices": [
+						22,
+						45
+					]
+				},
+				{
+					"url": "https://t.co/iLCuUag8F7",
+					"expanded_url": "http://rev.com",
+					"display_url": "rev.com",
+					"indices": [
+						150,
+						173
+					]
+				},
+				{
+					"url": "https://t.co/C7vVPcc4yr",
+					"expanded_url": "http://otter.ai",
+					"display_url": "otter.ai",
+					"indices": [
+						267,
+						290
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1356650656350802000,
+		"in_reply_to_status_id_str": "1356650656350801921",
+		"in_reply_to_user_id": 73977188,
+		"in_reply_to_user_id_str": "73977188",
+		"in_reply_to_screen_name": "JohnStewartPhD",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10474,
+			"friends_count": 1531,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 876,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92660,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@JohnStewartPhD I use https://t.co/C7vVPcc4yr for podcast transcriptions, it's good (and apparently now built into Google Meet). At @oeglobal we used https://t.co/iLCuUag8F7 for our Nov conference ($20/month for like 80 hours) was as ok as AI gets (I believe it uses https://t.co/C7vVPcc4yr)"
+	},
+	{
 		"created_at": "Tue Feb 02 14:02:58 +0000 2021",
 		"id": 1356604023592210400,
 		"id_str": "1356604023592210433",

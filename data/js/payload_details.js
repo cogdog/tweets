99e979ed6c9a4a45c14d90874d04808b1cefccf5
Alan Levine <cogdogblog@gmail.com>
2021-02-22 10:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92030,
-	"created_at": "Mon Feb 22 15:15:09 +0000 2021",
+	"tweets": 92031,
+	"created_at": "Mon Feb 22 16:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,150 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Mon Feb 22 15:56:17 +0000 2021",
+		"id": 1363880296962171000,
+		"id_str": "1363880296962170880",
+		"full_text": "@KUWSP @KeanUniversity @KeanEnglish 10th grade English teacher, Mrs Kirschman, Baltimore, 1980... changed everything with reading essays of Emerson's Self-Reliance and Nonconformity and Thoreau's Walden changed *everything* for me. #netnarr",
+		"truncated": false,
+		"display_text_range": [
+			36,
+			240
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						232,
+						240
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KUWSP",
+					"name": "Kean Writing Studies",
+					"id": 3003321917,
+					"id_str": "3003321917",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"screen_name": "KeanUniversity",
+					"name": "Kean University",
+					"id": 42666033,
+					"id_str": "42666033",
+					"indices": [
+						7,
+						22
+					]
+				},
+				{
+					"screen_name": "KeanEnglish",
+					"name": "English Studies at Kean University",
+					"id": 56834210,
+					"id_str": "56834210",
+					"indices": [
+						23,
+						35
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1363861096411631600,
+		"in_reply_to_status_id_str": "1363861096411631619",
+		"in_reply_to_user_id": 3003321917,
+		"in_reply_to_user_id_str": "3003321917",
+		"in_reply_to_screen_name": "KUWSP",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10482,
+			"friends_count": 1536,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 879,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92732,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@KUWSP @KeanUniversity @KeanEnglish 10th grade English teacher, Mrs Kirschman, Baltimore, 1980... changed everything with reading essays of Emerson's Self-Reliance and Nonconformity and Thoreau's Walden changed *everything* for me. #netnarr"
+	},
+	{
 		"created_at": "Mon Feb 22 14:15:48 +0000 2021",
 		"id": 1363855009104846800,
 		"id_str": "1363855009104846853",

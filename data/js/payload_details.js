483d95abf311956e9a5453f6467fefff54a6d529
Alan Levine <cogdogblog@gmail.com>
2021-02-25 14:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92047,
-	"created_at": "Thu Feb 25 19:15:09 +0000 2021",
+	"tweets": 92048,
+	"created_at": "Thu Feb 25 20:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Thu Feb 25 19:42:06 +0000 2021",
+		"id": 1365024291478151200,
+		"id_str": "1365024291478151168",
+		"full_text": "8 folks are at a perfect 100% (56/56) on the #ds106 Daily Create leaderboard https://t.co/8K4wgyfOPa \n\nKudos to @NomadWarMachine @grammasheri @AnnyCow @ronald_2008 @ProfCaritat @JaapSoft @Untertacker @dogtrax \n\n#TheyAreTheChampions\n\nI'm down 12 but will make up for them (maybe)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ds106",
+					"indices": [
+						45,
+						51
+					]
+				},
+				{
+					"text": "TheyAreTheChampions",
+					"indices": [
+						211,
+						231
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NomadWarMachine",
+					"name": "Sarah Honeychurch",
+					"id": 558091832,
+					"id_str": "558091832",
+					"indices": [
+						112,
+						128
+					]
+				},
+				{
+					"screen_name": "grammasheri",
+					"name": "Sheri Edwards",
+					"id": 8373192,
+					"id_str": "8373192",
+					"indices": [
+						129,
+						141
+					]
+				},
+				{
+					"screen_name": "AnnyCow",
+					"name": "AnnaCow",
+					"id": 2389814168,
+					"id_str": "2389814168",
+					"indices": [
+						142,
+						150
+					]
+				},
+				{
+					"screen_name": "ronald_2008",
+					"name": "Ronald_2008 🤔",
+					"id": 211458710,
+					"id_str": "211458710",
+					"indices": [
+						151,
+						163
+					]
+				},
+				{
+					"screen_name": "ProfCaritat",
+					"name": "profCaritat",
+					"id": 4149181692,
+					"id_str": "4149181692",
+					"indices": [
+						164,
+						176
+					]
+				},
+				{
+					"screen_name": "JaapSoft",
+					"name": "Jaap Bosman",
+					"id": 17039585,
+					"id_str": "17039585",
+					"indices": [
+						177,
+						186
+					]
+				},
+				{
+					"screen_name": "Untertacker",
+					"name": "Under Taker",
+					"id": 4688709284,
+					"id_str": "4688709284",
+					"indices": [
+						187,
+						199
+					]
+				},
+				{
+					"screen_name": "dogtrax",
+					"name": "KevinHodgson",
+					"id": 13307352,
+					"id_str": "13307352",
+					"indices": [
+						200,
+						208
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8K4wgyfOPa",
+					"expanded_url": "https://daily.ds106.us/leaders/",
+					"display_url": "daily.ds106.us/leaders/",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10485,
+			"friends_count": 1536,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 879,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92752,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "8 folks are at a perfect 100% (56/56) on the #ds106 Daily Create leaderboard https://t.co/8K4wgyfOPa \n\nKudos to @NomadWarMachine @grammasheri @AnnyCow @ronald_2008 @ProfCaritat @JaapSoft @Untertacker @dogtrax \n\n#TheyAreTheChampions\n\nI'm down 12 but will make up for them (maybe)"
+	},
+	{
 		"created_at": "Thu Feb 25 18:38:13 +0000 2021",
 		"id": 1365008213851136000,
 		"id_str": "1365008213851136002",

787512dbd7fd518003680bb521378e667d75bb34
Alan Levine <cogdogblog@gmail.com>
2021-02-26 11:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92052,
-	"created_at": "Fri Feb 26 15:15:08 +0000 2021",
+	"tweets": 92053,
+	"created_at": "Fri Feb 26 17:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Fri Feb 26 17:11:41 +0000 2021",
+		"id": 1365348826106634200,
+		"id_str": "1365348826106634240",
+		"full_text": "@ammienoot @edtechfactotum @greeneterry @MiaZamoraPhD You just cannot buy notoriety (well the kind worth having)",
+		"truncated": false,
+		"display_text_range": [
+			54,
+			112
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ammienoot",
+					"name": "Anne-Marie Scott",
+					"id": 22673907,
+					"id_str": "22673907",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "edtechfactotum",
+					"name": "Clint Lalonde (he/him)",
+					"id": 822204322796609500,
+					"id_str": "822204322796609536",
+					"indices": [
+						11,
+						26
+					]
+				},
+				{
+					"screen_name": "greeneterry",
+					"name": "Terry Greene (He/Him)",
+					"id": 212019344,
+					"id_str": "212019344",
+					"indices": [
+						27,
+						39
+					]
+				},
+				{
+					"screen_name": "MiaZamoraPhD",
+					"name": "Mia Zamora",
+					"id": 579873574,
+					"id_str": "579873574",
+					"indices": [
+						40,
+						53
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1365346882080280600,
+		"in_reply_to_status_id_str": "1365346882080280576",
+		"in_reply_to_user_id": 22673907,
+		"in_reply_to_user_id_str": "22673907",
+		"in_reply_to_screen_name": "ammienoot",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10487,
+			"friends_count": 1536,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 879,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92760,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ammienoot @edtechfactotum @greeneterry @MiaZamoraPhD You just cannot buy notoriety (well the kind worth having)"
+	},
+	{
 		"created_at": "Fri Feb 26 14:52:23 +0000 2021",
 		"id": 1365313770365415400,
 		"id_str": "1365313770365415425",
@@ -130,7 +276,7 @@ Grailbird.data.tweets_2021_02 =
 			"created_at": "Fri Feb 26 03:55:39 +0000 2021",
 			"id": 1365148498509791200,
 			"id_str": "1365148498509791234",
-			"full_text": "Hello everyone, thank you for supporting my #SpaceArt &amp; #Poetry journey. I hope you’ll check out my @Shift4Shop: https://t.co/pUOcwAwmE1 &amp; help share my #inspiration4contest message &amp; #poem. Let’s unleash our #Space2inspire &amp; create a J.E.D.I. future for all of humanity. ❤️🌍👩🏾‍🚀🚀 https://t.co/JzRf6ayGcL",
+			"full_text": "Hello everyone, thank you for supporting my #SpaceArt &amp; #Poetry journey. I hope you’ll check out my @Shift4Shop: https://t.co/pUOcwAwmE1 &amp; help share my #inspiration4contest message &amp; #poem. Let’s unleash our #Space2inspire &amp; create a J.E.D.I. future for all of humanity. ❤️🌍👩🏾🚀🚀 https://t.co/JzRf6ayGcL",
 			"truncated": false,
 			"display_text_range": [
 				0,
@@ -320,7 +466,7 @@ Grailbird.data.tweets_2021_02 =
 				"name": "Doctor Proctor",
 				"screen_name": "DrSianProctor",
 				"location": "Phoenix, AZ",
-				"description": "🚀#Space2inspire 👩🏽‍🚀#AnalogAstronaut👩🏽‍🍳#SpaceChef 🌍 #GeoExplorer 🪐#SpaceFashionFriday 🧑🏽‍🎨#AfronautSpace Art 📺 #SciComm @pastpreservers #JEDIspace",
+				"description": "🚀#Space2inspire 👩🏽🚀#AnalogAstronaut👩🏽🍳#SpaceChef 🌍 #GeoExplorer 🪐#SpaceFashionFriday 🧑🏽🎨#AfronautSpace Art 📺 #SciComm @pastpreservers #JEDIspace",
 				"url": "https://t.co/r2G2JsVQl2",
 				"entities": {
 					"url": {

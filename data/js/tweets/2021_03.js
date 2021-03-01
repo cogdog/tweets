a7345d61da239ad52e487edc0a6866757fa57990
Alan Levine <cogdogblog@gmail.com>
2021-02-28 20:15:10 -0600

    Added 1 tweets

---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,150 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Mon Mar 01 01:22:05 +0000 2021",
+		"id": 1366197014854176800,
+		"id_str": "1366197014854176768",
+		"full_text": "@edtechfactotum @laurapasquini Note- Helen Barrett @eportfolios is self-proclaimed \"grandmother\" (not godmother) of e-portfolios. She was an influence too on digital storytelling (Hi Helen!)\n\nI met her ~1996 at a NECC conference in Baltimore had her as a speaker at Maricopa in 2004 https://t.co/8Z8Q9HJNBh",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			306
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "edtechfactotum",
+					"name": "Clint Lalonde (he/him)",
+					"id": 822204322796609500,
+					"id_str": "822204322796609536",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "laurapasquini",
+					"name": "Laura Pasquini, Ph.D.",
+					"id": 16708242,
+					"id_str": "16708242",
+					"indices": [
+						16,
+						30
+					]
+				},
+				{
+					"screen_name": "eportfolios",
+					"name": "Helen Barrett",
+					"id": 11725592,
+					"id_str": "11725592",
+					"indices": [
+						51,
+						63
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8Z8Q9HJNBh",
+					"expanded_url": "https://cogdogblog.com/2004/12/helens-online/",
+					"display_url": "cogdogblog.com/2004/12/helens…",
+					"indices": [
+						283,
+						306
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1366194715285053400,
+		"in_reply_to_status_id_str": "1366194715285053440",
+		"in_reply_to_user_id": 822204322796609500,
+		"in_reply_to_user_id_str": "822204322796609536",
+		"in_reply_to_screen_name": "edtechfactotum",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10488,
+			"friends_count": 1536,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 879,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92791,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@edtechfactotum @laurapasquini Note- Helen Barrett @eportfolios is self-proclaimed \"grandmother\" (not godmother) of e-portfolios. She was an influence too on digital storytelling (Hi Helen!)\n\nI met her ~1996 at a NECC conference in Baltimore had her as a speaker at Maricopa in 2004 https://t.co/8Z8Q9HJNBh"
+	}
+]

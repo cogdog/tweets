4860f4772ef4f87d7e61a7c03b864f30aae26bf4
Alan Levine <cogdogblog@gmail.com>
2021-03-09 13:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92123,
-	"created_at": "Tue Mar 09 17:15:09 +0000 2021",
+	"tweets": 92124,
+	"created_at": "Tue Mar 09 19:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 09 19:12:41 +0000 2021",
+		"id": 1369294863540363300,
+		"id_str": "1369365543522820098",
+		"full_text": "Episode 4 of the Banton Biggies Podcast is out now. Scripted &amp; recorded by pupils in their home and edited together by the children in school all in less than a week.\n\nhttps://t.co/oi8sPAey8X",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oi8sPAey8X",
+					"expanded_url": "https://blogs.glowscotland.org.uk/nl/bbpod/2021/03/09/bbp-episode-4/",
+					"display_url": "blogs.glowscotland.org.uk/nl/bbpod/2021/…",
+					"indices": [
+						172,
+						195
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 702565959354163200,
+			"id_str": "702565959354163200",
+			"name": "Banton Primary",
+			"screen_name": "Banton_Pr",
+			"location": "Banton, Scotland",
+			"description": "A Small Primary School in North Lanarkshire",
+			"url": "https://t.co/WNBQyt3iF5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WNBQyt3iF5",
+							"expanded_url": "https://blogs.glowscotland.org.uk/nl/banton/",
+							"display_url": "blogs.glowscotland.org.uk/nl/banton/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 423,
+			"friends_count": 81,
+			"listed_count": 8,
+			"created_at": "Wed Feb 24 18:49:12 +0000 2016",
+			"favourites_count": 689,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1285,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/702578403564773376/V51uWMrV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/702578403564773376/V51uWMrV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/702565959354163200/1456342883",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Episode 4 of the Banton Biggies Podcast is out now. Scripted &amp; recorded by pupils in their home and edited together by the children in school all in less than a week.\n\nhttps://t.co/oi8sPAey8X"
+	},
+	{
 		"created_at": "Tue Mar 09 17:07:31 +0000 2021",
 		"id": 1369334041556570000,
 		"id_str": "1369334041556570112",

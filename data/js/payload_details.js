0e02292ad7afccf6a737c00611fd639258a628c3
Alan Levine <cogdogblog@gmail.com>
2021-03-30 14:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92272,
-	"created_at": "Tue Mar 30 17:15:10 +0000 2021",
+	"tweets": 92273,
+	"created_at": "Tue Mar 30 20:15:10 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 30 19:15:14 +0000 2021",
+		"id": 1376976330307248000,
+		"id_str": "1376976330307248130",
+		"full_text": "Love seeing... er hearing... these student produced podcasts... https://t.co/YzTXGGmz43",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YzTXGGmz43",
+					"expanded_url": "https://twitter.com/soul4real/status/1376966709525934080",
+					"display_url": "twitter.com/soul4real/stat…",
+					"indices": [
+						64,
+						87
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10491,
+			"friends_count": 1535,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 883,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93034,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1376966709525934000,
+		"quoted_status_id_str": "1376966709525934080",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YzTXGGmz43",
+			"expanded": "https://twitter.com/soul4real/status/1376966709525934080",
+			"display": "twitter.com/soul4real/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Mar 30 18:37:01 +0000 2021",
+			"id": 1376966709525934000,
+			"id_str": "1376966709525934080",
+			"full_text": "My students are still killin' it! New podcast episode. https://t.co/oaiiGWEy96",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				54
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/oaiiGWEy96",
+						"expanded_url": "https://twitter.com/JRN203GCC/status/1376961763082559488",
+						"display_url": "twitter.com/JRN203GCC/stat…",
+						"indices": [
+							55,
+							78
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2239931,
+				"id_str": "2239931",
+				"name": "Coop",
+				"screen_name": "soul4real",
+				"location": "Phoenix, AZ (Hot!)",
+				"description": "I am an instructional technologist who just happens to teach Freshman Composition at a community college in AZ. Yes, I am!",
+				"url": "http://t.co/e1oHbb5AC8",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/e1oHbb5AC8",
+								"expanded_url": "http://soul4real.me/",
+								"display_url": "soul4real.me",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 905,
+				"friends_count": 343,
+				"listed_count": 94,
+				"created_at": "Mon Mar 26 02:23:55 +0000 2007",
+				"favourites_count": 465,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 31346,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1349768475296714752/rYlSXcr5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1349768475296714752/rYlSXcr5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2239931/1422661588",
+				"profile_link_color": "990000",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F3F3F3",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1376961763082559500,
+			"quoted_status_id_str": "1376961763082559488",
+			"quoted_status_permalink": {
+				"url": "https://t.co/oaiiGWEy96",
+				"expanded": "https://twitter.com/JRN203GCC/status/1376961763082559488",
+				"display": "twitter.com/JRN203GCC/stat…"
+			},
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Love seeing... er hearing... these student produced podcasts... https://t.co/YzTXGGmz43"
+	},
+	{
 		"created_at": "Tue Mar 30 16:35:22 +0000 2021",
 		"id": 1376936095686946800,
 		"id_str": "1376936095686946820",

156fa26a987630408abda9fc688b39f9d472cfc0
Alan Levine <cogdogblog@gmail.com>
2021-04-09 21:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92301,
-	"created_at": "Sat Apr 10 01:15:08 +0000 2021",
+	"tweets": 92302,
+	"created_at": "Sat Apr 10 03:15:08 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,261 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sat Apr 10 03:14:45 +0000 2021",
+		"id": 1380720881270333400,
+		"id_str": "1380720881270333440",
+		"full_text": "Hey #netnarr This has been our course topic. Are you ready to offer ideas? @brennacgray is coming to visit class soon https://t.co/K77fRvH7wq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						4,
+						12
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "brennacgray",
+					"name": "The Jackie Weaver of EdTech",
+					"id": 14109848,
+					"id_str": "14109848",
+					"indices": [
+						75,
+						87
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/K77fRvH7wq",
+					"expanded_url": "https://twitter.com/brennacgray/status/1380704746692956160",
+					"display_url": "twitter.com/brennacgray/st…",
+					"indices": [
+						118,
+						141
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10486,
+			"friends_count": 1535,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 885,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93068,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1380704746692956200,
+		"quoted_status_id_str": "1380704746692956160",
+		"quoted_status_permalink": {
+			"url": "https://t.co/K77fRvH7wq",
+			"expanded": "https://twitter.com/brennacgray/status/1380704746692956160",
+			"display": "twitter.com/brennacgray/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 10 02:10:38 +0000 2021",
+			"id": 1380704746692956200,
+			"id_str": "1380704746692956160",
+			"full_text": "Pals! My keynote for #CascadiaOpenEd is titled “Manifesto for the Future: My Hopes for the Post-Pandemic University” — and I want to know about your hopes. What comes next? Share with me, please, as I finish drafting my remarks? @BCcampus (pls RT?)",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				248
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "CascadiaOpenEd",
+						"indices": [
+							21,
+							36
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "BCcampus",
+						"name": "BCcampus",
+						"id": 93710949,
+						"id_str": "93710949",
+						"indices": [
+							229,
+							238
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14109848,
+				"id_str": "14109848",
+				"name": "The Jackie Weaver of EdTech",
+				"screen_name": "brennacgray",
+				"location": "Tk’emlups te Secwepemc",
+				"description": "Manic pixie dream academic; doozer with a PhD. I put the care back in “but I don’t care about EdTech.” Feminist, settler, antiracist. Co-host #HKHSPod. She/her.",
+				"url": "https://t.co/mumGuRnXTk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mumGuRnXTk",
+								"expanded_url": "http://blog.communityofpraxis.ca/",
+								"display_url": "blog.communityofpraxis.ca",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4136,
+				"friends_count": 1661,
+				"listed_count": 143,
+				"created_at": "Sun Mar 09 22:29:19 +0000 2008",
+				"favourites_count": 16763,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 63584,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme19/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme19/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1097321857110294528/xiPS2QUn_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1097321857110294528/xiPS2QUn_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14109848/1606427385",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "61464F",
+				"profile_text_color": "3C8059",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hey #netnarr This has been our course topic. Are you ready to offer ideas? @brennacgray is coming to visit class soon https://t.co/K77fRvH7wq"
+	},
+	{
 		"created_at": "Sat Apr 10 00:37:03 +0000 2021",
 		"id": 1380681193574727700,
 		"id_str": "1380681193574727683",

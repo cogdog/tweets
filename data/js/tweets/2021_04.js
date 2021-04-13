ea3fd12b9ca2dd345d7364c5a7f9ff9ee641147c
Alan Levine <cogdogblog@gmail.com>
2021-04-12 20:15:08 -0600

    Added 1 tweets

--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 13 01:39:32 +0000 2021",
+		"id": 1381784082586083300,
+		"id_str": "1381784082586083331",
+		"full_text": "Go Get Gasta! https://t.co/TIDRoVBbr3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TIDRoVBbr3",
+					"expanded_url": "https://twitter.com/TomFarrelly/status/1381541315826085895",
+					"display_url": "twitter.com/TomFarrelly/st…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10487,
+			"friends_count": 1536,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 886,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93093,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1381541315826086000,
+		"quoted_status_id_str": "1381541315826085895",
+		"quoted_status_permalink": {
+			"url": "https://t.co/TIDRoVBbr3",
+			"expanded": "https://twitter.com/TomFarrelly/status/1381541315826085895",
+			"display": "twitter.com/TomFarrelly/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 12 09:34:52 +0000 2021",
+			"id": 1381541315826086000,
+			"id_str": "1381541315826085895",
+			"full_text": "Registration closing today for the #GastaGoesGlobal event tomorrow evening (6pm IST) Zoom link https://t.co/H4o9AZVgvA",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				118
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GastaGoesGlobal",
+						"indices": [
+							35,
+							51
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/H4o9AZVgvA",
+						"expanded_url": "http://gasta.me",
+						"display_url": "gasta.me",
+						"indices": [
+							95,
+							118
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 104772398,
+				"id_str": "104772398",
+				"name": "Tom Farrelly",
+				"screen_name": "TomFarrelly",
+				"location": "Firies, Killarney, Co. Kerry ",
+				"description": "Dub in the Kingdom. Lecturer in @MTU_ie  “Being Irish, he had an abiding sense of tragedy, which sustained him through temporary periods of joy” – WB Yeats.",
+				"url": "https://t.co/bo7vKDhIbn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bo7vKDhIbn",
+								"expanded_url": "https://www.researchgate.net/profile/Tom_Farrelly/research",
+								"display_url": "researchgate.net/profile/Tom_Fa…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1371,
+				"friends_count": 838,
+				"listed_count": 28,
+				"created_at": "Thu Jan 14 10:20:42 +0000 2010",
+				"favourites_count": 16477,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5148,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "352726",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1352333257329807361/RjzR23bq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1352333257329807361/RjzR23bq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/104772398/1611256304",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "1147023b519541b3",
+				"url": "https://api.twitter.com/1.1/geo/id/1147023b519541b3.json",
+				"place_type": "city",
+				"name": "Mayo",
+				"full_name": "Mayo, Ireland",
+				"country_code": "IE",
+				"country": "Ireland",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-10.2576986,
+								53.4726027
+							],
+							[
+								-8.5829761,
+								53.4726027
+							],
+							[
+								-8.5829761,
+								54.345902
+							],
+							[
+								-10.2576986,
+								54.345902
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 15,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Go Get Gasta! https://t.co/TIDRoVBbr3"
+	},
+	{
 		"created_at": "Mon Apr 12 22:32:13 +0000 2021",
 		"id": 1381736945127104500,
 		"id_str": "1381736945127104513",

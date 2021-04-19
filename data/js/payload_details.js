e4d0e9ea9007cc42a601f5fc099f3c245b3f748c
Alan Levine <cogdogblog@gmail.com>
2021-04-19 14:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92351,
-	"created_at": "Mon Apr 19 18:15:09 +0000 2021",
+	"tweets": 92352,
+	"created_at": "Mon Apr 19 20:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 19 19:46:34 +0000 2021",
+		"id": 1384231973791494100,
+		"id_str": "1384231973791494152",
+		"full_text": "@brennacgray Been whinging this since 2013 https://t.co/n1MWGIYvCA \n\nno,  2008 https://t.co/RiBMZ1msio\n\nmaybe 2004 https://t.co/BTiORi1GHP\n\nforever...",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "brennacgray",
+					"name": "The Jackie Weaver of EdTech",
+					"id": 14109848,
+					"id_str": "14109848",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/n1MWGIYvCA",
+					"expanded_url": "https://cogdogblog.com/2013/11/this/",
+					"display_url": "cogdogblog.com/2013/11/this/",
+					"indices": [
+						43,
+						66
+					]
+				},
+				{
+					"url": "https://t.co/RiBMZ1msio",
+					"expanded_url": "https://cogdogblog.com/2008/01/on-conferencing/",
+					"display_url": "cogdogblog.com/2008/01/on-con…",
+					"indices": [
+						79,
+						102
+					]
+				},
+				{
+					"url": "https://t.co/BTiORi1GHP",
+					"expanded_url": "https://cogdogblog.com/2004/10/conference-hangover/",
+					"display_url": "cogdogblog.com/2004/10/confer…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1384202806182092800,
+		"in_reply_to_status_id_str": "1384202806182092802",
+		"in_reply_to_user_id": 14109848,
+		"in_reply_to_user_id_str": "14109848",
+		"in_reply_to_screen_name": "brennacgray",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10484,
+			"friends_count": 1536,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 881,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93134,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@brennacgray Been whinging this since 2013 https://t.co/n1MWGIYvCA \n\nno,  2008 https://t.co/RiBMZ1msio\n\nmaybe 2004 https://t.co/BTiORi1GHP\n\nforever..."
+	},
+	{
 		"created_at": "Mon Apr 19 17:38:25 +0000 2021",
 		"id": 1384199722403958800,
 		"id_str": "1384199722403958789",

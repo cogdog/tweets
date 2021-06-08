018d62b373f26bbeb2fb269c39362e0754f75043
Alan Levine <cogdogblog@gmail.com>
2021-06-08 16:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92507,
-	"created_at": "Tue Jun 08 21:15:08 +0000 2021",
+	"tweets": 92508,
+	"created_at": "Tue Jun 08 22:15:08 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Tue Jun 08 22:09:31 +0000 2021",
+		"id": 1402387339310141400,
+		"id_str": "1402387339310141440",
+		"full_text": "@edwsonoma My Geology degree has some use. \n\nLargest watershed in the US feeds the Mississippi, so I put my drop on the eastern flank of the Rockies near aptly named \"Two Dot\" Montana, and gave it a 5400km ride to the Gulf of Mexico (well it's still going) https://t.co/6b0t13ZVp2 https://t.co/TZCSAt9SIa",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "edwsonoma",
+					"name": "Ellen Wagner",
+					"id": 18120676,
+					"id_str": "18120676",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6b0t13ZVp2",
+					"expanded_url": "https://river-runner.samlearner.com/",
+					"display_url": "river-runner.samlearner.com",
+					"indices": [
+						257,
+						280
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1402387247568146400,
+					"id_str": "1402387247568146435",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/E3ZHxYDWUAM_0gU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3ZHxYDWUAM_0gU.jpg",
+					"url": "https://t.co/TZCSAt9SIa",
+					"display_url": "pic.twitter.com/TZCSAt9SIa",
+					"expanded_url": "https://twitter.com/cogdog/status/1402387339310141440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 646,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1103,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 366,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1402387247568146400,
+					"id_str": "1402387247568146435",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/E3ZHxYDWUAM_0gU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3ZHxYDWUAM_0gU.jpg",
+					"url": "https://t.co/TZCSAt9SIa",
+					"display_url": "pic.twitter.com/TZCSAt9SIa",
+					"expanded_url": "https://twitter.com/cogdog/status/1402387339310141440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 646,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1103,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 366,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1402373557850722300,
+		"in_reply_to_status_id_str": "1402373557850722306",
+		"in_reply_to_user_id": 740343,
+		"in_reply_to_user_id_str": "740343",
+		"in_reply_to_screen_name": "cogdog",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10435,
+			"friends_count": 1532,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 887,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93321,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@edwsonoma My Geology degree has some use. \n\nLargest watershed in the US feeds the Mississippi, so I put my drop on the eastern flank of the Rockies near aptly named \"Two Dot\" Montana, and gave it a 5400km ride to the Gulf of Mexico (well it's still going) https://t.co/6b0t13ZVp2 https://t.co/TZCSAt9SIa"
+	},
+	{
 		"created_at": "Tue Jun 08 21:14:45 +0000 2021",
 		"id": 1402373557850722300,
 		"id_str": "1402373557850722306",

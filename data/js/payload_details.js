6088b9dd977cd233c9d1dc0e1d6e1742f93fe541
Alan Levine <cogdogblog@gmail.com>
2021-06-21 15:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92561,
-	"created_at": "Mon Jun 21 20:15:09 +0000 2021",
+	"tweets": 92562,
+	"created_at": "Mon Jun 21 21:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 21 21:13:56 +0000 2021",
+		"id": 1407084393856999400,
+		"id_str": "1407084393856999428",
+		"full_text": "A quick fiddle to add \"back\" navigation to H5P Branching Scenario. This for a crazy complex one created to provide simulation based on Student Textbook game designed by students of @Arley_McNeney  https://t.co/AbWGJPP3sJ \n\nBut now Preet can click \"back\"! https://t.co/Z4WWGnH4Ll",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Arley_McNeney",
+					"name": "Arley McNeney (Cruthers)",
+					"id": 391836441,
+					"id_str": "391836441",
+					"indices": [
+						181,
+						195
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AbWGJPP3sJ",
+					"expanded_url": "https://kpu.pressbooks.pub/gamelab/",
+					"display_url": "kpu.pressbooks.pub/gamelab/",
+					"indices": [
+						197,
+						220
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1407084285979512800,
+					"id_str": "1407084285979512836",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/E4b3s_rX0AQ3okX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4b3s_rX0AQ3okX.jpg",
+					"url": "https://t.co/Z4WWGnH4Ll",
+					"display_url": "pic.twitter.com/Z4WWGnH4Ll",
+					"expanded_url": "https://twitter.com/cogdog/status/1407084393856999428/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1564,
+							"h": 982,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 427,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 753,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1407084285979512800,
+					"id_str": "1407084285979512836",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/E4b3s_rX0AQ3okX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4b3s_rX0AQ3okX.jpg",
+					"url": "https://t.co/Z4WWGnH4Ll",
+					"display_url": "pic.twitter.com/Z4WWGnH4Ll",
+					"expanded_url": "https://twitter.com/cogdog/status/1407084393856999428/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1564,
+							"h": 982,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 427,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 753,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10423,
+			"friends_count": 1531,
+			"listed_count": 789,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 886,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93386,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A quick fiddle to add \"back\" navigation to H5P Branching Scenario. This for a crazy complex one created to provide simulation based on Student Textbook game designed by students of @Arley_McNeney  https://t.co/AbWGJPP3sJ \n\nBut now Preet can click \"back\"! https://t.co/Z4WWGnH4Ll"
+	},
+	{
 		"created_at": "Mon Jun 21 19:35:34 +0000 2021",
 		"id": 1407059639997436000,
 		"id_str": "1407059639997435913",

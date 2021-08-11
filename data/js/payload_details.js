73de79c31314536a0da88ce4ded48588c55b1b22
Alan Levine <cogdogblog@gmail.com>
2021-08-11 15:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92732,
-	"created_at": "Wed Aug 11 19:15:09 +0000 2021",
+	"tweets": 92733,
+	"created_at": "Wed Aug 11 21:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,236 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Wed Aug 11 21:14:30 +0000 2021",
+		"id": 1425566316539302000,
+		"id_str": "1425566316539301901",
+		"full_text": "This is not only some fantastic web-based storytelling but also creative parody work. And you can contribute via GitHub. https://t.co/zwa4JuSPJJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zwa4JuSPJJ",
+					"expanded_url": "https://twitter.com/notjessewalker/status/1425564781134614533",
+					"display_url": "twitter.com/notjessewalker…",
+					"indices": [
+						121,
+						144
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Mortlach, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10416,
+			"friends_count": 1535,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 892,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93601,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1425564781134614500,
+		"quoted_status_id_str": "1425564781134614533",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zwa4JuSPJJ",
+			"expanded": "https://twitter.com/notjessewalker/status/1425564781134614533",
+			"display": "twitter.com/notjessewalker…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Aug 11 21:08:24 +0000 2021",
+			"id": 1425564781134614500,
+			"id_str": "1425564781134614533",
+			"full_text": "\"Fictional movies within movies? Got 'em. Fake shows within shows? You bet. Browse our selection of over 400 stories within stories.\" https://t.co/DahwWHHlnP",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				157
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/DahwWHHlnP",
+						"expanded_url": "https://nestflix.fun/",
+						"display_url": "nestflix.fun",
+						"indices": [
+							134,
+							157
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 211031794,
+				"id_str": "211031794",
+				"name": "Jesse Walker",
+				"screen_name": "notjessewalker",
+				"location": "Baltimore",
+				"description": "Books editor at @reason; author of THE UNITED STATES OF PARANOIA (HarperCollins) and REBELS ON THE AIR (NYU Press). Stoopnocracy is peachy.",
+				"url": "http://t.co/mweqzae1rk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/mweqzae1rk",
+								"expanded_url": "http://www.jessewalker.blogspot.com",
+								"display_url": "jessewalker.blogspot.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11412,
+				"friends_count": 762,
+				"listed_count": 397,
+				"created_at": "Tue Nov 02 03:09:06 +0000 2010",
+				"favourites_count": 17741,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 69405,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/876626319123132421/SOUE_xE4_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/876626319123132421/SOUE_xE4_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/211031794/1401327307",
+				"profile_link_color": "001A99",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is not only some fantastic web-based storytelling but also creative parody work. And you can contribute via GitHub. https://t.co/zwa4JuSPJJ"
+	},
+	{
 		"created_at": "Wed Aug 11 18:23:11 +0000 2021",
 		"id": 1425523206031650800,
 		"id_str": "1425523206031650821",

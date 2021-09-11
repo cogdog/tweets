640c0505c74e600156a8755aab7e5498ac488b25
Alan Levine <cogdogblog@gmail.com>
2021-09-11 14:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92816,
-	"created_at": "Sat Sep 11 19:15:09 +0000 2021",
+	"tweets": 92817,
+	"created_at": "Sat Sep 11 20:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 11 19:38:03 +0000 2021",
+		"id": 1436776070871720000,
+		"id_str": "1436776070871719943",
+		"full_text": "@ggevalt @StephenPKiernan @eliast05 @SSTaylorBooks @laura_ritchie @abbene1 @LauraWMcCaffrey @anniecorreal @RobinMacArthur Great to hear from you always. Something might be in your mail “soon”",
+		"truncated": false,
+		"display_text_range": [
+			122,
+			191
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ggevalt",
+					"name": "Geoffrey Gevalt",
+					"id": 17316318,
+					"id_str": "17316318",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "StephenPKiernan",
+					"name": "Stephen Kiernan",
+					"id": 517089662,
+					"id_str": "517089662",
+					"indices": [
+						9,
+						25
+					]
+				},
+				{
+					"screen_name": "eliast05",
+					"name": "Tanya",
+					"id": 206925887,
+					"id_str": "206925887",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"screen_name": "SSTaylorBooks",
+					"name": "Sarah Stewart Taylor",
+					"id": 2424083426,
+					"id_str": "2424083426",
+					"indices": [
+						36,
+						50
+					]
+				},
+				{
+					"screen_name": "laura_ritchie",
+					"name": "laura ritchie",
+					"id": 17862408,
+					"id_str": "17862408",
+					"indices": [
+						51,
+						65
+					]
+				},
+				{
+					"screen_name": "abbene1",
+					"name": "Darcie Abbene",
+					"id": 579464097,
+					"id_str": "579464097",
+					"indices": [
+						66,
+						74
+					]
+				},
+				{
+					"screen_name": "LauraWMcCaffrey",
+					"name": "Laura McCaffrey",
+					"id": 498297203,
+					"id_str": "498297203",
+					"indices": [
+						75,
+						91
+					]
+				},
+				{
+					"screen_name": "anniecorreal",
+					"name": "Annie Correal",
+					"id": 48023374,
+					"id_str": "48023374",
+					"indices": [
+						92,
+						105
+					]
+				},
+				{
+					"screen_name": "RobinMacArthur",
+					"name": "Robin Marie MacArthur",
+					"id": 3004145993,
+					"id_str": "3004145993",
+					"indices": [
+						106,
+						121
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1436775261232640000,
+		"in_reply_to_status_id_str": "1436775261232640004",
+		"in_reply_to_user_id": 17316318,
+		"in_reply_to_user_id_str": "17316318",
+		"in_reply_to_screen_name": "ggevalt",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10404,
+			"friends_count": 1539,
+			"listed_count": 790,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 896,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93704,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ggevalt @StephenPKiernan @eliast05 @SSTaylorBooks @laura_ritchie @abbene1 @LauraWMcCaffrey @anniecorreal @RobinMacArthur Great to hear from you always. Something might be in your mail “soon”"
+	},
+	{
 		"created_at": "Sat Sep 11 18:42:40 +0000 2021",
 		"id": 1436762130083160000,
 		"id_str": "1436762130083160064",

7a0ec67f946776fce3af22ec1102846336284df8
Alan Levine <cogdogblog@gmail.com>
2021-10-22 09:15:10 -0600

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93000,
-	"created_at": "Fri Oct 22 04:15:10 +0000 2021",
+	"tweets": 93002,
+	"created_at": "Fri Oct 22 15:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,349 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 22 14:37:28 +0000 2021",
+		"id": 1451558326878326800,
+		"id_str": "1451558326878326791",
+		"full_text": "@SunKScribbles @MiaZamoraPhD And one more thing on research methodology... the paper never provided access to the memes that were used. Does that make you wonder?\n\nAlchemists are always questioning...  Cheers, Sun!",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			214
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SunKScribbles",
+					"name": "☀️ Sun K.",
+					"id": 1299072689932562400,
+					"id_str": "1299072689932562434",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "MiaZamoraPhD",
+					"name": "Mia Zamora",
+					"id": 579873574,
+					"id_str": "579873574",
+					"indices": [
+						15,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1451557717374537700,
+		"in_reply_to_status_id_str": "1451557717374537728",
+		"in_reply_to_user_id": 740343,
+		"in_reply_to_user_id_str": "740343",
+		"in_reply_to_screen_name": "cogdog",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10412,
+			"friends_count": 1541,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 908,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93939,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SunKScribbles @MiaZamoraPhD And one more thing on research methodology... the paper never provided access to the memes that were used. Does that make you wonder?\n\nAlchemists are always questioning...  Cheers, Sun!"
+	},
+	{
+		"created_at": "Fri Oct 22 14:35:03 +0000 2021",
+		"id": 1451557717374537700,
+		"id_str": "1451557717374537728",
+		"full_text": "@SunKScribbles @MiaZamoraPhD Though if you follow 2 links back to the research study, they looked only at the effect of *viewing* memes, not at all the impact of making them https://t.co/DhmhEKawkU https://t.co/RjvmEZTGng",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			197
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SunKScribbles",
+					"name": "☀️ Sun K.",
+					"id": 1299072689932562400,
+					"id_str": "1299072689932562434",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "MiaZamoraPhD",
+					"name": "Mia Zamora",
+					"id": 579873574,
+					"id_str": "579873574",
+					"indices": [
+						15,
+						28
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DhmhEKawkU",
+					"expanded_url": "https://www.apa.org/pubs/journals/releases/ppm-ppm0000371.pdf",
+					"display_url": "apa.org/pubs/journals/…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1451557446552535000,
+					"id_str": "1451557446552535040",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FCT3zpTWQAAIBL4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FCT3zpTWQAAIBL4.jpg",
+					"url": "https://t.co/RjvmEZTGng",
+					"display_url": "pic.twitter.com/RjvmEZTGng",
+					"expanded_url": "https://twitter.com/cogdog/status/1451557717374537728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 610,
+							"h": 409,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 610,
+							"h": 409,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 610,
+							"h": 409,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1451557446552535000,
+					"id_str": "1451557446552535040",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FCT3zpTWQAAIBL4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FCT3zpTWQAAIBL4.jpg",
+					"url": "https://t.co/RjvmEZTGng",
+					"display_url": "pic.twitter.com/RjvmEZTGng",
+					"expanded_url": "https://twitter.com/cogdog/status/1451557717374537728/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 610,
+							"h": 409,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 610,
+							"h": 409,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 610,
+							"h": 409,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1451545890821443600,
+		"in_reply_to_status_id_str": "1451545890821443585",
+		"in_reply_to_user_id": 1299072689932562400,
+		"in_reply_to_user_id_str": "1299072689932562434",
+		"in_reply_to_screen_name": "SunKScribbles",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10412,
+			"friends_count": 1541,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 908,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93939,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@SunKScribbles @MiaZamoraPhD Though if you follow 2 links back to the research study, they looked only at the effect of *viewing* memes, not at all the impact of making them https://t.co/DhmhEKawkU https://t.co/RjvmEZTGng"
+	},
+	{
 		"created_at": "Fri Oct 22 03:15:48 +0000 2021",
 		"id": 1451386780704551000,
 		"id_str": "1451386780704550915",

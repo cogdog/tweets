b625667ff3ff317c7990e160c6d1870ce5a4425c
Alan Levine <cogdogblog@gmail.com>
2021-10-28 10:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93038,
-	"created_at": "Thu Oct 28 15:15:09 +0000 2021",
+	"tweets": 93039,
+	"created_at": "Thu Oct 28 16:15:10 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 28 16:09:46 +0000 2021",
+		"id": 1453755881489846300,
+		"id_str": "1453755881489846277",
+		"full_text": "@walledculture @OnlineCrsLady @techdirt I've gotten lawyer slapped twice (but as bad as this story) for what I felt were obvious, non commercial parodies of \"Believe it or Not\" and \"Amazing Stories\" \n\nhttps://t.co/r3wwhjkx7z\n\nhttps://t.co/MUCoO6ifd9",
+		"truncated": false,
+		"display_text_range": [
+			40,
+			249
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "walledculture",
+					"name": "Walled Culture",
+					"id": 971762499291877400,
+					"id_str": "971762499291877376",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "OnlineCrsLady",
+					"name": "Laura Gibbs",
+					"id": 7044082,
+					"id_str": "7044082",
+					"indices": [
+						15,
+						29
+					]
+				},
+				{
+					"screen_name": "techdirt",
+					"name": "techdirt",
+					"id": 11382292,
+					"id_str": "11382292",
+					"indices": [
+						30,
+						39
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/r3wwhjkx7z",
+					"expanded_url": "https://cogdogblog.com/2005/04/xxxxxxx-xx/",
+					"display_url": "cogdogblog.com/2005/04/xxxxxx…",
+					"indices": [
+						201,
+						224
+					]
+				},
+				{
+					"url": "https://t.co/MUCoO6ifd9",
+					"expanded_url": "https://cogdogblog.com/2012/10/take-me-down/",
+					"display_url": "cogdogblog.com/2012/10/take-m…",
+					"indices": [
+						226,
+						249
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1453655309168349200,
+		"in_reply_to_status_id_str": "1453655309168349184",
+		"in_reply_to_user_id": 971762499291877400,
+		"in_reply_to_user_id_str": "971762499291877376",
+		"in_reply_to_screen_name": "walledculture",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10413,
+			"friends_count": 1541,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 909,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93988,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@walledculture @OnlineCrsLady @techdirt I've gotten lawyer slapped twice (but as bad as this story) for what I felt were obvious, non commercial parodies of \"Believe it or Not\" and \"Amazing Stories\" \n\nhttps://t.co/r3wwhjkx7z\n\nhttps://t.co/MUCoO6ifd9"
+	},
+	{
 		"created_at": "Thu Oct 28 15:03:17 +0000 2021",
 		"id": 1453739152504201200,
 		"id_str": "1453739152504201216",

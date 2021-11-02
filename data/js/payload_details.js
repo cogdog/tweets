af7108bd5432eb44f4cf60b383677d86ad5e2742
Alan Levine <cogdogblog@gmail.com>
2021-11-01 21:15:08 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93076,
-	"created_at": "Mon Nov 01 21:15:08 +0000 2021",
+	"tweets": 93077,
+	"created_at": "Tue Nov 02 03:15:07 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,164 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 02 02:35:54 +0000 2021",
+		"id": 1455363003646414800,
+		"id_str": "1455363003646414848",
+		"full_text": "@xb7r @shareski @bonni208 @greeneterry Jim nabbed the ones in my player.\n\nBut to be honest, I get tons of insight from podcasts not explicitly about education\n\nhttps://t.co/kCkCm9SfAn",
+		"truncated": false,
+		"display_text_range": [
+			39,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "xb7r",
+					"name": "Jim Stauffer",
+					"id": 205077445,
+					"id_str": "205077445",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "shareski",
+					"name": "Dean Shareski",
+					"id": 739743,
+					"id_str": "739743",
+					"indices": [
+						6,
+						15
+					]
+				},
+				{
+					"screen_name": "bonni208",
+					"name": "Bonni Stachowiak",
+					"id": 14777884,
+					"id_str": "14777884",
+					"indices": [
+						16,
+						25
+					]
+				},
+				{
+					"screen_name": "greeneterry",
+					"name": "Terry Greene: November 2021 Edition",
+					"id": 212019344,
+					"id_str": "212019344",
+					"indices": [
+						26,
+						38
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kCkCm9SfAn",
+					"expanded_url": "https://cogdogblog.com/2021/10/the-truth/",
+					"display_url": "cogdogblog.com/2021/10/the-tr…",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1455357678847398000,
+		"in_reply_to_status_id_str": "1455357678847397894",
+		"in_reply_to_user_id": 205077445,
+		"in_reply_to_user_id_str": "205077445",
+		"in_reply_to_screen_name": "xb7r",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10410,
+			"friends_count": 1543,
+			"listed_count": 0,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 908,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 94038,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@xb7r @shareski @bonni208 @greeneterry Jim nabbed the ones in my player.\n\nBut to be honest, I get tons of insight from podcasts not explicitly about education\n\nhttps://t.co/kCkCm9SfAn"
+	},
+	{
 		"created_at": "Mon Nov 01 20:17:07 +0000 2021",
 		"id": 1455267682392154000,
 		"id_str": "1455267682392154120",

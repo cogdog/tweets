3562d767803d3d0ab7bb4d2ef42631a56b3af76a
Alan Levine <cogdogblog@gmail.com>
2021-11-15 08:15:15 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93142,
-	"created_at": "Mon Nov 15 06:15:08 +0000 2021",
+	"tweets": 93143,
+	"created_at": "Mon Nov 15 14:15:14 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Mon Nov 15 14:02:24 +0000 2021",
+		"id": 1460246811696840700,
+		"id_str": "1460246811696840708",
+		"full_text": "@growAllison It was one of the most original approaches to understanding OOP via Macromedia Director scripting. Is my memory fuzzy! My copy is autographed. https://t.co/i5k4JcgKbj",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "growAllison",
+					"name": "Allison (Bishop) Rodgers",
+					"id": 2603997561,
+					"id_str": "2603997561",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1460246801584468000,
+					"id_str": "1460246801584467971",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FEPWurTXsAMvmw7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEPWurTXsAMvmw7.jpg",
+					"url": "https://t.co/i5k4JcgKbj",
+					"display_url": "pic.twitter.com/i5k4JcgKbj",
+					"expanded_url": "https://twitter.com/cogdog/status/1460246811696840708/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 486,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 858,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1464,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1460246801584468000,
+					"id_str": "1460246801584467971",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FEPWurTXsAMvmw7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEPWurTXsAMvmw7.jpg",
+					"url": "https://t.co/i5k4JcgKbj",
+					"display_url": "pic.twitter.com/i5k4JcgKbj",
+					"expanded_url": "https://twitter.com/cogdog/status/1460246811696840708/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 486,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 858,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1464,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1460246801605439500,
+					"id_str": "1460246801605439491",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FEPWurYXsAMGTs3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEPWurYXsAMGTs3.jpg",
+					"url": "https://t.co/i5k4JcgKbj",
+					"display_url": "pic.twitter.com/i5k4JcgKbj",
+					"expanded_url": "https://twitter.com/cogdog/status/1460246811696840708/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1234,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 723,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 410,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1459638755912999000,
+		"in_reply_to_status_id_str": "1459638755912998921",
+		"in_reply_to_user_id": 2603997561,
+		"in_reply_to_user_id_str": "2603997561",
+		"in_reply_to_screen_name": "growAllison",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10408,
+			"friends_count": 1544,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 912,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 94116,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@growAllison It was one of the most original approaches to understanding OOP via Macromedia Director scripting. Is my memory fuzzy! My copy is autographed. https://t.co/i5k4JcgKbj"
+	},
+	{
 		"created_at": "Mon Nov 15 05:58:35 +0000 2021",
 		"id": 1460125054134595600,
 		"id_str": "1460125054134595585",

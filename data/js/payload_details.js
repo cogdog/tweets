5f2974bd60569d887538dce6735080e2abeba036
Alan Levine <cogdogblog@gmail.com>
2021-12-01 10:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93222,
-	"created_at": "Wed Dec 01 06:15:08 +0000 2021",
+	"tweets": 93223,
+	"created_at": "Wed Dec 01 16:15:08 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Wed Dec 01 16:10:27 +0000 2021",
+		"id": 1466077176453816300,
+		"id_str": "1466077242459627525",
+		"full_text": "For the next 6+ hours, we have a drop in open annotation lab for our Three Day Focus on Annotating the OER Recommendation ... annotate with others including @pgstacey @remikalir @enkerli \n\nhttps://t.co/uAFvYEbLLc https://t.co/3a5KDvykYK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			212
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pgstacey",
+					"name": "Paul Stacey",
+					"id": 48018738,
+					"id_str": "48018738",
+					"indices": [
+						157,
+						166
+					]
+				},
+				{
+					"screen_name": "remikalir",
+					"name": "Remi Kalir",
+					"id": 2557762548,
+					"id_str": "2557762548",
+					"indices": [
+						167,
+						177
+					]
+				},
+				{
+					"screen_name": "enkerli",
+					"name": "Alexandre Enkerli",
+					"id": 4132991,
+					"id_str": "4132991",
+					"indices": [
+						178,
+						186
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uAFvYEbLLc",
+					"expanded_url": "https://connect.oeglobal.org/t/join-the-open-annotation-lab/3313",
+					"display_url": "connect.oeglobal.org/t/join-the-ope…",
+					"indices": [
+						189,
+						212
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1466077078118359000,
+					"id_str": "1466077078118359043",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/media/FFiNVb6WYAMvddt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFiNVb6WYAMvddt.jpg",
+					"url": "https://t.co/3a5KDvykYK",
+					"display_url": "pic.twitter.com/3a5KDvykYK",
+					"expanded_url": "https://twitter.com/OpenEdGlobal/status/1466077176453816328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 678,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1082,
+							"h": 678,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1466077078118359000,
+					"id_str": "1466077078118359043",
+					"indices": [
+						213,
+						236
+					],
+					"media_url": "http://pbs.twimg.com/media/FFiNVb6WYAMvddt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFiNVb6WYAMvddt.jpg",
+					"url": "https://t.co/3a5KDvykYK",
+					"display_url": "pic.twitter.com/3a5KDvykYK",
+					"expanded_url": "https://twitter.com/OpenEdGlobal/status/1466077176453816328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 678,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1082,
+							"h": 678,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16548567,
+			"id_str": "16548567",
+			"name": "Open Education Global",
+			"screen_name": "OpenEdGlobal",
+			"location": "Global",
+			"description": "Open Education Global is a member-based non-profit committed to the development, use and advancement of open education with its worldwide community and members.",
+			"url": "https://t.co/FolbQBguMQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FolbQBguMQ",
+							"expanded_url": "https://www.oeglobal.org/",
+							"display_url": "oeglobal.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11184,
+			"friends_count": 515,
+			"listed_count": 552,
+			"created_at": "Wed Oct 01 19:17:02 +0000 2008",
+			"favourites_count": 2617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10744,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E6E3E6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1445807809472471042/pi152R87_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1445807809472471042/pi152R87_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16548567/1625846041",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For the next 6+ hours, we have a drop in open annotation lab for our Three Day Focus on Annotating the OER Recommendation ... annotate with others including @pgstacey @remikalir @enkerli \n\nhttps://t.co/uAFvYEbLLc https://t.co/3a5KDvykYK"
+	},
+	{
 		"created_at": "Wed Dec 01 05:34:38 +0000 2021",
 		"id": 1465917233432076300,
 		"id_str": "1465917233432076288",

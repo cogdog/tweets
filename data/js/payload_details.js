423b279cbee41ce4dff1efa6da409d3341f0c32a
Alan Levine <cogdogblog@gmail.com>
2021-12-14 15:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93289,
-	"created_at": "Tue Dec 14 15:15:09 +0000 2021",
+	"tweets": 93290,
+	"created_at": "Tue Dec 14 21:15:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Tue Dec 14 20:25:23 +0000 2021",
+		"id": 1470852438089478100,
+		"id_str": "1470852438089478145",
+		"full_text": "@OERcamp @joeranDE @OpenEdGlobal Thanks again for taking time during  #OERcampglobal to record this podcast, especially without offering much of a script for our conversational format.  Congratulations TEAM OERcamp for a successful online event",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			244
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OERcampglobal",
+					"indices": [
+						70,
+						84
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OERcamp",
+					"name": "#OERcamp",
+					"id": 4470536123,
+					"id_str": "4470536123",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "joeranDE",
+					"name": "Jöran Muuß-Merholz",
+					"id": 17154029,
+					"id_str": "17154029",
+					"indices": [
+						9,
+						18
+					]
+				},
+				{
+					"screen_name": "OpenEdGlobal",
+					"name": "Open Education Global",
+					"id": 16548567,
+					"id_str": "16548567",
+					"indices": [
+						19,
+						32
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1470835481621540900,
+		"in_reply_to_status_id_str": "1470835481621540867",
+		"in_reply_to_user_id": 4470536123,
+		"in_reply_to_user_id_str": "4470536123",
+		"in_reply_to_screen_name": "OERcamp",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10412,
+			"friends_count": 1550,
+			"listed_count": 788,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 914,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 94294,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@OERcamp @joeranDE @OpenEdGlobal Thanks again for taking time during  #OERcampglobal to record this podcast, especially without offering much of a script for our conversational format.  Congratulations TEAM OERcamp for a successful online event"
+	},
+	{
 		"created_at": "Tue Dec 14 14:57:30 +0000 2021",
 		"id": 1470769927359451100,
 		"id_str": "1470769927359451144",

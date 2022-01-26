4f8c1bfa967c50a17fe664d3d7895f4a40f9e8da
Alan Levine <cogdogblog@gmail.com>
2022-01-26 13:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93497,
-	"created_at": "Wed Jan 26 15:15:10 +0000 2022",
+	"tweets": 93498,
+	"created_at": "Wed Jan 26 19:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,364 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 26 18:28:47 +0000 2022",
+		"id": 1486405776218411000,
+		"id_str": "1486405776218411010",
+		"full_text": "Get ready to be part of #OEWeek 2022, happening all over the world. https://t.co/wmUrOnQrdt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OEWeek",
+					"indices": [
+						24,
+						31
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wmUrOnQrdt",
+					"expanded_url": "https://twitter.com/OEWeek/status/1486392882059784196",
+					"display_url": "twitter.com/OEWeek/status/…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10413,
+			"friends_count": 1546,
+			"listed_count": 784,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 94564,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1486392882059784200,
+		"quoted_status_id_str": "1486392882059784196",
+		"quoted_status_permalink": {
+			"url": "https://t.co/wmUrOnQrdt",
+			"expanded": "https://twitter.com/OEWeek/status/1486392882059784196",
+			"display": "twitter.com/OEWeek/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 26 17:37:33 +0000 2022",
+			"id": 1486392882059784200,
+			"id_str": "1486392882059784196",
+			"full_text": "#OEWeek takes place from March 7–11\n\ncelebrate • inform • share • discuss • learn • excite • develop • #whatareyousharing ?\n\nShare the power of Open Education.\nShare your activity or #OER with your global community 👉 https://t.co/YHwdd0Fpbi\n\n#JoinUs https://t.co/rXNUEs60g0",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				249
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OEWeek",
+						"indices": [
+							0,
+							7
+						]
+					},
+					{
+						"text": "whatareyousharing",
+						"indices": [
+							103,
+							121
+						]
+					},
+					{
+						"text": "OER",
+						"indices": [
+							183,
+							187
+						]
+					},
+					{
+						"text": "JoinUs",
+						"indices": [
+							242,
+							249
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/YHwdd0Fpbi",
+						"expanded_url": "https://bit.ly/OEWeek22",
+						"display_url": "bit.ly/OEWeek22",
+						"indices": [
+							217,
+							240
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1486377598796062700,
+						"id_str": "1486377598796062723",
+						"indices": [
+							250,
+							273
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/FKCsjQpWYAMb5-l.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FKCsjQpWYAMb5-l.jpg",
+						"url": "https://t.co/rXNUEs60g0",
+						"display_url": "pic.twitter.com/rXNUEs60g0",
+						"expanded_url": "https://twitter.com/OEWeek/status/1486392882059784196/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1012,
+								"h": 506,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1012,
+								"h": 506,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1486377598796062700,
+						"id_str": "1486377598796062723",
+						"indices": [
+							250,
+							273
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/FKCsjQpWYAMb5-l.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FKCsjQpWYAMb5-l.jpg",
+						"url": "https://t.co/rXNUEs60g0",
+						"display_url": "pic.twitter.com/rXNUEs60g0",
+						"expanded_url": "https://twitter.com/OEWeek/status/1486392882059784196/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1012,
+								"h": 506,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1012,
+								"h": 506,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								2,
+								1
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/FKCsjQpWYAMb5-l.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 396632857,
+				"id_str": "396632857",
+				"name": "Open Education Week",
+				"screen_name": "OEWeek",
+				"location": "Worldwide",
+				"description": "#OEWeek is where the global open education community meets, shares, collaborates, discusses, and builds Open Edu resources, practices, & more. March 7-11, 2022",
+				"url": "https://t.co/9YI3tqpDdD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/9YI3tqpDdD",
+								"expanded_url": "https://www.openeducationweek.org/",
+								"display_url": "openeducationweek.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6211,
+				"friends_count": 47,
+				"listed_count": 283,
+				"created_at": "Sun Oct 23 15:13:33 +0000 2011",
+				"favourites_count": 121,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3966,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "5FB2A9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1483463114284834824/C4BFtBSN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1483463114284834824/C4BFtBSN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/396632857/1642521529",
+				"profile_link_color": "19CF86",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EEEEEE",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get ready to be part of #OEWeek 2022, happening all over the world. https://t.co/wmUrOnQrdt"
+	},
+	{
 		"created_at": "Wed Jan 26 14:42:31 +0000 2022",
 		"id": 1486348834552746000,
 		"id_str": "1486348834552745984",

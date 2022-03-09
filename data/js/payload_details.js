2145e23e40f68410e063d6ae7792cc7e577d62f4
Alan Levine <cogdogblog@gmail.com>
2022-03-09 12:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93731,
-	"created_at": "Wed Mar 09 17:15:09 +0000 2022",
+	"tweets": 93732,
+	"created_at": "Wed Mar 09 18:15:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 09 17:17:23 +0000 2022",
+		"id": 1501608097663664000,
+		"id_str": "1501608097663664135",
+		"full_text": "This is quite the cloud for OER researchers. Add to your toolkit! #OEweek https://t.co/HmOzG5akF6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			73
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OEweek",
+					"indices": [
+						66,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HmOzG5akF6",
+					"expanded_url": "https://twitter.com/aulibarchives/status/1501600452600860678",
+					"display_url": "twitter.com/aulibarchives/…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10442,
+			"friends_count": 1548,
+			"listed_count": 784,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 925,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 94859,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1501600452600860700,
+		"quoted_status_id_str": "1501600452600860678",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HmOzG5akF6",
+			"expanded": "https://twitter.com/aulibarchives/status/1501600452600860678",
+			"display": "twitter.com/aulibarchives/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 09 16:47:00 +0000 2022",
+			"id": 1501600452600860700,
+			"id_str": "1501600452600860678",
+			"full_text": "Developed by Athabasca University, The OER Knowledge Cloud is a curated database and repository for all data related to open educational resources. Visit https://t.co/6QqzvskZwJ to learn more. #OpenAccess #OEWeek https://t.co/7P0PU2MDIw",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				212
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OpenAccess",
+						"indices": [
+							193,
+							204
+						]
+					},
+					{
+						"text": "OEWeek",
+						"indices": [
+							205,
+							212
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/6QqzvskZwJ",
+						"expanded_url": "http://www.oerknowledgecloud.org",
+						"display_url": "oerknowledgecloud.org",
+						"indices": [
+							154,
+							177
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1501600450008789000,
+						"id_str": "1501600450008788992",
+						"indices": [
+							213,
+							236
+						],
+						"media_url": "http://pbs.twimg.com/media/FNbBp0lXMAAa8lK.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FNbBp0lXMAAa8lK.jpg",
+						"url": "https://t.co/7P0PU2MDIw",
+						"display_url": "pic.twitter.com/7P0PU2MDIw",
+						"expanded_url": "https://twitter.com/aulibarchives/status/1501600452600860678/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 674,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1500,
+								"h": 843,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1501600450008789000,
+						"id_str": "1501600450008788992",
+						"indices": [
+							213,
+							236
+						],
+						"media_url": "http://pbs.twimg.com/media/FNbBp0lXMAAa8lK.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FNbBp0lXMAAa8lK.jpg",
+						"url": "https://t.co/7P0PU2MDIw",
+						"display_url": "pic.twitter.com/7P0PU2MDIw",
+						"expanded_url": "https://twitter.com/aulibarchives/status/1501600452600860678/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 674,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1500,
+								"h": 843,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.canva.com\" rel=\"nofollow\">Canva</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1159122567631786000,
+				"id_str": "1159122567631785984",
+				"name": "Athabasca University Library & Archives",
+				"screen_name": "aulibarchives",
+				"location": "",
+				"description": "Athabasca University \nLibrary & Scholarly Resources",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 237,
+				"friends_count": 360,
+				"listed_count": 5,
+				"created_at": "Wed Aug 07 15:22:10 +0000 2019",
+				"favourites_count": 1639,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1509,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1159590293366140928/HdldeQSg_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1159590293366140928/HdldeQSg_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1159122567631785984/1641855636",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is quite the cloud for OER researchers. Add to your toolkit! #OEweek https://t.co/HmOzG5akF6"
+	},
+	{
 		"created_at": "Wed Mar 09 16:55:39 +0000 2022",
 		"id": 1501602626928001000,
 		"id_str": "1501602626928001024",

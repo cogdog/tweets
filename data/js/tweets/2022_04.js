78d6ea814cf75e9fde42b076ce4ed5670427a2a8
Alan Levine <cogdogblog@gmail.com>
2022-03-31 20:15:09 -0600

    Added 1 tweets

--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,359 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 01 01:16:45 +0000 2022",
+		"id": 1509701264485101600,
+		"id_str": "1509701264485101570",
+		"full_text": "@ds106dc #tdc3730 #ds106 I Was Today Years Old When I found out the mold is used to make the whale (\"of a dad\") cake was also used to make Santa.\n\nhttps://t.co/AjJROa6knH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc3730",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						18,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AjJROa6knH",
+					"expanded_url": "https://twitter.com/carvelicecream/status/1204879287326822402",
+					"display_url": "twitter.com/carvelicecream…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10464,
+			"friends_count": 1548,
+			"listed_count": 785,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 931,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95024,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1204879287326822400,
+		"quoted_status_id_str": "1204879287326822402",
+		"quoted_status_permalink": {
+			"url": "https://t.co/AjJROa6knH",
+			"expanded": "https://twitter.com/carvelicecream/status/1204879287326822402",
+			"display": "twitter.com/carvelicecream…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 11 21:43:02 +0000 2019",
+			"id": 1204879287326822400,
+			"id_str": "1204879287326822402",
+			"full_text": "The more the merrier! Here’s to a Merry Fudgie Christmas this #holidayseason 🎅 \n#fudgiethewhale #santa #holidayplanning https://t.co/2Ogc38PIUv",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				119
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "holidayseason",
+						"indices": [
+							62,
+							76
+						]
+					},
+					{
+						"text": "fudgiethewhale",
+						"indices": [
+							80,
+							95
+						]
+					},
+					{
+						"text": "santa",
+						"indices": [
+							96,
+							102
+						]
+					},
+					{
+						"text": "holidayplanning",
+						"indices": [
+							103,
+							119
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1204879281786146800,
+						"id_str": "1204879281786146817",
+						"indices": [
+							120,
+							143
+						],
+						"media_url": "http://pbs.twimg.com/media/ELiXUrQXUAEAtpw.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/ELiXUrQXUAEAtpw.jpg",
+						"url": "https://t.co/2Ogc38PIUv",
+						"display_url": "pic.twitter.com/2Ogc38PIUv",
+						"expanded_url": "https://twitter.com/CarvelIceCream/status/1204879287326822402/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2035,
+								"h": 1017,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 600,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1204879281786146800,
+						"id_str": "1204879281786146817",
+						"indices": [
+							120,
+							143
+						],
+						"media_url": "http://pbs.twimg.com/media/ELiXUrQXUAEAtpw.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/ELiXUrQXUAEAtpw.jpg",
+						"url": "https://t.co/2Ogc38PIUv",
+						"display_url": "pic.twitter.com/2Ogc38PIUv",
+						"expanded_url": "https://twitter.com/CarvelIceCream/status/1204879287326822402/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2035,
+								"h": 1017,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 600,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 31290792,
+				"id_str": "31290792",
+				"name": "Carvel Ice Cream",
+				"screen_name": "CarvelIceCream",
+				"location": "Atlanta, GA",
+				"description": "Soft serving since 1934 🍦",
+				"url": "https://t.co/WT2MRBrrNN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WT2MRBrrNN",
+								"expanded_url": "http://www.carvel.com/",
+								"display_url": "carvel.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 22045,
+				"friends_count": 20949,
+				"listed_count": 202,
+				"created_at": "Wed Apr 15 01:09:56 +0000 2009",
+				"favourites_count": 53320,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 74535,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1341498534990917632/uQIszTEg_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1341498534990917632/uQIszTEg_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/31290792/1635768718",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 36,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc #tdc3730 #ds106 I Was Today Years Old When I found out the mold is used to make the whale (\"of a dad\") cake was also used to make Santa.\n\nhttps://t.co/AjJROa6knH"
+	},
+	{
 		"created_at": "Fri Apr 01 00:28:52 +0000 2022",
 		"id": 1509689217852625000,
 		"id_str": "1509689217852624915",

0d11bac1d20eec669e102ccb13fc42314dd9fb92
Alan Levine <cogdogblog@gmail.com>
2022-04-14 02:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93929,
-	"created_at": "Thu Apr 14 06:15:09 +0000 2022",
+	"tweets": 93930,
+	"created_at": "Thu Apr 14 08:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,348 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Thu Apr 14 07:27:29 +0000 2022",
+		"id": 1514505607470010400,
+		"id_str": "1514505607470010368",
+		"full_text": "You heard it from the source. You do not want to le a penguin down. \n\nShare a story now! #oer22 https://t.co/3N3xMIfQYw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "oer22",
+					"indices": [
+						89,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3N3xMIfQYw",
+					"expanded_url": "https://twitter.com/OERConf/status/1514498922689052674",
+					"display_url": "twitter.com/OERConf/status…",
+					"indices": [
+						96,
+						119
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10460,
+			"friends_count": 1547,
+			"listed_count": 787,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 933,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95111,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1514498922689052700,
+		"quoted_status_id_str": "1514498922689052674",
+		"quoted_status_permalink": {
+			"url": "https://t.co/3N3xMIfQYw",
+			"expanded": "https://twitter.com/OERConf/status/1514498922689052674",
+			"display": "twitter.com/OERConf/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 14 07:00:55 +0000 2022",
+			"id": 1514498922689052700,
+			"id_str": "1514498922689052674",
+			"full_text": "Alan Levine AKA @cogdog and a penguin want YOU to share stories for #OER22. Please share  anything that has happened unexpectedly because of sharing or connections made online. Learn more - \n https://t.co/VQ2MGdhE1Z #altc https://t.co/81ASM8gHEz",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				221
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OER22",
+						"indices": [
+							68,
+							74
+						]
+					},
+					{
+						"text": "altc",
+						"indices": [
+							216,
+							221
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "cogdog",
+						"name": "Alan Levine",
+						"id": 740343,
+						"id_str": "740343",
+						"indices": [
+							16,
+							23
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/VQ2MGdhE1Z",
+						"expanded_url": "http://stories.cogdogblog.com/share-oer22",
+						"display_url": "stories.cogdogblog.com/share-oer22",
+						"indices": [
+							192,
+							215
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1514498921640476700,
+						"id_str": "1514498921640476672",
+						"indices": [
+							222,
+							245
+						],
+						"media_url": "http://pbs.twimg.com/media/FQSUvr0XwAAiD62.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FQSUvr0XwAAiD62.jpg",
+						"url": "https://t.co/81ASM8gHEz",
+						"display_url": "pic.twitter.com/81ASM8gHEz",
+						"expanded_url": "https://twitter.com/OERConf/status/1514498922689052674/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1514498921640476700,
+						"id_str": "1514498921640476672",
+						"indices": [
+							222,
+							245
+						],
+						"media_url": "http://pbs.twimg.com/media/FQSUvr0XwAAiD62.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FQSUvr0XwAAiD62.jpg",
+						"url": "https://t.co/81ASM8gHEz",
+						"display_url": "pic.twitter.com/81ASM8gHEz",
+						"expanded_url": "https://twitter.com/OERConf/status/1514498922689052674/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2695133612,
+				"id_str": "2695133612",
+				"name": "OER Open Education Conference",
+				"screen_name": "OERConf",
+				"location": "Online",
+				"description": "Official account for the OER Conference, organised by @A_L_T, UK, convening in person (London, UK) and online, 26-28 April 2022. Co-Chair: @GOGN_OER",
+				"url": "https://t.co/oBMbtWR1PK",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/oBMbtWR1PK",
+								"expanded_url": "https://www.alt.ac.uk/events/open-education-conference",
+								"display_url": "alt.ac.uk/events/open-ed…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3162,
+				"friends_count": 1270,
+				"listed_count": 100,
+				"created_at": "Thu Jul 31 08:18:01 +0000 2014",
+				"favourites_count": 1769,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3657,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1465647321866244098/8Io9roC3_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1465647321866244098/8Io9roC3_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2695133612/1638272331",
+				"profile_link_color": "444444",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "You heard it from the source. You do not want to le a penguin down. \n\nShare a story now! #oer22 https://t.co/3N3xMIfQYw"
+	},
+	{
 		"created_at": "Thu Apr 14 05:17:26 +0000 2022",
 		"id": 1514472880171622400,
 		"id_str": "1514472880171622406",

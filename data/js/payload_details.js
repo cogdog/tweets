2fef40bb7238674dd335b8f653e062d9e106f0bb
Alan Levine <cogdogblog@gmail.com>
2022-06-01 09:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94075,
-	"created_at": "Wed Jun 01 04:15:08 +0000 2022",
+	"tweets": 94076,
+	"created_at": "Wed Jun 01 15:15:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,367 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 01 14:42:52 +0000 2022",
+		"id": 1532009792788652000,
+		"id_str": "1532009792788652033",
+		"full_text": "Opening day for #MyFest22 where anyone welcome to do #ds106 #dailycreate - participants can respond in twitter but also in the Slack channel. More at https://t.co/6cqOQ7XxWO https://t.co/DW4ALbgk6N",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MyFest22",
+					"indices": [
+						16,
+						25
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						53,
+						59
+					]
+				},
+				{
+					"text": "dailycreate",
+					"indices": [
+						60,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6cqOQ7XxWO",
+					"expanded_url": "https://myfest.equityunbound.org/theme-stream/wellbeing-joy/daily-creative-habit/",
+					"display_url": "myfest.equityunbound.org/theme-stream/w…",
+					"indices": [
+						150,
+						173
+					]
+				},
+				{
+					"url": "https://t.co/DW4ALbgk6N",
+					"expanded_url": "https://twitter.com/ds106dc/status/1531948306519101440",
+					"display_url": "twitter.com/ds106dc/status…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10444,
+			"friends_count": 1548,
+			"listed_count": 783,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 937,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95281,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1531948306519101400,
+		"quoted_status_id_str": "1531948306519101440",
+		"quoted_status_permalink": {
+			"url": "https://t.co/DW4ALbgk6N",
+			"expanded": "https://twitter.com/ds106dc/status/1531948306519101440",
+			"display": "twitter.com/ds106dc/status…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jun 01 10:38:33 +0000 2022",
+			"id": 1531948306519101400,
+			"id_str": "1531948306519101440",
+			"full_text": "For today's TDC... #tdc3793 #ds106 Salute to Zork! Create Options for Choose Your Own Adventure (and welcome #MyFest22) https://t.co/xeAqlHW6ZP https://t.co/t65gZUJJlW",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				143
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "tdc3793",
+						"indices": [
+							19,
+							27
+						]
+					},
+					{
+						"text": "ds106",
+						"indices": [
+							28,
+							34
+						]
+					},
+					{
+						"text": "MyFest22",
+						"indices": [
+							109,
+							118
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/xeAqlHW6ZP",
+						"expanded_url": "http://dlvr.it/SRQchN",
+						"display_url": "dlvr.it/SRQchN",
+						"indices": [
+							120,
+							143
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1531948304782610400,
+						"id_str": "1531948304782610432",
+						"indices": [
+							144,
+							167
+						],
+						"media_url": "http://pbs.twimg.com/media/FUKS3djUUAAKrZm.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FUKS3djUUAAKrZm.jpg",
+						"url": "https://t.co/t65gZUJJlW",
+						"display_url": "pic.twitter.com/t65gZUJJlW",
+						"expanded_url": "https://twitter.com/ds106dc/status/1531948306519101440/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 456,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 687,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 687,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1531948304782610400,
+						"id_str": "1531948304782610432",
+						"indices": [
+							144,
+							167
+						],
+						"media_url": "http://pbs.twimg.com/media/FUKS3djUUAAKrZm.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FUKS3djUUAAKrZm.jpg",
+						"url": "https://t.co/t65gZUJJlW",
+						"display_url": "pic.twitter.com/t65gZUJJlW",
+						"expanded_url": "https://twitter.com/ds106dc/status/1531948306519101440/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 456,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 687,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 687,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 995562187,
+				"id_str": "995562187",
+				"name": "ds106 Daily Create",
+				"screen_name": "ds106dc",
+				"location": "everywhere",
+				"description": "Each day we give you a new creative challenge, something to do in photography, drawing, audio, video, and writing. Follow us and create!",
+				"url": "https://t.co/VciGKYbj0S",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VciGKYbj0S",
+								"expanded_url": "http://daily.ds106.us/",
+								"display_url": "daily.ds106.us",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2237,
+				"friends_count": 49,
+				"listed_count": 65,
+				"created_at": "Fri Dec 07 17:57:14 +0000 2012",
+				"favourites_count": 2,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4479,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2944465117/f92e31c22dfcce90a1b544ffd125802e_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2944465117/f92e31c22dfcce90a1b544ffd125802e_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/995562187/1451540977",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Opening day for #MyFest22 where anyone welcome to do #ds106 #dailycreate - participants can respond in twitter but also in the Slack channel. More at https://t.co/6cqOQ7XxWO https://t.co/DW4ALbgk6N"
+	},
+	{
 		"created_at": "Wed Jun 01 04:09:23 +0000 2022",
 		"id": 1531850370653294600,
 		"id_str": "1531850370653294593",

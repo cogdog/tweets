3d8ea5e964de3ea7c69b1932ec43fed1901212cc
Alan Levine <cogdogblog@gmail.com>
2022-06-15 09:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94118,
-	"created_at": "Wed Jun 15 14:15:09 +0000 2022",
+	"tweets": 94119,
+	"created_at": "Wed Jun 15 15:15:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 15 15:05:33 +0000 2022",
+		"id": 1537088931895844900,
+		"id_str": "1537088931895844866",
+		"full_text": "@LibreTexts @ymdahi The course content becomes a place to hang links to the CH \"study resources\" that lead to signing up for accounts to see,  which slithers down into paying for an account to see more. \n\n$$$ https://t.co/KCpzk9UgJf",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			208
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LibreTexts",
+					"name": "Delmar Larsen",
+					"id": 80937370,
+					"id_str": "80937370",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "ymdahi",
+					"name": "Yasin Dahi",
+					"id": 972250261,
+					"id_str": "972250261",
+					"indices": [
+						12,
+						19
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1537088113390100500,
+					"id_str": "1537088113390100481",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/FVTVfssXoAEOvqf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVTVfssXoAEOvqf.jpg",
+					"url": "https://t.co/KCpzk9UgJf",
+					"display_url": "pic.twitter.com/KCpzk9UgJf",
+					"expanded_url": "https://twitter.com/cogdog/status/1537088931895844866/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 855,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 484,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1786,
+							"h": 1272,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537088113390100500,
+					"id_str": "1537088113390100481",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/FVTVfssXoAEOvqf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVTVfssXoAEOvqf.jpg",
+					"url": "https://t.co/KCpzk9UgJf",
+					"display_url": "pic.twitter.com/KCpzk9UgJf",
+					"expanded_url": "https://twitter.com/cogdog/status/1537088931895844866/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 855,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 484,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1786,
+							"h": 1272,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1537088532333973500,
+					"id_str": "1537088532333973504",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/FVTV4FYX0AAsxTh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVTV4FYX0AAsxTh.jpg",
+					"url": "https://t.co/KCpzk9UgJf",
+					"display_url": "pic.twitter.com/KCpzk9UgJf",
+					"expanded_url": "https://twitter.com/cogdog/status/1537088931895844866/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 664,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1134,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1537086600999342000,
+		"in_reply_to_status_id_str": "1537086600999342085",
+		"in_reply_to_user_id": 80937370,
+		"in_reply_to_user_id_str": "80937370",
+		"in_reply_to_screen_name": "LibreTexts",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10444,
+			"friends_count": 1551,
+			"listed_count": 785,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 938,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95336,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@LibreTexts @ymdahi The course content becomes a place to hang links to the CH \"study resources\" that lead to signing up for accounts to see,  which slithers down into paying for an account to see more. \n\n$$$ https://t.co/KCpzk9UgJf"
+	},
+	{
 		"created_at": "Wed Jun 15 14:03:16 +0000 2022",
 		"id": 1537073258029367300,
 		"id_str": "1537073258029367298",

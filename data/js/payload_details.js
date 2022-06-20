89bf6869ae3cebc009040d6ceda3a60bc004e697
Alan Levine <cogdogblog@gmail.com>
2022-06-20 14:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94156,
-	"created_at": "Mon Jun 20 19:15:09 +0000 2022",
+	"tweets": 94157,
+	"created_at": "Mon Jun 20 20:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Mon Jun 20 19:53:21 +0000 2022",
+		"id": 1538973298826129400,
+		"id_str": "1538973298826129415",
+		"full_text": "@Untertacker Ironically todays lunch was hotdogs lacking any entry on a block chain nor any NFT seasoning. Have a nice day ;-) https://t.co/7Qqv9ZAzos",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Untertacker",
+					"name": "Under Taker",
+					"id": 4688709284,
+					"id_str": "4688709284",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1538973284548722700,
+					"id_str": "1538973284548722688",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FVuIDGhXwAAdrOB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVuIDGhXwAAdrOB.jpg",
+					"url": "https://t.co/7Qqv9ZAzos",
+					"display_url": "pic.twitter.com/7Qqv9ZAzos",
+					"expanded_url": "https://twitter.com/cogdog/status/1538973298826129415/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 531,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 938,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1538973284548722700,
+					"id_str": "1538973284548722688",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FVuIDGhXwAAdrOB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVuIDGhXwAAdrOB.jpg",
+					"url": "https://t.co/7Qqv9ZAzos",
+					"display_url": "pic.twitter.com/7Qqv9ZAzos",
+					"expanded_url": "https://twitter.com/cogdog/status/1538973298826129415/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 531,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 938,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1538943255508820000,
+		"in_reply_to_status_id_str": "1538943255508819968",
+		"in_reply_to_user_id": 4688709284,
+		"in_reply_to_user_id_str": "4688709284",
+		"in_reply_to_screen_name": "Untertacker",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10442,
+			"friends_count": 1550,
+			"listed_count": 782,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 938,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95385,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Untertacker Ironically todays lunch was hotdogs lacking any entry on a block chain nor any NFT seasoning. Have a nice day ;-) https://t.co/7Qqv9ZAzos"
+	},
+	{
 		"created_at": "Mon Jun 20 18:48:47 +0000 2022",
 		"id": 1538957049651937300,
 		"id_str": "1538957049651937280",

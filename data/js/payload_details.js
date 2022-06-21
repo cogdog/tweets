d70fd71b7f8eb3430b03b6453224f29ad31250c2
Alan Levine <cogdogblog@gmail.com>
2022-06-21 17:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94169,
-	"created_at": "Tue Jun 21 20:15:09 +0000 2022",
+	"tweets": 94170,
+	"created_at": "Tue Jun 21 23:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Tue Jun 21 22:35:49 +0000 2022",
+		"id": 1539376569545891800,
+		"id_str": "1539376569545891840",
+		"full_text": "We are looking for more voices to add to opening segment of the OEG Voices Podcast. \n\nJust record 2 paragraphs using something like @Sodaphonic Boombox https://t.co/b4RxSOD1NW &amp; share with us https://t.co/JNsGYIv878\n\nSee how the mixer works https://t.co/HnH5s2yPGX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sodaphonic",
+					"name": "Sodaphonic",
+					"id": 771247405454856200,
+					"id_str": "771247405454856193",
+					"indices": [
+						132,
+						143
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/b4RxSOD1NW",
+					"expanded_url": "https://sodaphonic.com/boombox",
+					"display_url": "sodaphonic.com/boombox",
+					"indices": [
+						152,
+						175
+					]
+				},
+				{
+					"url": "https://t.co/JNsGYIv878",
+					"expanded_url": "https://connect.oeglobal.org/t/contribute-your-voice-to-oeg-voices-opening-segment/3213/39",
+					"display_url": "connect.oeglobal.org/t/contribute-y…",
+					"indices": [
+						196,
+						219
+					]
+				},
+				{
+					"url": "https://t.co/HnH5s2yPGX",
+					"expanded_url": "https://cogdog.github.io/mixvoices/",
+					"display_url": "cogdog.github.io/mixvoices/",
+					"indices": [
+						245,
+						268
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10442,
+			"friends_count": 1551,
+			"listed_count": 782,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 938,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95403,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are looking for more voices to add to opening segment of the OEG Voices Podcast. \n\nJust record 2 paragraphs using something like @Sodaphonic Boombox https://t.co/b4RxSOD1NW &amp; share with us https://t.co/JNsGYIv878\n\nSee how the mixer works https://t.co/HnH5s2yPGX"
+	},
+	{
 		"created_at": "Tue Jun 21 19:24:28 +0000 2022",
 		"id": 1539328417753088000,
 		"id_str": "1539328417753088001",

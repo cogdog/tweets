673e55a08a8003e65607285cbfb92930e2a5513b
Alan Levine <cogdogblog@gmail.com>
2022-07-03 21:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94230,
-	"created_at": "Sat Jul 02 03:15:08 +0000 2022",
+	"tweets": 94231,
+	"created_at": "Mon Jul 04 03:15:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Mon Jul 04 02:42:28 +0000 2022",
+		"id": 1543787298768257000,
+		"id_str": "1543787298768257026",
+		"full_text": "@ds106dc #tdc3825 #ds106 #myFest22 I inserted a story to write today's #dailycreate but #NoExcuses \n\nPlanning a podcast call with colleagues in New Zealand &amp; France reminded me of Earth Sandwich by @zefrank I once stayed in Wanaka NZ but needed a partner in Camariñas, Spain https://t.co/p4FRkfqpX0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc3825",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						18,
+						24
+					]
+				},
+				{
+					"text": "myFest22",
+					"indices": [
+						25,
+						34
+					]
+				},
+				{
+					"text": "dailycreate",
+					"indices": [
+						71,
+						83
+					]
+				},
+				{
+					"text": "NoExcuses",
+					"indices": [
+						88,
+						98
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "zefrank",
+					"name": "True Facts by Ze Frank",
+					"id": 11340982,
+					"id_str": "11340982",
+					"indices": [
+						202,
+						210
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1543787032148942800,
+					"id_str": "1543787032148942848",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FWyiIFwXoAANJUh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWyiIFwXoAANJUh.jpg",
+					"url": "https://t.co/p4FRkfqpX0",
+					"display_url": "pic.twitter.com/p4FRkfqpX0",
+					"expanded_url": "https://twitter.com/cogdog/status/1543787298768257026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 741,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2041,
+							"h": 1260,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 420,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1543787032148942800,
+					"id_str": "1543787032148942848",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FWyiIFwXoAANJUh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWyiIFwXoAANJUh.jpg",
+					"url": "https://t.co/p4FRkfqpX0",
+					"display_url": "pic.twitter.com/p4FRkfqpX0",
+					"expanded_url": "https://twitter.com/cogdog/status/1543787298768257026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 741,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2041,
+							"h": 1260,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 420,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10453,
+			"friends_count": 1551,
+			"listed_count": 783,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 941,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95479,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc #tdc3825 #ds106 #myFest22 I inserted a story to write today's #dailycreate but #NoExcuses \n\nPlanning a podcast call with colleagues in New Zealand &amp; France reminded me of Earth Sandwich by @zefrank I once stayed in Wanaka NZ but needed a partner in Camariñas, Spain https://t.co/p4FRkfqpX0"
+	},
+	{
 		"created_at": "Sat Jul 02 02:20:33 +0000 2022",
 		"id": 1543057007439237000,
 		"id_str": "1543057007439237120",

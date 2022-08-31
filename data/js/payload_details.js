8689f9da2c81836d0098482518cdf4087d4cf69d
Alan Levine <cogdogblog@gmail.com>
2022-08-31 03:15:10 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94396,
-	"created_at": "Wed Aug 31 04:15:09 +0000 2022",
+	"tweets": 94397,
+	"created_at": "Wed Aug 31 07:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 31 06:22:56 +0000 2022",
+		"id": 1564861275821195300,
+		"id_str": "1564861275821195267",
+		"full_text": "@ds106dc #tdc3883 #ds106 #myFest22 \"So fellow euarchontoglires, what say we go for a ride on your yacht and discuss all this true ruling we must do?  ref: https://t.co/rCNT0Vd8qO\n\nhttps://t.co/0TVx21sut6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc3883",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						18,
+						24
+					]
+				},
+				{
+					"text": "myFest22",
+					"indices": [
+						25,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rCNT0Vd8qO",
+					"expanded_url": "https://twitter.com/parasociality/status/1558941229638885382",
+					"display_url": "twitter.com/parasociality/…",
+					"indices": [
+						155,
+						178
+					]
+				},
+				{
+					"url": "https://t.co/0TVx21sut6",
+					"expanded_url": "https://www.flickr.com/photos/cogdog/7325405708",
+					"display_url": "flickr.com/photos/cogdog/…",
+					"indices": [
+						180,
+						203
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10453,
+			"friends_count": 1551,
+			"listed_count": 782,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 947,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95685,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1558941229638885400,
+		"quoted_status_id_str": "1558941229638885382",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rCNT0Vd8qO",
+			"expanded": "https://twitter.com/parasociality/status/1558941229638885382",
+			"display": "twitter.com/parasociality/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Aug 14 22:18:47 +0000 2022",
+			"id": 1558941229638885400,
+			"id_str": "1558941229638885382",
+			"full_text": "You and rabbits are both euarchontoglires, which hilariously translates to: \"True rulers and dormice.\"\n\nThis superorder includes both glires (\"dormice,\" rabbits and rodents) and euarchons (\"true rulers,\" the somewhat arrogant name for the group that includes humans). https://t.co/BvH0sOPMu3",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				267
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1558937764955136000,
+						"id_str": "1558937764955136000",
+						"indices": [
+							268,
+							291
+						],
+						"media_url": "http://pbs.twimg.com/media/FaJ1ozsXoAAxG6N.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FaJ1ozsXoAAxG6N.jpg",
+						"url": "https://t.co/BvH0sOPMu3",
+						"display_url": "pic.twitter.com/BvH0sOPMu3",
+						"expanded_url": "https://twitter.com/parasociality/status/1558941229638885382/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 605,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 605,
+								"h": 453,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 605,
+								"h": 453,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1558937764955136000,
+						"id_str": "1558937764955136000",
+						"indices": [
+							268,
+							291
+						],
+						"media_url": "http://pbs.twimg.com/media/FaJ1ozsXoAAxG6N.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FaJ1ozsXoAAxG6N.jpg",
+						"url": "https://t.co/BvH0sOPMu3",
+						"display_url": "pic.twitter.com/BvH0sOPMu3",
+						"expanded_url": "https://twitter.com/parasociality/status/1558941229638885382/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 605,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 605,
+								"h": 453,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 605,
+								"h": 453,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": 1558937674215563300,
+			"in_reply_to_status_id_str": "1558937674215563272",
+			"in_reply_to_user_id": 1407370842363908000,
+			"in_reply_to_user_id_str": "1407370842363908102",
+			"in_reply_to_screen_name": "parasociality",
+			"user": {
+				"id": 1407370842363908000,
+				"id_str": "1407370842363908102",
+				"name": "𝕻𝖆𝖗𝖆𝖘𝖔𝖈𝖎𝖆𝖑𝖎𝖙𝖞",
+				"screen_name": "parasociality",
+				"location": "Council Bluffs, IA",
+				"description": "Aspiring to be Twitter's friendliest anon. DMs always open and appreciated! Replies may not always be swift.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3167,
+				"friends_count": 193,
+				"listed_count": 28,
+				"created_at": "Tue Jun 22 16:17:14 +0000 2021",
+				"favourites_count": 15086,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 11209,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1493478146976493570/HNU0WcpG_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1493478146976493570/HNU0WcpG_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1407370842363908102/1640987692",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 414,
+			"favorite_count": 7922,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc #tdc3883 #ds106 #myFest22 \"So fellow euarchontoglires, what say we go for a ride on your yacht and discuss all this true ruling we must do?  ref: https://t.co/rCNT0Vd8qO\n\nhttps://t.co/0TVx21sut6"
+	},
+	{
 		"created_at": "Wed Aug 31 03:29:15 +0000 2022",
 		"id": 1564817566991876000,
 		"id_str": "1564817566991876096",

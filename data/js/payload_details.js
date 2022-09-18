ad60202197853ff7624eb06ce5fee3ba1ee3b2e5
Alan Levine <cogdogblog@gmail.com>
2022-09-18 14:15:09 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94429,
-	"created_at": "Sun Sep 18 14:15:08 +0000 2022",
+	"tweets": 94430,
+	"created_at": "Sun Sep 18 18:15:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sun Sep 18 17:47:35 +0000 2022",
+		"id": 1571556553697927200,
+		"id_str": "1571556553697927170",
+		"full_text": "@kbhildebrandt @courosa I just burned a wix feed w/ no problem https://t.co/EsfjBvSW3O but that probably does not help.\n\nAgreed it is questionable. Can try something like https://t.co/R80o8VwNqf to subscribe to  feed then reshare as RSS. Looks like it worked in FWP.",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			266
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kbhildebrandt",
+					"name": "Dr. Katia Hildebrandt",
+					"id": 1101780642,
+					"id_str": "1101780642",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "courosa",
+					"name": "Dr. Alec Couros",
+					"id": 739293,
+					"id_str": "739293",
+					"indices": [
+						15,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/EsfjBvSW3O",
+					"expanded_url": "https://feeds.feedburner.com/wix/pTVZB19Hu3w",
+					"display_url": "feeds.feedburner.com/wix/pTVZB19Hu3w",
+					"indices": [
+						63,
+						86
+					]
+				},
+				{
+					"url": "https://t.co/R80o8VwNqf",
+					"expanded_url": "http://follow.it",
+					"display_url": "follow.it",
+					"indices": [
+						171,
+						194
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1571528967651299300,
+		"in_reply_to_status_id_str": "1571528967651299330",
+		"in_reply_to_user_id": 1101780642,
+		"in_reply_to_user_id_str": "1101780642",
+		"in_reply_to_screen_name": "kbhildebrandt",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10456,
+			"friends_count": 1552,
+			"listed_count": 782,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 948,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95722,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@kbhildebrandt @courosa I just burned a wix feed w/ no problem https://t.co/EsfjBvSW3O but that probably does not help.\n\nAgreed it is questionable. Can try something like https://t.co/R80o8VwNqf to subscribe to  feed then reshare as RSS. Looks like it worked in FWP."
+	},
+	{
 		"created_at": "Sun Sep 18 14:00:52 +0000 2022",
 		"id": 1571499497985052700,
 		"id_str": "1571499497985052672",

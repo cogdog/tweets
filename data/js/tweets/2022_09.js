8d1da1315c8e68cd2ab588e79755f5381ff64526
Alan Levine <cogdogblog@gmail.com>
2022-09-26 21:15:15 -0600

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 27 03:05:06 +0000 2022",
+		"id": 1574595959614570500,
+		"id_str": "1574595959614570496",
+		"full_text": "Checkmate! Maybe the most fun you can have clicking web form checkboxes. \n\nI'm sure you can easily beat my 50 score. A gem by @hakimel https://t.co/VnQNgTJnp1 https://t.co/GgSwqh26XY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hakimel",
+					"name": "Hakim El Hattab",
+					"id": 73339662,
+					"id_str": "73339662",
+					"indices": [
+						126,
+						134
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VnQNgTJnp1",
+					"expanded_url": "https://lab.hakim.se/fullstack/",
+					"display_url": "lab.hakim.se/fullstack/",
+					"indices": [
+						135,
+						158
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1574595371065647000,
+					"id_str": "1574595371065647115",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/FdoWJb1XoAsSZmQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdoWJb1XoAsSZmQ.jpg",
+					"url": "https://t.co/GgSwqh26XY",
+					"display_url": "pic.twitter.com/GgSwqh26XY",
+					"expanded_url": "https://twitter.com/cogdog/status/1574595959614570496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 458,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 806,
+							"h": 1196,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 806,
+							"h": 1196,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1574595371065647000,
+					"id_str": "1574595371065647115",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/FdoWJb1XoAsSZmQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdoWJb1XoAsSZmQ.jpg",
+					"url": "https://t.co/GgSwqh26XY",
+					"display_url": "pic.twitter.com/GgSwqh26XY",
+					"expanded_url": "https://twitter.com/cogdog/status/1574595959614570496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 458,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 806,
+							"h": 1196,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 806,
+							"h": 1196,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10455,
+			"friends_count": 1555,
+			"listed_count": 783,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 948,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95750,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Checkmate! Maybe the most fun you can have clicking web form checkboxes. \n\nI'm sure you can easily beat my 50 score. A gem by @hakimel https://t.co/VnQNgTJnp1 https://t.co/GgSwqh26XY"
+	},
+	{
 		"created_at": "Mon Sep 26 23:26:10 +0000 2022",
 		"id": 1574540865980203000,
 		"id_str": "1574540865980203009",

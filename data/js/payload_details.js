b298d2d60fc01f5e09d94855147ee29f32a7ab32
Alan Levine <cogdogblog@gmail.com>
2022-10-01 12:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94467,
-	"created_at": "Fri Sep 30 23:15:09 +0000 2022",
+	"tweets": 94468,
+	"created_at": "Sat Oct 01 18:15:08 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_10.js
@@ -0,0 +1,149 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Sat Oct 01 17:35:12 +0000 2022",
+		"id": 1576264479725129700,
+		"id_str": "1576264479725129730",
+		"full_text": "@doctorow Now it’s noticed— https://t.co/qylMMYywxt and getting fixed. Highly recommend Openverse the https://t.co/QOEKM0lLsl CC image/audio search formerly at Creative Commons now at Wordpress, 600 million items https://t.co/QOEKM0lLsl",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			236
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "doctorow",
+					"name": "Cory Doctorow",
+					"id": 2729061,
+					"id_str": "2729061",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qylMMYywxt",
+					"expanded_url": "https://cogdogblog.com/2022/09/google-broke-cc-image-search/",
+					"display_url": "cogdogblog.com/2022/09/google…",
+					"indices": [
+						28,
+						51
+					]
+				},
+				{
+					"url": "https://t.co/QOEKM0lLsl",
+					"expanded_url": "https://wordpress.org/openverse/",
+					"display_url": "wordpress.org/openverse/",
+					"indices": [
+						102,
+						125
+					]
+				},
+				{
+					"url": "https://t.co/QOEKM0lLsl",
+					"expanded_url": "https://wordpress.org/openverse/",
+					"display_url": "wordpress.org/openverse/",
+					"indices": [
+						213,
+						236
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1576217046538281000,
+		"in_reply_to_status_id_str": "1576217046538280965",
+		"in_reply_to_user_id": 2729061,
+		"in_reply_to_user_id_str": "2729061",
+		"in_reply_to_screen_name": "doctorow",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10465,
+			"friends_count": 1555,
+			"listed_count": 783,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 948,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95766,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@doctorow Now it’s noticed— https://t.co/qylMMYywxt and getting fixed. Highly recommend Openverse the https://t.co/QOEKM0lLsl CC image/audio search formerly at Creative Commons now at Wordpress, 600 million items https://t.co/QOEKM0lLsl"
+	}
+]

f28c1e810e20189b94272184b0d495eb0470e64b
Alan Levine <cogdogblog@gmail.com>
2022-10-21 11:15:11 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94563,
-	"created_at": "Fri Oct 21 15:15:09 +0000 2022",
+	"tweets": 94564,
+	"created_at": "Fri Oct 21 17:15:10 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,182 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Fri Oct 21 16:28:40 +0000 2022",
+		"id": 1583495495070396400,
+		"id_str": "1583495495070396417",
+		"full_text": "@brlamb @VivienRolfe @dkernohan @grantpotter I will pile on the gratitude! Y'all came to see a cactus, visit the Flying Grizzly, and hosted me for a poker game with a vicar.\n\nhttps://t.co/wHagFLcfCy\nhttps://t.co/DL8FpiPHEu\nhttps://t.co/glAEusShAy",
+		"truncated": false,
+		"display_text_range": [
+			45,
+			246
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "brlamb",
+					"name": "Brian Lamb",
+					"id": 745903,
+					"id_str": "745903",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "VivienRolfe",
+					"name": "Dr Viv Rolfe 🍵🎵🌿🎷💚Head of Herbal Research",
+					"id": 214901910,
+					"id_str": "214901910",
+					"indices": [
+						8,
+						20
+					]
+				},
+				{
+					"screen_name": "dkernohan",
+					"name": "David Kernohan",
+					"id": 12219232,
+					"id_str": "12219232",
+					"indices": [
+						21,
+						31
+					]
+				},
+				{
+					"screen_name": "grantpotter",
+					"name": "Grant Potter",
+					"id": 6271482,
+					"id_str": "6271482",
+					"indices": [
+						32,
+						44
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wHagFLcfCy",
+					"expanded_url": "https://www.flickr.com/photos/cogdog/10772155625",
+					"display_url": "flickr.com/photos/cogdog/…",
+					"indices": [
+						175,
+						198
+					]
+				},
+				{
+					"url": "https://t.co/DL8FpiPHEu",
+					"expanded_url": "https://www.flickr.com/photos/cogdog/10793779735/",
+					"display_url": "flickr.com/photos/cogdog/…",
+					"indices": [
+						199,
+						222
+					]
+				},
+				{
+					"url": "https://t.co/glAEusShAy",
+					"expanded_url": "https://www.flickr.com/photos/cogdog/14088229125/",
+					"display_url": "flickr.com/photos/cogdog/…",
+					"indices": [
+						223,
+						246
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1583488068665114600,
+		"in_reply_to_status_id_str": "1583488068665114625",
+		"in_reply_to_user_id": 745903,
+		"in_reply_to_user_id_str": "745903",
+		"in_reply_to_screen_name": "brlamb",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10467,
+			"friends_count": 1558,
+			"listed_count": 783,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 950,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95891,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@brlamb @VivienRolfe @dkernohan @grantpotter I will pile on the gratitude! Y'all came to see a cactus, visit the Flying Grizzly, and hosted me for a poker game with a vicar.\n\nhttps://t.co/wHagFLcfCy\nhttps://t.co/DL8FpiPHEu\nhttps://t.co/glAEusShAy"
+	},
+	{
 		"created_at": "Fri Oct 21 14:45:41 +0000 2022",
 		"id": 1583469579657175000,
 		"id_str": "1583469579657175040",

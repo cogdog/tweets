564f5ba02c34279789f35151b05a6bb195d7b2b9
Alan Levine <cogdogblog@gmail.com>
2022-11-07 10:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94630,
-	"created_at": "Mon Nov 07 02:15:08 +0000 2022",
+	"tweets": 94631,
+	"created_at": "Mon Nov 07 16:15:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Mon Nov 07 15:43:49 +0000 2022",
+		"id": 1589644801967882200,
+		"id_str": "1589644801967882241",
+		"full_text": "Email phishing efforts make me bust out laughing. The prestigious 'Who's Who in America®\" sends invites as blurry text images sent from an address like \"info@udhfshd.dynu.com\" --and I feel the personalization of \"Dear First Name\"\n\nPTBarnum sits up in a grave and giggles. https://t.co/swwMnJlUsh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1589644137640362000,
+					"id_str": "1589644137640361985",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/Fg-M679WAAEJ8E7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fg-M679WAAEJ8E7.jpg",
+					"url": "https://t.co/swwMnJlUsh",
+					"display_url": "pic.twitter.com/swwMnJlUsh",
+					"expanded_url": "https://twitter.com/cogdog/status/1589644801967882241/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1784,
+							"h": 1020,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 686,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 389,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1589644137640362000,
+					"id_str": "1589644137640361985",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/Fg-M679WAAEJ8E7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fg-M679WAAEJ8E7.jpg",
+					"url": "https://t.co/swwMnJlUsh",
+					"display_url": "pic.twitter.com/swwMnJlUsh",
+					"expanded_url": "https://twitter.com/cogdog/status/1589644801967882241/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1784,
+							"h": 1020,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 686,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 389,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10359,
+			"friends_count": 1538,
+			"listed_count": 776,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 952,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 95978,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Email phishing efforts make me bust out laughing. The prestigious 'Who's Who in America®\" sends invites as blurry text images sent from an address like \"info@udhfshd.dynu.com\" --and I feel the personalization of \"Dear First Name\"\n\nPTBarnum sits up in a grave and giggles. https://t.co/swwMnJlUsh"
+	},
+	{
 		"created_at": "Mon Nov 07 02:14:47 +0000 2022",
 		"id": 1589441200259580000,
 		"id_str": "1589441200259579905",

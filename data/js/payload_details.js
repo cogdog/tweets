526bab5ad9938c6f0485fe3f65013561daffd227
Alan Levine <cogdogblog@gmail.com>
2022-11-16 13:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94648,
-	"created_at": "Wed Nov 16 18:15:10 +0000 2022",
+	"tweets": 94649,
+	"created_at": "Wed Nov 16 19:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 16 18:54:16 +0000 2022",
+		"id": 1592954222038892500,
+		"id_str": "1592954222038892545",
+		"full_text": "@TwitterSupport For that matter I disabled settings for \"Allow message requests from everyone\" -- should mean spammers like Jared and Loretta cant DM me -- have you read your own support text? https://t.co/AJtpOoOdYi \n\n#TwitterIsWorseThanBroken #TheSoundOfTwitterSwirlingDownThePipe https://t.co/PZ9QiguQcP",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TwitterIsWorseThanBroken",
+					"indices": [
+						219,
+						244
+					]
+				},
+				{
+					"text": "TheSoundOfTwitterSwirlingDownThePipe",
+					"indices": [
+						245,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TwitterSupport",
+					"name": "Twitter Support",
+					"id": 17874544,
+					"id_str": "17874544",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AJtpOoOdYi",
+					"expanded_url": "https://help.twitter.com/en/using-twitter/direct-messages#receive",
+					"display_url": "help.twitter.com/en/using-twitt…",
+					"indices": [
+						193,
+						216
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1592953170283593700,
+					"id_str": "1592953170283593728",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FhtOd_KWAAAT1TV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhtOd_KWAAAT1TV.jpg",
+					"url": "https://t.co/PZ9QiguQcP",
+					"display_url": "pic.twitter.com/PZ9QiguQcP",
+					"expanded_url": "https://twitter.com/cogdog/status/1592954222038892545/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 192,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1228,
+							"h": 346,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 338,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1592953170283593700,
+					"id_str": "1592953170283593728",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FhtOd_KWAAAT1TV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhtOd_KWAAAT1TV.jpg",
+					"url": "https://t.co/PZ9QiguQcP",
+					"display_url": "pic.twitter.com/PZ9QiguQcP",
+					"expanded_url": "https://twitter.com/cogdog/status/1592954222038892545/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 192,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1228,
+							"h": 346,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 338,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1592953384880963600,
+					"id_str": "1592953384880963586",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FhtOqemWAAImdFm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhtOqemWAAImdFm.jpg",
+					"url": "https://t.co/PZ9QiguQcP",
+					"display_url": "pic.twitter.com/PZ9QiguQcP",
+					"expanded_url": "https://twitter.com/cogdog/status/1592954222038892545/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 531,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 301,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1348,
+							"h": 596,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1592953805309714400,
+					"id_str": "1592953805309714432",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FhtPC80XoAApIwC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhtPC80XoAApIwC.jpg",
+					"url": "https://t.co/PZ9QiguQcP",
+					"display_url": "pic.twitter.com/PZ9QiguQcP",
+					"expanded_url": "https://twitter.com/cogdog/status/1592954222038892545/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 658,
+							"h": 980,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 658,
+							"h": 980,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 457,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1592952803122708500,
+		"in_reply_to_status_id_str": "1592952803122708480",
+		"in_reply_to_user_id": 740343,
+		"in_reply_to_user_id_str": "740343",
+		"in_reply_to_screen_name": "cogdog",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10326,
+			"friends_count": 1534,
+			"listed_count": 773,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 951,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96000,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@TwitterSupport For that matter I disabled settings for \"Allow message requests from everyone\" -- should mean spammers like Jared and Loretta cant DM me -- have you read your own support text? https://t.co/AJtpOoOdYi \n\n#TwitterIsWorseThanBroken #TheSoundOfTwitterSwirlingDownThePipe https://t.co/PZ9QiguQcP"
+	},
+	{
 		"created_at": "Wed Nov 16 17:40:57 +0000 2022",
 		"id": 1592935768548429800,
 		"id_str": "1592935768548429827",

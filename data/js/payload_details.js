565271aaa5bdf03fc474343e67106726ee56e6eb
Alan Levine <cogdogblog@gmail.com>
2022-11-30 10:15:16 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94700,
-	"created_at": "Wed Nov 30 05:15:09 +0000 2022",
+	"tweets": 94701,
+	"created_at": "Wed Nov 30 16:15:15 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,274 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 30 16:04:55 +0000 2022",
+		"id": 1597985031451975700,
+		"id_str": "1597985031451975680",
+		"full_text": "@KelsonV Thanks for this, can see it was exposing my app key! Editing in Mastodon App was easy, also note you can name it whatever you want, need not be IFTTT.\n\nEasy change! https://t.co/I1nMAUmJCR",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			173
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KelsonV",
+					"name": "Kelson Vibber",
+					"id": 16940840,
+					"id_str": "16940840",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1597984319028461600,
+					"id_str": "1597984319028461571",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi0uRYxWAAM-ucr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi0uRYxWAAM-ucr.jpg",
+					"url": "https://t.co/I1nMAUmJCR",
+					"display_url": "pic.twitter.com/I1nMAUmJCR",
+					"expanded_url": "https://twitter.com/cogdog/status/1597985031451975680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 918,
+							"h": 334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 247,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 918,
+							"h": 334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1597984319028461600,
+					"id_str": "1597984319028461571",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi0uRYxWAAM-ucr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi0uRYxWAAM-ucr.jpg",
+					"url": "https://t.co/I1nMAUmJCR",
+					"display_url": "pic.twitter.com/I1nMAUmJCR",
+					"expanded_url": "https://twitter.com/cogdog/status/1597985031451975680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 918,
+							"h": 334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 247,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 918,
+							"h": 334,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1597984527745519600,
+					"id_str": "1597984527745519619",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi0udiTXkAMaJfu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi0udiTXkAMaJfu.jpg",
+					"url": "https://t.co/I1nMAUmJCR",
+					"display_url": "pic.twitter.com/I1nMAUmJCR",
+					"expanded_url": "https://twitter.com/cogdog/status/1597985031451975680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1702,
+							"h": 390,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 275,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 156,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1597984761888260000,
+					"id_str": "1597984761888260097",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi0urKjWQAEfv6u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi0urKjWQAEfv6u.jpg",
+					"url": "https://t.co/I1nMAUmJCR",
+					"display_url": "pic.twitter.com/I1nMAUmJCR",
+					"expanded_url": "https://twitter.com/cogdog/status/1597985031451975680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 518,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 650,
+							"h": 854,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 650,
+							"h": 854,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1597848688864690200,
+		"in_reply_to_status_id_str": "1597848688864690177",
+		"in_reply_to_user_id": 16940840,
+		"in_reply_to_user_id_str": "16940840",
+		"in_reply_to_screen_name": "KelsonV",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10246,
+			"friends_count": 1530,
+			"listed_count": 769,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 946,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96057,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@KelsonV Thanks for this, can see it was exposing my app key! Editing in Mastodon App was easy, also note you can name it whatever you want, need not be IFTTT.\n\nEasy change! https://t.co/I1nMAUmJCR"
+	},
+	{
 		"created_at": "Wed Nov 30 04:49:22 +0000 2022",
 		"id": 1597815024298647600,
 		"id_str": "1597815024298647557",

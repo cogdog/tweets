f70bab7826d9bcea17dab0f827bac2df824723dd
Alan Levine <cogdogblog@gmail.com>
2022-12-08 11:15:10 -0600

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94722,
-	"created_at": "Thu Dec 08 15:15:09 +0000 2022",
+	"tweets": 94724,
+	"created_at": "Thu Dec 08 17:15:09 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,555 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Thu Dec 08 17:07:41 +0000 2022",
+		"id": 1600899931375636500,
+		"id_str": "1600899931375636485",
+		"full_text": "@ds106dc #tdc3982   ai = lame\n\nIt knows not of #ds106 or the #dailycreate .... https://t.co/zwhOBwTL9e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc3982",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						47,
+						53
+					]
+				},
+				{
+					"text": "dailycreate",
+					"indices": [
+						61,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1600899404302540800,
+					"id_str": "1600899404302540800",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/FjeJhrqWYAA9Qz2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjeJhrqWYAA9Qz2.jpg",
+					"url": "https://t.co/zwhOBwTL9e",
+					"display_url": "pic.twitter.com/zwhOBwTL9e",
+					"expanded_url": "https://twitter.com/cogdog/status/1600899931375636485/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1650,
+							"h": 1330,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 967,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 548,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1600899404302540800,
+					"id_str": "1600899404302540800",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/FjeJhrqWYAA9Qz2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjeJhrqWYAA9Qz2.jpg",
+					"url": "https://t.co/zwhOBwTL9e",
+					"display_url": "pic.twitter.com/zwhOBwTL9e",
+					"expanded_url": "https://twitter.com/cogdog/status/1600899931375636485/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1650,
+							"h": 1330,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 967,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 548,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10234,
+			"friends_count": 1532,
+			"listed_count": 768,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 938,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96085,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc #tdc3982   ai = lame\n\nIt knows not of #ds106 or the #dailycreate .... https://t.co/zwhOBwTL9e"
+	},
+	{
+		"created_at": "Thu Dec 08 17:02:12 +0000 2022",
+		"id": 1600898552611356700,
+		"id_str": "1600898552611356673",
+		"full_text": "Hector needs to make some turtle art! https://t.co/dhtG0mkEVU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dhtG0mkEVU",
+					"expanded_url": "https://twitter.com/HAKretschmer/status/1600867723084447744",
+					"display_url": "twitter.com/HAKretschmer/s…",
+					"indices": [
+						38,
+						61
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10234,
+			"friends_count": 1532,
+			"listed_count": 768,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 938,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96085,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1600867723084447700,
+		"quoted_status_id_str": "1600867723084447744",
+		"quoted_status_permalink": {
+			"url": "https://t.co/dhtG0mkEVU",
+			"expanded": "https://twitter.com/HAKretschmer/status/1600867723084447744",
+			"display": "twitter.com/HAKretschmer/s…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Dec 08 14:59:42 +0000 2022",
+			"id": 1600867723084447700,
+			"id_str": "1600867723084447744",
+			"full_text": "@ds106dc #tdc3982 #ds106 @dogtrax \n\nhttps://t.co/0HKXzws6a7 did not require signing up. I love how Hector was good at steering the conversation in the direction he wanted. 😁 https://t.co/gCHWz7S7WU",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				173
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "tdc3982",
+						"indices": [
+							9,
+							17
+						]
+					},
+					{
+						"text": "ds106",
+						"indices": [
+							18,
+							24
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ds106dc",
+						"name": "ds106 Daily Create",
+						"id": 995562187,
+						"id_str": "995562187",
+						"indices": [
+							0,
+							8
+						]
+					},
+					{
+						"screen_name": "dogtrax",
+						"name": "KevinHodgson",
+						"id": 13307352,
+						"id_str": "13307352",
+						"indices": [
+							25,
+							33
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/0HKXzws6a7",
+						"expanded_url": "https://ochatbot.com/hector/",
+						"display_url": "ochatbot.com/hector/",
+						"indices": [
+							36,
+							59
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1600867155049865200,
+						"id_str": "1600867155049865219",
+						"indices": [
+							174,
+							197
+						],
+						"media_url": "http://pbs.twimg.com/media/FjdsMh1WYAMWz32.png",
+						"media_url_https": "https://pbs.twimg.com/media/FjdsMh1WYAMWz32.png",
+						"url": "https://t.co/gCHWz7S7WU",
+						"display_url": "pic.twitter.com/gCHWz7S7WU",
+						"expanded_url": "https://twitter.com/HAKretschmer/status/1600867723084447744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 347,
+								"h": 446,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 347,
+								"h": 446,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 347,
+								"h": 446,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1600867155049865200,
+						"id_str": "1600867155049865219",
+						"indices": [
+							174,
+							197
+						],
+						"media_url": "http://pbs.twimg.com/media/FjdsMh1WYAMWz32.png",
+						"media_url_https": "https://pbs.twimg.com/media/FjdsMh1WYAMWz32.png",
+						"url": "https://t.co/gCHWz7S7WU",
+						"display_url": "pic.twitter.com/gCHWz7S7WU",
+						"expanded_url": "https://twitter.com/HAKretschmer/status/1600867723084447744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 347,
+								"h": 446,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 347,
+								"h": 446,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 347,
+								"h": 446,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 995562187,
+			"in_reply_to_user_id_str": "995562187",
+			"in_reply_to_screen_name": "ds106dc",
+			"user": {
+				"id": 1492155646988464000,
+				"id_str": "1492155646988464139",
+				"name": "Heather Kretschmer",
+				"screen_name": "HAKretschmer",
+				"location": "",
+				"description": "\"Development is really the main thing in life, just as it is in music; because development means change, growing, blossoming out.\" - Leonard Bernstein",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 45,
+				"friends_count": 44,
+				"listed_count": 0,
+				"created_at": "Fri Feb 11 15:18:02 +0000 2022",
+				"favourites_count": 2906,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 317,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1532774945339719680/3vCr7Co8_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1532774945339719680/3vCr7Co8_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1492155646988464139/1644681010",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hector needs to make some turtle art! https://t.co/dhtG0mkEVU"
+	},
+	{
 		"created_at": "Thu Dec 08 14:32:10 +0000 2022",
 		"id": 1600860795730071600,
 		"id_str": "1600860795730071559",

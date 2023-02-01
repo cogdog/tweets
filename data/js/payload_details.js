c6a62a7657bb351fd64a3c5d0fe57af72c8e22c2
Alan Levine <cogdogblog@gmail.com>
2023-02-01 13:15:20 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94886,
-	"created_at": "Tue Jan 31 23:15:09 +0000 2023",
+	"tweets": 94887,
+	"created_at": "Wed Feb 01 19:15:09 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_02.js
@@ -0,0 +1,238 @@
+Grailbird.data.tweets_2023_02 = 
+[
+	{
+		"created_at": "Wed Feb 01 19:12:34 +0000 2023",
+		"id": 1620862692457123800,
+		"id_str": "1620862692457123844",
+		"full_text": "@ds106dc  #dailycreate ... #tdc4037 #ds106  Protobot served me an easy lob: \"Design a storybook that can keep a secret\"-- That's a #SPLOT! https://t.co/NsqBhedHE9 https://t.co/WUjv4fqVot",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "dailycreate",
+					"indices": [
+						10,
+						22
+					]
+				},
+				{
+					"text": "tdc4037",
+					"indices": [
+						27,
+						35
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						36,
+						42
+					]
+				},
+				{
+					"text": "SPLOT",
+					"indices": [
+						131,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NsqBhedHE9",
+					"expanded_url": "https://whisper.arganee.world/",
+					"display_url": "whisper.arganee.world",
+					"indices": [
+						139,
+						162
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1620862336889143300,
+					"id_str": "1620862336889143298",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn51tSwWQAI-q6u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn51tSwWQAI-q6u.jpg",
+					"url": "https://t.co/WUjv4fqVot",
+					"display_url": "pic.twitter.com/WUjv4fqVot",
+					"expanded_url": "https://twitter.com/cogdog/status/1620862692457123844/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1280,
+							"h": 690,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 647,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 367,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1620862336889143300,
+					"id_str": "1620862336889143298",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn51tSwWQAI-q6u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn51tSwWQAI-q6u.jpg",
+					"url": "https://t.co/WUjv4fqVot",
+					"display_url": "pic.twitter.com/WUjv4fqVot",
+					"expanded_url": "https://twitter.com/cogdog/status/1620862692457123844/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1280,
+							"h": 690,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 647,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 367,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10193,
+			"friends_count": 1527,
+			"listed_count": 765,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 934,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96296,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc  #dailycreate ... #tdc4037 #ds106  Protobot served me an easy lob: \"Design a storybook that can keep a secret\"-- That's a #SPLOT! https://t.co/NsqBhedHE9 https://t.co/WUjv4fqVot"
+	}
+]

3ef53c9990fd611282d6db91e602008f513989cc
Alan Levine <cogdogblog@gmail.com>
2023-02-03 14:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94902,
-	"created_at": "Fri Feb 03 17:15:08 +0000 2023",
+	"tweets": 94903,
+	"created_at": "Fri Feb 03 20:15:09 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,346 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Fri Feb 03 19:59:57 +0000 2023",
+		"id": 1621599392552599600,
+		"id_str": "1621599392552599552",
+		"full_text": "#netnarr lives on with the 2023 edition by @MiaZamoraPhD \n\nHome site updated https://t.co/koUYCpAoa4 and syndicating in course announcements but follow the action directly https://t.co/1CtwHFL51J https://t.co/oBfDghJW7d",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "netnarr",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MiaZamoraPhD",
+					"name": "Mia Zamora",
+					"id": 579873574,
+					"id_str": "579873574",
+					"indices": [
+						43,
+						56
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/koUYCpAoa4",
+					"expanded_url": "http://netnarr.arganee.world/",
+					"display_url": "netnarr.arganee.world",
+					"indices": [
+						77,
+						100
+					]
+				},
+				{
+					"url": "https://t.co/1CtwHFL51J",
+					"expanded_url": "http://netnarr23.miazamoraphd.com/",
+					"display_url": "netnarr23.miazamoraphd.com",
+					"indices": [
+						172,
+						195
+					]
+				},
+				{
+					"url": "https://t.co/oBfDghJW7d",
+					"expanded_url": "https://twitter.com/MiaZamoraPhD/status/1621523690512093185",
+					"display_url": "twitter.com/MiaZamoraPhD/s…",
+					"indices": [
+						196,
+						219
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10191,
+			"friends_count": 1528,
+			"listed_count": 765,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 934,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1621523690512093200,
+		"quoted_status_id_str": "1621523690512093185",
+		"quoted_status_permalink": {
+			"url": "https://t.co/oBfDghJW7d",
+			"expanded": "https://twitter.com/MiaZamoraPhD/status/1621523690512093185",
+			"display": "twitter.com/MiaZamoraPhD/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Feb 03 14:59:08 +0000 2023",
+			"id": 1621523690512093200,
+			"id_str": "1621523690512093185",
+			"full_text": "Our first #NetNarr '23 experiment: the human voice vs. #chatgpt in our \"the human nature of writing\" course. https://t.co/hy51hAdOdF #unboundeq @EnglishOER @Bali_Maha @OnlineCrsLady @dogtrax @cogdog",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				198
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NetNarr",
+						"indices": [
+							10,
+							18
+						]
+					},
+					{
+						"text": "chatgpt",
+						"indices": [
+							55,
+							63
+						]
+					},
+					{
+						"text": "unboundeq",
+						"indices": [
+							133,
+							143
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "EnglishOER",
+						"name": "Anna Mills, amills@mastodon.oeru.org, she/her",
+						"id": 1283804252482048000,
+						"id_str": "1283804252482048005",
+						"indices": [
+							144,
+							155
+						]
+					},
+					{
+						"screen_name": "Bali_Maha",
+						"name": "Maha Bali, PhD مها بالي 🌵",
+						"id": 1535273520,
+						"id_str": "1535273520",
+						"indices": [
+							156,
+							166
+						]
+					},
+					{
+						"screen_name": "OnlineCrsLady",
+						"name": "Laura Gibbs",
+						"id": 7044082,
+						"id_str": "7044082",
+						"indices": [
+							167,
+							181
+						]
+					},
+					{
+						"screen_name": "dogtrax",
+						"name": "KevinHodgson",
+						"id": 13307352,
+						"id_str": "13307352",
+						"indices": [
+							182,
+							190
+						]
+					},
+					{
+						"screen_name": "cogdog",
+						"name": "Alan Levine",
+						"id": 740343,
+						"id_str": "740343",
+						"indices": [
+							191,
+							198
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/hy51hAdOdF",
+						"expanded_url": "http://netnarr23.miazamoraphd.com/uncategorized/our-first-writing-experiment-the-human-voice-vs-chatgpt/",
+						"display_url": "netnarr23.miazamoraphd.com/uncategorized/…",
+						"indices": [
+							109,
+							132
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 579873574,
+				"id_str": "579873574",
+				"name": "Mia Zamora",
+				"screen_name": "MiaZamoraPhD",
+				"location": "",
+				"description": "Prof of Eng, Dir of MA in Writing Studies @KUWSP; #OER20 Co-Chair; Co-founder @unboundeq @netnarr; ELC Vol 4 co-editor @eliterature; Dig Culture Fulbright",
+				"url": "https://t.co/km69bjb0mK",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/km69bjb0mK",
+								"expanded_url": "http://miazamoraphd.com",
+								"display_url": "miazamoraphd.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3543,
+				"friends_count": 2448,
+				"listed_count": 201,
+				"created_at": "Mon May 14 13:20:04 +0000 2012",
+				"favourites_count": 14795,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 16508,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2242077148/MZpic_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2242077148/MZpic_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/579873574/1432505380",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#netnarr lives on with the 2023 edition by @MiaZamoraPhD \n\nHome site updated https://t.co/koUYCpAoa4 and syndicating in course announcements but follow the action directly https://t.co/1CtwHFL51J https://t.co/oBfDghJW7d"
+	},
+	{
 		"created_at": "Fri Feb 03 16:48:55 +0000 2023",
 		"id": 1621551314797830100,
 		"id_str": "1621551314797830147",

36dc9a39d01a696c17fa227d328cae6813d23704
Alan Levine <cogdogblog@gmail.com>
2023-03-01 09:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94981,
-	"created_at": "Wed Mar 01 02:15:09 +0000 2023",
+	"tweets": 94982,
+	"created_at": "Wed Mar 01 15:15:08 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Wed Mar 01 14:48:49 +0000 2023",
+		"id": 1630943178382532600,
+		"id_str": "1630943178382532610",
+		"full_text": "@ds106dc  #dailycreate ... #tdc4065 #ds106  With some catch up scramble to fill my February quota for the Daily Create Leaderboard (59/59) I had to ask the Eight Ball if I could keep pace for March. Apparently, I need to ask again at the end of the month. https://t.co/mBAK1oiZWO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "dailycreate",
+					"indices": [
+						10,
+						22
+					]
+				},
+				{
+					"text": "tdc4065",
+					"indices": [
+						27,
+						35
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						36,
+						42
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1630942868050178000,
+					"id_str": "1630942868050178048",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/FqJF5bsWcAAZVb1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqJF5bsWcAAZVb1.jpg",
+					"url": "https://t.co/mBAK1oiZWO",
+					"display_url": "pic.twitter.com/mBAK1oiZWO",
+					"expanded_url": "https://twitter.com/cogdog/status/1630943178382532610/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 668,
+							"h": 486,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 486,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 668,
+							"h": 486,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1630942868050178000,
+					"id_str": "1630942868050178048",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/FqJF5bsWcAAZVb1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqJF5bsWcAAZVb1.jpg",
+					"url": "https://t.co/mBAK1oiZWO",
+					"display_url": "pic.twitter.com/mBAK1oiZWO",
+					"expanded_url": "https://twitter.com/cogdog/status/1630943178382532610/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 668,
+							"h": 486,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 486,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 668,
+							"h": 486,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10177,
+			"friends_count": 1526,
+			"listed_count": 763,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 927,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96422,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc  #dailycreate ... #tdc4065 #ds106  With some catch up scramble to fill my February quota for the Daily Create Leaderboard (59/59) I had to ask the Eight Ball if I could keep pace for March. Apparently, I need to ask again at the end of the month. https://t.co/mBAK1oiZWO"
+	},
+	{
 		"created_at": "Wed Mar 01 01:30:52 +0000 2023",
 		"id": 1630742366809211000,
 		"id_str": "1630742366809210882",

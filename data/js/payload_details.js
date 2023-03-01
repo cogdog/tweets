6d022eb3eabd4e5c1187a7c4052f3f4572de824a
Alan Levine <cogdogblog@gmail.com>
2023-02-28 18:15:10 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 94979,
-	"created_at": "Mon Feb 27 21:15:19 +0000 2023",
+	"tweets": 94980,
+	"created_at": "Wed Mar 01 00:15:09 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Tue Feb 28 23:20:43 +0000 2023",
+		"id": 1630709613287940000,
+		"id_str": "1630709613287940096",
+		"full_text": "@ds106dc #dailycreate ... #tdc4064 #ds106  For these dogs the mystery fills the sky. \n\nI sought (did not resort to AI) an image of a cat in a flying saucer, but found instead Tigger the Well-Processed Cat\n\nhttps://t.co/N8aQGeSmdz https://t.co/Jfa5EkCgU6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "dailycreate",
+					"indices": [
+						9,
+						21
+					]
+				},
+				{
+					"text": "tdc4064",
+					"indices": [
+						26,
+						34
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						35,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/N8aQGeSmdz",
+					"expanded_url": "https://www.flickr.com/photos/24354425@N03/40269617760",
+					"display_url": "flickr.com/photos/2435442…",
+					"indices": [
+						206,
+						229
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1630709092611154000,
+					"id_str": "1630709092611153920",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/FqFxR6UWYAAzriR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqFxR6UWYAAzriR.jpg",
+					"url": "https://t.co/Jfa5EkCgU6",
+					"display_url": "pic.twitter.com/Jfa5EkCgU6",
+					"expanded_url": "https://twitter.com/cogdog/status/1630709613287940096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 566,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 566,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1630709092611154000,
+					"id_str": "1630709092611153920",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/FqFxR6UWYAAzriR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqFxR6UWYAAzriR.jpg",
+					"url": "https://t.co/Jfa5EkCgU6",
+					"display_url": "pic.twitter.com/Jfa5EkCgU6",
+					"expanded_url": "https://twitter.com/cogdog/status/1630709613287940096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 566,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 566,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10179,
+			"friends_count": 1526,
+			"listed_count": 763,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 927,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96418,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc #dailycreate ... #tdc4064 #ds106  For these dogs the mystery fills the sky. \n\nI sought (did not resort to AI) an image of a cat in a flying saucer, but found instead Tigger the Well-Processed Cat\n\nhttps://t.co/N8aQGeSmdz https://t.co/Jfa5EkCgU6"
+	},
+	{
 		"created_at": "Mon Feb 27 20:29:45 +0000 2023",
 		"id": 1630304197806571500,
 		"id_str": "1630304197806571526",

e141f3523a0ee91a1156cb08808f1e6fd8b052c8
Alan Levine <cogdogblog@gmail.com>
2023-03-06 13:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 95003,
-	"created_at": "Mon Mar 06 16:15:08 +0000 2023",
+	"tweets": 95004,
+	"created_at": "Mon Mar 06 19:15:08 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,164 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Mon Mar 06 19:02:19 +0000 2023",
+		"id": 1632818909756752000,
+		"id_str": "1632818909756751873",
+		"full_text": "@AnnGagne @brennacgray @greeneterry @karenraycosta I have more than photos- I care about stories of openness as creating potential serendipity\n\nhttps://t.co/wYNJgMbHES",
+		"truncated": false,
+		"display_text_range": [
+			51,
+			167
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AnnGagne",
+					"name": "Ann Gagné, PhD",
+					"id": 404289962,
+					"id_str": "404289962",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "brennacgray",
+					"name": "Brenna has the same @ on mastodon DOT social",
+					"id": 14109848,
+					"id_str": "14109848",
+					"indices": [
+						10,
+						22
+					]
+				},
+				{
+					"screen_name": "greeneterry",
+					"name": "Greene, Terry",
+					"id": 212019344,
+					"id_str": "212019344",
+					"indices": [
+						23,
+						35
+					]
+				},
+				{
+					"screen_name": "karenraycosta",
+					"name": "Karen Costa (she/her)",
+					"id": 25416326,
+					"id_str": "25416326",
+					"indices": [
+						36,
+						50
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wYNJgMbHES",
+					"expanded_url": "https://stories.cogdogblog.com/",
+					"display_url": "stories.cogdogblog.com",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1632772540752904200,
+		"in_reply_to_status_id_str": "1632772540752904192",
+		"in_reply_to_user_id": 404289962,
+		"in_reply_to_user_id_str": "404289962",
+		"in_reply_to_screen_name": "AnnGagne",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10174,
+			"friends_count": 1526,
+			"listed_count": 761,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 927,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96451,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@AnnGagne @brennacgray @greeneterry @karenraycosta I have more than photos- I care about stories of openness as creating potential serendipity\n\nhttps://t.co/wYNJgMbHES"
+	},
+	{
 		"created_at": "Mon Mar 06 15:44:09 +0000 2023",
 		"id": 1632769039893241900,
 		"id_str": "1632769039893241864",

b23c7004e0e01176857618aa1e667501c399d550
Alan Levine <cogdogblog@gmail.com>
2023-03-23 12:15:10 -0600

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,294 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Thu Mar 23 18:07:08 +0000 2023",
+		"id": 1638965616488124400,
+		"id_str": "1638965616488124417",
+		"full_text": "@ResearchBuzz To each their AI own. Is there pride / joy in pressing a button on the easy create image oven? Is promptism creativity?\n\nMy hand made remixes for me are a joy to make and a weird gift of sorts for friends like @BryanAlexander and @jimgroom https://t.co/v2SrtKilw8",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResearchBuzz",
+					"name": "Tara Calishain",
+					"id": 6979102,
+					"id_str": "6979102",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "BryanAlexander",
+					"name": "Bryan Alexander",
+					"id": 755991,
+					"id_str": "755991",
+					"indices": [
+						224,
+						239
+					]
+				},
+				{
+					"screen_name": "jimgroom",
+					"name": "Jim Groom",
+					"id": 3362981,
+					"id_str": "3362981",
+					"indices": [
+						244,
+						253
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1638965389546668000,
+					"id_str": "1638965389546668032",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fr7GVpMWwAAMRrR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fr7GVpMWwAAMRrR.jpg",
+					"url": "https://t.co/v2SrtKilw8",
+					"display_url": "pic.twitter.com/v2SrtKilw8",
+					"expanded_url": "https://twitter.com/cogdog/status/1638965616488124417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1638965389546668000,
+					"id_str": "1638965389546668032",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fr7GVpMWwAAMRrR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fr7GVpMWwAAMRrR.jpg",
+					"url": "https://t.co/v2SrtKilw8",
+					"display_url": "pic.twitter.com/v2SrtKilw8",
+					"expanded_url": "https://twitter.com/cogdog/status/1638965616488124417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1638965389534150700,
+					"id_str": "1638965389534150656",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fr7GVpJXwAA4Pm5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fr7GVpJXwAA4Pm5.jpg",
+					"url": "https://t.co/v2SrtKilw8",
+					"display_url": "pic.twitter.com/v2SrtKilw8",
+					"expanded_url": "https://twitter.com/cogdog/status/1638965616488124417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1618,
+							"h": 688,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 289,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1638965389550841900,
+					"id_str": "1638965389550841869",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fr7GVpNWcA0e-kd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fr7GVpNWcA0e-kd.jpg",
+					"url": "https://t.co/v2SrtKilw8",
+					"display_url": "pic.twitter.com/v2SrtKilw8",
+					"expanded_url": "https://twitter.com/cogdog/status/1638965616488124417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 371,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 546,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 546,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1638957027677573000,
+		"in_reply_to_status_id_str": "1638957027677573121",
+		"in_reply_to_user_id": 6979102,
+		"in_reply_to_user_id_str": "6979102",
+		"in_reply_to_screen_name": "ResearchBuzz",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10171,
+			"friends_count": 1523,
+			"listed_count": 761,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 929,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96541,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ResearchBuzz To each their AI own. Is there pride / joy in pressing a button on the easy create image oven? Is promptism creativity?\n\nMy hand made remixes for me are a joy to make and a weird gift of sorts for friends like @BryanAlexander and @jimgroom https://t.co/v2SrtKilw8"
+	},
+	{
 		"created_at": "Wed Mar 22 23:15:43 +0000 2023",
 		"id": 1638680888748945400,
 		"id_str": "1638680888748945408",

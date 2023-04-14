ab8b0234ff5f258cea87ab9afdbe2677fa8baa81
Alan Levine <cogdogblog@gmail.com>
2023-04-14 04:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 95132,
-	"created_at": "Fri Apr 14 06:15:08 +0000 2023",
+	"tweets": 95133,
+	"created_at": "Fri Apr 14 10:15:08 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Fri Apr 14 10:10:33 +0000 2023",
+		"id": 1646818214947553300,
+		"id_str": "1646818214947553282",
+		"full_text": "@ds106 #tdc4109 #ds106 #dailycreate From circa 1998, A Web of Labyrinths https://t.co/dpVNWtulCo\n\nwatch out for broken links and dead technologies... https://t.co/XN3g0tRn86",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc4109",
+					"indices": [
+						7,
+						15
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						16,
+						22
+					]
+				},
+				{
+					"text": "dailycreate",
+					"indices": [
+						23,
+						35
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106",
+					"name": "ds106 is #4life",
+					"id": 104730042,
+					"id_str": "104730042",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dpVNWtulCo",
+					"expanded_url": "https://mcli.cogdogblog.com/smc/labyrinth/",
+					"display_url": "mcli.cogdogblog.com/smc/labyrinth/",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1646818142524514300,
+					"id_str": "1646818142524514306",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/FtqsYBrXoAIUQEI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtqsYBrXoAIUQEI.jpg",
+					"url": "https://t.co/XN3g0tRn86",
+					"display_url": "pic.twitter.com/XN3g0tRn86",
+					"expanded_url": "https://twitter.com/cogdog/status/1646818214947553282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 611,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2006,
+							"h": 1022,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 346,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1646818142524514300,
+					"id_str": "1646818142524514306",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/FtqsYBrXoAIUQEI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtqsYBrXoAIUQEI.jpg",
+					"url": "https://t.co/XN3g0tRn86",
+					"display_url": "pic.twitter.com/XN3g0tRn86",
+					"expanded_url": "https://twitter.com/cogdog/status/1646818214947553282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 611,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2006,
+							"h": 1022,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 346,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 104730042,
+		"in_reply_to_user_id_str": "104730042",
+		"in_reply_to_screen_name": "ds106",
+		"user": {
+			"id": 740343,
+			"id_str": "740343",
+			"name": "Alan Levine",
+			"screen_name": "cogdog",
+			"location": "Archydal, Saskatchewan",
+			"description": "Barks about web tech at https://t.co/ZxC70YWtmO Digs photography, guitars, #ds106, Wordpress, hiking, coding, the Who. Makes SPLOTs. Loves @corisaas",
+			"url": "https://t.co/wAzdfm9TIB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wAzdfm9TIB",
+							"expanded_url": "http://cog.dog",
+							"display_url": "cog.dog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ZxC70YWtmO",
+							"expanded_url": "http://cogdogblog.com",
+							"display_url": "cogdogblog.com",
+							"indices": [
+								24,
+								47
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10141,
+			"friends_count": 1521,
+			"listed_count": 758,
+			"created_at": "Wed Jan 31 21:24:02 +0000 2007",
+			"favourites_count": 930,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96621,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/740063389527859201/BN9buLB9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740343/1389655067",
+			"profile_link_color": "9D582E",
+			"profile_sidebar_border_color": "D9B17E",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106 #tdc4109 #ds106 #dailycreate From circa 1998, A Web of Labyrinths https://t.co/dpVNWtulCo\n\nwatch out for broken links and dead technologies... https://t.co/XN3g0tRn86"
+	},
+	{
 		"created_at": "Fri Apr 14 05:56:06 +0000 2023",
 		"id": 1646754181368234000,
 		"id_str": "1646754181368233984",

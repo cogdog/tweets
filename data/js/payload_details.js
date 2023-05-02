9d76e57181c600a5cbca831d8152c4ae76966fe6
Alan Levine <cogdogblog@gmail.com>
2023-05-01 19:15:09 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 95182,
-	"created_at": "Tue May 02 00:15:08 +0000 2023",
+	"tweets": 95183,
+	"created_at": "Tue May 02 01:15:08 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Tue May 02 01:10:31 +0000 2023",
+		"id": 1653178676681523200,
+		"id_str": "1653205294762139648",
+		"full_text": "Sharing slides: \"Towards Transparency: How Can We Distinguish AI from Human Text Going Forward?\" \n(Futile? I hope not!) \nhttps://t.co/yezlMwpua1\n\nAt the #FutureWriting Symposium, building on @CriticalAI @Bali_Maha @DrSarahEaton @sleepinyourhat @geoffreyhinton and Jaron Lanier.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FutureWriting",
+					"indices": [
+						153,
+						167
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CriticalAI",
+					"name": "Critical AI : first issue coming in Aug. 23! :)",
+					"id": 1290793547122303000,
+					"id_str": "1290793547122302977",
+					"indices": [
+						191,
+						202
+					]
+				},
+				{
+					"screen_name": "Bali_Maha",
+					"name": "Maha Bali, PhD مها بالي 🌵",
+					"id": 1535273520,
+					"id_str": "1535273520",
+					"indices": [
+						203,
+						213
+					]
+				},
+				{
+					"screen_name": "DrSarahEaton",
+					"name": "Dr. Sarah Elaine Eaton🇨🇦",
+					"id": 36371994,
+					"id_str": "36371994",
+					"indices": [
+						214,
+						227
+					]
+				},
+				{
+					"screen_name": "sleepinyourhat",
+					"name": "Sam Bowman",
+					"id": 338526004,
+					"id_str": "338526004",
+					"indices": [
+						228,
+						243
+					]
+				},
+				{
+					"screen_name": "geoffreyhinton",
+					"name": "Geoffrey Hinton",
+					"id": 1084212657761148900,
+					"id_str": "1084212657761148928",
+					"indices": [
+						244,
+						259
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yezlMwpua1",
+					"expanded_url": "https://bit.ly/TowardsTransparencyGPT",
+					"display_url": "bit.ly/TowardsTranspa…",
+					"indices": [
+						121,
+						144
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1283804252482048000,
+			"id_str": "1283804252482048005",
+			"name": "Anna Mills, amills@mastodon.oeru.org, she/her",
+			"screen_name": "EnglishOER",
+			"location": "San Francisco",
+			"description": "Writing teacher, author of #OER text https://t.co/YCqlG8jlY1, interested in #LLMs, #OpenEd, #CollaborativeAnnotation. AI Resource list: https://t.co/hvweD6eFxH",
+			"url": "https://t.co/6nzUZzfVzJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6nzUZzfVzJ",
+							"expanded_url": "http://www.annarmills.com",
+							"display_url": "annarmills.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/YCqlG8jlY1",
+							"expanded_url": "http://HowArgumentsWork.org",
+							"display_url": "HowArgumentsWork.org",
+							"indices": [
+								37,
+								60
+							]
+						},
+						{
+							"url": "https://t.co/hvweD6eFxH",
+							"expanded_url": "https://bit.ly/AIwritingEDU",
+							"display_url": "bit.ly/AIwritingEDU",
+							"indices": [
+								136,
+								159
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2749,
+			"friends_count": 4998,
+			"listed_count": 42,
+			"created_at": "Thu Jul 16 16:42:49 +0000 2020",
+			"favourites_count": 17631,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4297,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283809287181963265/JZChOmRq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283809287181963265/JZChOmRq_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sharing slides: \"Towards Transparency: How Can We Distinguish AI from Human Text Going Forward?\" \n(Futile? I hope not!) \nhttps://t.co/yezlMwpua1\n\nAt the #FutureWriting Symposium, building on @CriticalAI @Bali_Maha @DrSarahEaton @sleepinyourhat @geoffreyhinton and Jaron Lanier."
+	},
+	{
 		"created_at": "Mon May 01 23:31:36 +0000 2023",
 		"id": 1653180398115987500,
 		"id_str": "1653180398115987458",
